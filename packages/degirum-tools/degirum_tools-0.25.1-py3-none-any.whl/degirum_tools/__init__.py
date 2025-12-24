393a692b20e21d043802de00c234a5397c7c07f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 14:04:59 +0000

    Add package degirum-tools/0.25.1/degirum_tools-0.25.1-py3-none-any.whl

---
+++ packages/degirum-tools/degirum_tools-0.25.1-py3-none-any.whl/degirum_tools/__init__.py
@@ -0,0 +1,111 @@
+#
+# degirum_tools.py: toolkit for PySDK samples
+#
+# Copyright DeGirum Corporation 2023
+# All rights reserved
+#
+
+import logging
+from typing import Optional
+
+
+def logger_get():
+    """
+    Get the package logger.
+
+    Returns:
+        Returns the package logger.
+    """
+    return logging.getLogger(__name__)
+
+
+def logger_add_handler(
+    handler: Optional[logging.Handler] = None,
+    format: str = "",
+    level: int = logging.DEBUG,
+) -> logging.Handler:
+    """
+    Add a handler to the package logger.
+
+    Args:
+        handler: Handler to add to the logger. If None, a new StreamHandler to console is added.
+        format: Format string for handler formatter. Defaults to "%(asctime)s [%(levelname)s][%(threadName)s] %(message)s".
+        level: Logging level as defined in logging python package. Defaults to logging.DEBUG.
+
+    Returns:
+        Returns an instance of added handler.
+    """
+    logger = logger_get()
+
+    if handler is None:
+        handler = logging.StreamHandler()
+
+    if not format:
+        format = "%(asctime)s [%(levelname)s][%(threadName)s] %(message)s"
+    handler.setFormatter(logging.Formatter(format))
+    logger.addHandler(handler)
+    logger.setLevel(level)
+    return handler
+
+
+# flake8: noqa
+
+import argparse
+
+from ._version import __version__, __version_info__
+from .analyzers import *
+from .compound_models import *
+from .evaluators import *
+from .inference_support import *
+from .tools import *
+
+# aliases for backward compatibility
+from .tools import (
+    in_colab as _in_colab,
+    reload_env as _reload_env,
+    get_test_mode as _get_test_mode,
+)
+
+
+def _command_entrypoint(arg_str=None):
+    from .tools.figure_annotator import (
+        _figure_annotator_args,
+        _zone_annotator_args,
+        _line_annotator_args,
+    )
+
+    parser = argparse.ArgumentParser(description="DeGirum tools")
+
+    subparsers = parser.add_subparsers(
+        help="use -h flag to see help on subcommands", required=True
+    )
+
+    # figure_annotator subcommand
+    subparser = subparsers.add_parser(
+        "figure_annotator",
+        description="Launch interactive utility for geometric figure annotation in images",
+        help="launch interactive utility for geometric figure annotation in images",
+    )
+    _figure_annotator_args(subparser)
+
+    # zone_annotator subcommand
+    subparser = subparsers.add_parser(
+        "zone_annotator",
+        description="Launch interactive utility for 4-sided zone annotation in images",
+        help="launch interactive utility for 4-sided zone annotation in images",
+    )
+    _zone_annotator_args(subparser)
+
+    # line_annotator subcommand
+    subparser = subparsers.add_parser(
+        "line_annotator",
+        description="Launch interactive utility for line annotation in images",
+        help="launch interactive utility for line annotation in images",
+    )
+    _line_annotator_args(subparser)
+
+    # parse args
+    args = parser.parse_args(arg_str.split() if arg_str else None)
+
+    # execute subcommand
+    args.func(args)
---
+++ packages/degirum-tools/degirum_tools-0.25.1-py3-none-any.whl/degirum_tools/_version.py
@@ -0,0 +1,10 @@
+#
+# _version.py: degirum_tools package version
+#
+# Copyright DeGirum Corporation 2025
+# All rights reserved
+#
+
+# >>> increment version here vvv
+__version_info__ = ("0", "25", "1")
+__version__ = ".".join(__version_info__)
