64391a1f00a472d47316ea3daba1ecc8f0fc485c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 20:05:46 +0000

    Add package social-insight-summarizer/2025.12.21113835/social_insight_summarizer-2025.12.21113835-py3-none-any.whl

---
+++ packages/social-insight-summarizer/social_insight_summarizer-2025.12.21113835-py3-none-any.whl/social_insight_summarizer/__init__.py
@@ -0,0 +1,4 @@
+from .main import social_insight_summarizer
+from .prompts import human_prompt, pattern, system_prompt
+
+__all__ = ["social_insight_summarizer", "system_prompt", "human_prompt", "pattern"]
