f1a97f7511b14eda8df84a3b09dd3900e61139d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 18:04:45 +0000

    Add package ez-a-sync/0.33.10/ez_a_sync-0.33.10-cp310-cp310-manylinux1_i686.manylinux_2_28_i686.manylinux_2_5_i686.whl

---
+++ packages/ez-a-sync/ez_a_sync-0.33.10-cp310-cp310-manylinux1_i686.manylinux_2_28_i686.manylinux_2_5_i686.whl/a_sync/asyncio/igather.pyi
@@ -0,0 +1,8 @@
+import asyncio.tasks
+from typing import Awaitable, Iterable, List, TypeVar
+
+__T = TypeVar("__T")
+
+def igather(
+    coros_or_futures: Iterable[Awaitable[__T]], return_exceptions: bool = False
+) -> asyncio.tasks._GatheringFuture[List[__T]]: ...
