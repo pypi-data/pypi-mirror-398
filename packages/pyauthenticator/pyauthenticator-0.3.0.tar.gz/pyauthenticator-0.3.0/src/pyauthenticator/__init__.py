18a7d64643c67e03ca3ba41bcd4588dbd7d46b1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 19:05:31 +0000

    Add package pyauthenticator/0.3.0/pyauthenticator-0.3.0.tar.gz

---
+++ packages/pyauthenticator/pyauthenticator-0.3.0.tar.gz/pyauthenticator-0.3.0/src/pyauthenticator/__init__.py
@@ -0,0 +1,19 @@
+"""
+Generate two factor authentication codes on the command line
+"""
+
+from . import _version
+from pyauthenticator._user import (
+    write_qrcode_to_file,
+    get_two_factor_code,
+    add_two_factor_provider,
+    list_two_factor_providers,
+)
+
+__all__ = [
+    "write_qrcode_to_file",
+    "get_two_factor_code",
+    "add_two_factor_provider",
+    "list_two_factor_providers",
+]
+__version__: str = _version.__version__
