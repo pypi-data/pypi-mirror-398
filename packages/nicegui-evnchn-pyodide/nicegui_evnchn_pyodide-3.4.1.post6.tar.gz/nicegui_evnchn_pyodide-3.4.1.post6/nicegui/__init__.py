1f9e0acff9183ab9623949b6a53f80bb79366e11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 19:05:49 +0000

    Add package nicegui-evnchn-pyodide/3.4.1.post6/nicegui_evnchn_pyodide-3.4.1.post6.tar.gz

---
+++ packages/nicegui-evnchn-pyodide/nicegui_evnchn_pyodide-3.4.1.post6.tar.gz/nicegui_evnchn_pyodide-3.4.1.post6/nicegui/__init__.py
@@ -0,0 +1,39 @@
+from . import binding, elements, html, storage, ui
+from .api_router import APIRouter
+from .app.app import App
+from .client import Client
+from .context import context
+from .element_filter import ElementFilter
+from .event import Event
+from .page_arguments import PageArguments
+from .version import __version__
+from .pyodide_compat import IS_PYODIDE
+
+# Conditionally import server-dependent modules
+if not IS_PYODIDE:
+    from . import run
+    from .nicegui import app
+    from .page import page
+else:
+    run = None  # type: ignore
+    app = None  # type: ignore
+    from .page_pyodide import page
+
+__all__ = [
+    'APIRouter',
+    'App',
+    'Client',
+    'ElementFilter',
+    'Event',
+    'PageArguments',
+    '__version__',
+    'app',
+    'binding',
+    'context',
+    'elements',
+    'html',
+    'page',
+    'run',
+    'storage',
+    'ui',
+]
