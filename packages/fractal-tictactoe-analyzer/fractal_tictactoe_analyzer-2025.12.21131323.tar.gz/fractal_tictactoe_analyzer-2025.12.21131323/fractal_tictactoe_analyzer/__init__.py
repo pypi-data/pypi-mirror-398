31d2bba62160841c010bbca1f2f1d6492a769872
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 20:05:56 +0000

    Add package fractal-tictactoe-analyzer/2025.12.21131323/fractal_tictactoe_analyzer-2025.12.21131323.tar.gz

---
+++ packages/fractal-tictactoe-analyzer/fractal_tictactoe_analyzer-2025.12.21131323.tar.gz/fractal_tictactoe_analyzer-2025.12.21131323/fractal_tictactoe_analyzer/__init__.py
@@ -0,0 +1,4 @@
+from .main import fractal_tictactoe_analyzer
+from .prompts import human_prompt, pattern, system_prompt
+
+__all__ = ["fractal_tictactoe_analyzer", "system_prompt", "human_prompt", "pattern"]
