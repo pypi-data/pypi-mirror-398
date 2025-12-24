17cd1c617f307809b2786144712fb45997252005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 19:05:35 +0000

    Add package devtools-qr/1.0.0/devtools_qr-1.0.0.tar.gz

---
+++ packages/devtools-qr/devtools_qr-1.0.0.tar.gz/devtools_qr-1.0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="devtools_qr",
+    version="1.0.0",
+    description="QR code generator",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="DevTools.at",
+    author_email="hello@devtools.at",
+    url="https://devtools.at/tools/qr-generator",
+    project_urls={
+        "Homepage": "https://devtools.at/tools/qr-generator",
+        "Repository": "https://github.com/devtools-at/qr-generator",
+    },
+    packages=find_packages(),
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+)
