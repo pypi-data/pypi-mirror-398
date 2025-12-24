9b3578af6bc350d84ea7657151da06978eef9d92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 20:04:43 +0000

    Add package devtools-json-formatter/1.0.1/devtools_json_formatter-1.0.1.tar.gz

---
+++ packages/devtools-json-formatter/devtools_json_formatter-1.0.1.tar.gz/devtools_json_formatter-1.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="devtools_json_formatter",
+    version="1.0.1",
+    description="JSON formatter/prettifier",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="DevTools.at",
+    author_email="hello@devtools.at",
+    url="https://devtools.at/tools/json-formatter",
+    project_urls={
+        "Homepage": "https://devtools.at/tools/json-formatter",
+        "Repository": "https://github.com/devtools-at/json-formatter",
+    },
+    packages=find_packages(),
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+)
