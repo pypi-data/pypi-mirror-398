e3db4d70fe34914c799b0b3f8842bd6cd1ed4f0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 20:04:44 +0000

    Add package gjt/2.1/gjt-2.1.tar.gz

---
+++ packages/gjt/gjt-2.1.tar.gz/gjt-2.1/gjt/__init__.py
@@ -0,0 +1,11 @@
+from misc.connect_to_websocket import connect_to_websocket
+from misc.get_error_data import get_error_data
+from misc.get_network_data import get_network_data
+from account.login_to_account import login_to_account
+from misc.skips_calculator import skips_calculator
+__all__ = ["connect_to_websocket", "get_error_data", "get_network_data", "login_to_account", "skips_calculator"]
+__version__ = "2.1"
+
+
+
+
