434ab78c66e70104e5a26d0a7999195d59e4113c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 16:05:28 +0000

    Add package pyhandel/0.1.11/pyhandel-0.1.11-cp313-cp313-manylinux_2_34_x86_64.whl

---
+++ packages/pyhandel/pyhandel-0.1.11-cp313-cp313-manylinux_2_34_x86_64.whl/pyhandel/__init__.pyi
@@ -0,0 +1,17 @@
+from pyhandel import player as player
+
+from . import messages as messages
+from . import animals as animals
+from . import client as client
+
+from typing import TypeAlias
+
+Money: TypeAlias = int
+Value: TypeAlias = int
+
+__all__ = [
+    "player",
+    "messages",
+    "animals",
+    "client",
+]
