837df1cd5092a305b0ae1b8b80a8a23c036c168d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 20:05:48 +0000

    Add package design-insight-builder/2025.12.21114125/design_insight_builder-2025.12.21114125-py3-none-any.whl

---
+++ packages/design-insight-builder/design_insight_builder-2025.12.21114125-py3-none-any.whl/design_insight_builder/__init__.py
@@ -0,0 +1,4 @@
+from .main import design_insight_builder
+from .prompts import human_prompt, pattern, system_prompt
+
+__all__ = ["design_insight_builder", "system_prompt", "human_prompt", "pattern"]
