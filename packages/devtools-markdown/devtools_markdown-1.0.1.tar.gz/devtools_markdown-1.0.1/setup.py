45c718755487a366ed4bfbdf9247da45c6a222d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 20:04:45 +0000

    Add package devtools-markdown/1.0.1/devtools_markdown-1.0.1.tar.gz

---
+++ packages/devtools-markdown/devtools_markdown-1.0.1.tar.gz/devtools_markdown-1.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="devtools_markdown",
+    version="1.0.1",
+    description="Markdown preview",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="DevTools.at",
+    author_email="hello@devtools.at",
+    url="https://devtools.at/tools/markdown-preview",
+    project_urls={
+        "Homepage": "https://devtools.at/tools/markdown-preview",
+        "Repository": "https://github.com/devtools-at/markdown-preview",
+    },
+    packages=find_packages(),
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+)
