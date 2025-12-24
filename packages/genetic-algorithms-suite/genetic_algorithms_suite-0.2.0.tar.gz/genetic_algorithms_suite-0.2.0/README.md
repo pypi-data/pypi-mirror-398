eb0b9ff1fd4314f048c7f4c103bd3c7106c98390
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 16:05:18 +0000

    Add package genetic-algorithms-suite/0.2.0/genetic_algorithms_suite-0.2.0.tar.gz

---
+++ packages/genetic-algorithms-suite/genetic_algorithms_suite-0.2.0.tar.gz/genetic_algorithms_suite-0.2.0/README.md
@@ -0,0 +1,13 @@
+# Genetic Algorithms Suite
+
+A collection of genetic algorithm implementations for:
+
+- Map Coloring
+- N-Queens
+- Knapsack Problem
+- Function Optimization
+- Traveling Salesman Problem (TSP)
+
+## Installation
+```bash
+pip install genetic-algorithms-suite
---
+++ packages/genetic-algorithms-suite/genetic_algorithms_suite-0.2.0.tar.gz/genetic_algorithms_suite-0.2.0/src/genetic_algorithms/__init__.py
@@ -0,0 +1,5 @@
+from .map_coloring import genetic_algorithm as map_coloring_ga
+from .n_queens import run_genetic_algorithm as n_queens_ga
+from .knapsack import genetic_algorithm as knapsack_ga
+from .function_optimization import genetic_algorithm as func_opt_ga
+from .tsp import genetic_algorithm as tsp_ga
