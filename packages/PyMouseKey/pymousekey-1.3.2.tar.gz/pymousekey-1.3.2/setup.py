182ed0ce5836eb08a79318d3f30106dc57bc245a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 16:04:42 +0000

    Add package PyMouseKey/1.3.2/pymousekey-1.3.2.tar.gz

---
+++ packages/PyMouseKey/pymousekey-1.3.2.tar.gz/pymousekey-1.3.2/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+VERSION = '1.3.2'
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+setuptools.setup(
+    name="PyMouseKey",
+    version=VERSION,
+    author="Chasss",
+    description="A python package for handling keyboard and mouse inputs",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+)
