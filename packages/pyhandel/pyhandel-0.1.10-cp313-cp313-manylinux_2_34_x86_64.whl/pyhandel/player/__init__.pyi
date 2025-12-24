5d70314f7448ebcaeee5ccaf702bec27c71f0bde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 16:05:24 +0000

    Add package pyhandel/0.1.10/pyhandel-0.1.10-cp313-cp313-manylinux_2_34_x86_64.whl

---
+++ packages/pyhandel/pyhandel-0.1.10-cp313-cp313-manylinux_2_34_x86_64.whl/pyhandel/player/__init__.pyi
@@ -0,0 +1,11 @@
+from . import player_actions as player_actions
+from . import simple_player as simple_player
+from . import random_player as random_player
+from . import wallet as wallet
+
+__all__ = [
+    "player_actions",
+    "simple_player",
+    "random_player",
+    "wallet",
+]
