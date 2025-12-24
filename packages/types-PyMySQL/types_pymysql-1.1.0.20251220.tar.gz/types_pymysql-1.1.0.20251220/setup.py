aca0332e544afb16d079524d99a836b1afb64dbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 15:04:34 +0000

    Add package types-PyMySQL/1.1.0.20251220/types_pymysql-1.1.0.20251220.tar.gz

---
+++ packages/types-PyMySQL/types_pymysql-1.1.0.20251220.tar.gz/types_pymysql-1.1.0.20251220/setup.py
@@ -0,0 +1,4 @@
+
+from setuptools import setup
+
+setup(package_data={'pymysql-stubs': ['__init__.pyi', '_auth.pyi', 'charset.pyi', 'connections.pyi', 'constants/CLIENT.pyi', 'constants/COMMAND.pyi', 'constants/CR.pyi', 'constants/ER.pyi', 'constants/FIELD_TYPE.pyi', 'constants/FLAG.pyi', 'constants/SERVER_STATUS.pyi', 'constants/__init__.pyi', 'converters.pyi', 'cursors.pyi', 'err.pyi', 'optionfile.pyi', 'protocol.pyi', 'times.pyi', 'METADATA.toml', 'py.typed']})
