6c9de0974f9f5b3e4ab27b36162ec2ee47a03efe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 19:06:22 +0000

    Add package dash-pydantic-form/0.18.0/dash_pydantic_form-0.18.0-py3-none-any.whl

---
+++ packages/dash-pydantic-form/dash_pydantic_form-0.18.0-py3-none-any.whl/dash_pydantic_form/ids.py
@@ -0,0 +1,36 @@
+from functools import partial
+
+from dash.dependencies import _Wildcard
+
+
+def form_dependent_id(component: str, aio_id: str | _Wildcard, form_id: str | _Wildcard) -> dict:
+    """A component id to do callbacks at the document level (e.g. edit/delete)."""
+    return {"component": component, "aio_id": aio_id, "form_id": form_id}
+
+
+def form_base_id(part: str, aio_id: str | _Wildcard, form_id: str | _Wildcard, parent: str | _Wildcard = ""):
+    """Form parts id."""
+    return {"part": part, "aio_id": aio_id, "form_id": form_id, "parent": parent}
+
+
+def field_dependent_id(  # noqa: PLR0913
+    component: str,
+    aio_id: str | _Wildcard,
+    form_id: str | _Wildcard,
+    field: str | _Wildcard,
+    parent: str | _Wildcard = "",
+    meta: str | _Wildcard = "",
+) -> dict:
+    """A component id to do callbacks at the field level (e.g. in the form)."""
+    return {
+        "component": component,
+        "aio_id": aio_id,
+        "form_id": form_id,
+        "field": field,
+        "parent": parent,
+        "meta": meta,
+    }
+
+
+value_field = partial(field_dependent_id, "_pydf-value-field")
+checked_field = partial(field_dependent_id, "_pydf-checked-field")
