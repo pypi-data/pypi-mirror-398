9e6c3ead65badff037f4dfd3739282a7dbe80ba8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 19:05:50 +0000

    Add package nicegui-evnchn-pyodide/3.4.1.post14/nicegui_evnchn_pyodide-3.4.1.post14-py3-none-any.whl

---
+++ packages/nicegui-evnchn-pyodide/nicegui_evnchn_pyodide-3.4.1.post14-py3-none-any.whl/nicegui/staticfiles.py
@@ -0,0 +1,21 @@
+try:
+    from starlette.responses import Response
+    from starlette.staticfiles import StaticFiles
+    from starlette.types import Scope
+except ImportError:
+    Response = None  # type: ignore
+    StaticFiles = object  # type: ignore
+    Scope = None  # type: ignore
+
+
+class CacheControlledStaticFiles(StaticFiles):  # type: ignore
+
+    def __init__(self, *args, max_cache_age: int = 3600, **kwargs) -> None:
+        self.max_cache_age = max_cache_age
+        if StaticFiles is not object:
+            super().__init__(*args, **kwargs)
+
+    async def get_response(self, path: str, scope: Scope) -> Response:
+        response = await super().get_response(path, scope)
+        response.headers['Cache-Control'] = f'public, max-age={self.max_cache_age}'
+        return response
