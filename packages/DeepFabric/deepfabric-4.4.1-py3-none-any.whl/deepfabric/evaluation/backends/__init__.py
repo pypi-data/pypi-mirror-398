f77bc5eadf6e51b6dfe5ba2a62f7ea5506524e68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 19:05:53 +0000

    Add package DeepFabric/4.4.1/deepfabric-4.4.1-py3-none-any.whl

---
+++ packages/DeepFabric/deepfabric-4.4.1-py3-none-any.whl/deepfabric/evaluation/backends/__init__.py
@@ -0,0 +1,32 @@
+"""Inference backend implementations."""
+
+from .ollama_backend import OllamaBackend
+from .tool_call_parsers import (
+    GenericToolCallParser,
+    HermesToolCallParser,
+    LlamaToolCallParser,
+    MistralToolCallParser,
+    QwenToolCallParser,
+    ToolCallParser,
+    ToolCallParserRegistry,
+    get_parser,
+    get_parser_for_model,
+    register_parser,
+)
+from .transformers_backend import TransformersBackend
+
+__all__ = [
+    "TransformersBackend",
+    "OllamaBackend",
+    # Tool call parsers
+    "ToolCallParser",
+    "ToolCallParserRegistry",
+    "QwenToolCallParser",
+    "LlamaToolCallParser",
+    "MistralToolCallParser",
+    "HermesToolCallParser",
+    "GenericToolCallParser",
+    "get_parser",
+    "get_parser_for_model",
+    "register_parser",
+]
