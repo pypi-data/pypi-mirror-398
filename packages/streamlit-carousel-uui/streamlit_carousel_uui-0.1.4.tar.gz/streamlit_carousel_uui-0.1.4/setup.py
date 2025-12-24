8bc9cf980e5ac81ff90d3a03487148cefef58861
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 14:04:10 +0000

    Add package streamlit-carousel-uui/0.1.4/streamlit_carousel_uui-0.1.4.tar.gz

---
+++ packages/streamlit-carousel-uui/streamlit_carousel_uui-0.1.4.tar.gz/streamlit_carousel_uui-0.1.4/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setuptools.setup(
+    name="streamlit-carousel-uui",
+    version="0.1.4",
+    author="Jan du Plessis",
+    author_email="",
+    description="A Streamlit component for displaying an Untitled UI styled carousel",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/janduplessis883/streamlit_carousel_uui",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "streamlit>=1.0.0",
+    ],
+)
