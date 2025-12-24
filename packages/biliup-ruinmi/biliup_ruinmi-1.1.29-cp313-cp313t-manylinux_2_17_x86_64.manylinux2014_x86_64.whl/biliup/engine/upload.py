cf1e34c4887be97fa783de5c26b317b99f6b023d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 19:05:22 +0000

    Add package biliup-ruinmi/1.1.29/biliup_ruinmi-1.1.29-cp313-cp313t-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/biliup-ruinmi/biliup_ruinmi-1.1.29-cp313-cp313t-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/biliup/engine/upload.py
@@ -0,0 +1,22 @@
+import logging
+
+from typing import NamedTuple, Optional, List
+
+logger = logging.getLogger('biliup')
+logger1 = logging.getLogger('biliup1')
+
+
+class UploadBase:
+    class FileInfo(NamedTuple):
+        video: str
+        danmaku: Optional[str]
+
+    def __init__(self, principal, data, persistence_path=None, postprocessor=None):
+        self.principal = principal
+        self.persistence_path = persistence_path
+        self.data: dict = data
+        self.post_processor = postprocessor
+
+
+    def upload(self, file_list: List[FileInfo]) -> List[FileInfo]:
+        raise NotImplementedError()
