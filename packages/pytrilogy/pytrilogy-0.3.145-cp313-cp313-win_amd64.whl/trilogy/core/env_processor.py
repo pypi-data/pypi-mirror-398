75951ced8942cf3c44ecb57b9ef2934a109bd14a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 19:06:32 +0000

    Add package pytrilogy/0.3.145/pytrilogy-0.3.145-cp313-cp313-win_amd64.whl

---
+++ packages/pytrilogy/pytrilogy-0.3.145-cp313-cp313-win_amd64.whl/trilogy/core/env_processor.py
@@ -0,0 +1,137 @@
+from trilogy.core.enums import Derivation
+from trilogy.core.graph_models import (
+    ReferenceGraph,
+    concept_to_node,
+    datasource_to_node,
+)
+from trilogy.core.models.build import BuildConcept, BuildDatasource
+from trilogy.core.models.build_environment import BuildEnvironment
+
+
+def add_concept(
+    concept: BuildConcept,
+    g: ReferenceGraph,
+    concept_mapping: dict[str, BuildConcept],
+    default_concept_graph: dict[str, BuildConcept],
+    seen: set[str],
+):
+
+    # if we have sources, recursively add them
+    node_name = concept_to_node(concept)
+    if node_name in seen:
+        return
+    seen.add(node_name)
+    g.concepts[node_name] = concept
+    g.add_node(node_name)
+    root_name = node_name.split("@", 1)[0]
+    if concept.concept_arguments:
+        for source in concept.concept_arguments:
+            if not isinstance(source, BuildConcept):
+                raise ValueError(
+                    f"Invalid non-build concept {source} passed into graph generation from {concept}"
+                )
+            generic = get_default_grain_concept(source, default_concept_graph)
+            generic_node = concept_to_node(generic)
+            add_concept(generic, g, concept_mapping, default_concept_graph, seen)
+
+            g.add_edge(generic_node, node_name, fast=True)
+    for ps_address in concept.pseudonyms:
+        if ps_address not in concept_mapping:
+            raise SyntaxError(f"Concept {concept} has invalid pseudonym {ps_address}")
+        pseudonym = concept_mapping[ps_address]
+        pseudonym = get_default_grain_concept(pseudonym, default_concept_graph)
+        pseudonym_node = concept_to_node(pseudonym)
+        if (pseudonym_node, node_name) in g.edges and (
+            node_name,
+            pseudonym_node,
+        ) in g.edges:
+            continue
+        if pseudonym_node.split("@", 1)[0] == root_name:
+            continue
+        g.add_edge(pseudonym_node, node_name, fast=True)
+        g.add_edge(node_name, pseudonym_node, fast=True)
+        g.pseudonyms.add((pseudonym_node, node_name))
+        g.pseudonyms.add((node_name, pseudonym_node))
+        add_concept(pseudonym, g, concept_mapping, default_concept_graph, seen)
+
+
+def get_default_grain_concept(
+    concept: BuildConcept, default_concept_graph: dict[str, BuildConcept]
+) -> BuildConcept:
+    """Get the default grain concept from the graph."""
+    if concept.address in default_concept_graph:
+        return default_concept_graph[concept.address]
+    default = concept.with_default_grain()
+    default_concept_graph[concept.address] = default
+    return default
+
+
+def generate_adhoc_graph(
+    concepts: list[BuildConcept],
+    datasources: list[BuildDatasource],
+    default_concept_graph: dict[str, BuildConcept],
+) -> ReferenceGraph:
+    g = ReferenceGraph()
+    concept_mapping = {x.address: x for x in concepts}
+    seen: set[str] = set()
+    for concept in concepts:
+        if not isinstance(concept, BuildConcept):
+            raise ValueError(f"Invalid non-build concept {concept}")
+
+    # add all parsed concepts
+    for concept in concepts:
+        add_concept(concept, g, concept_mapping, default_concept_graph, seen)
+
+    basic_map = [
+        ({c.canonical_address for c in concept.concept_arguments}, concept)
+        for concept in concepts
+        if concept.derivation == Derivation.BASIC and concept.concept_arguments
+    ]
+    for dataset in datasources:
+        node = datasource_to_node(dataset)
+        g.add_datasource_node(node, dataset)
+        eligible = dataset.concepts
+        seen = set(x.canonical_address for x in eligible)
+        complete_contains = set(
+            [c.concept.canonical_address for c in dataset.columns if c.is_complete]
+        )
+        break_flag = False
+        while not break_flag:
+            break_flag = True
+            for input_set, concept in basic_map:
+                if input_set.issubset(complete_contains):
+                    if concept.canonical_address not in seen:
+                        break_flag = False
+                        seen.add(concept.canonical_address)
+                        eligible.append(concept)
+
+        for concept in eligible:
+            cnode = concept_to_node(concept)
+            g.concepts[cnode] = concept
+            g.add_node(cnode)
+            g.add_edge(node, cnode, fast=True)
+            g.add_edge(cnode, node, fast=True)
+            # if there is a key on a table at a different grain
+            # add an FK edge to the canonical source, if it exists
+            # for example, order ID on order product table
+            default = get_default_grain_concept(concept, default_concept_graph)
+
+            if concept != default:
+                dcnode = concept_to_node(default)
+                g.concepts[dcnode] = default
+                g.add_node(dcnode)
+                g.add_edge(cnode, dcnode, fast=True)
+                g.add_edge(dcnode, cnode, fast=True)
+    return g
+
+
+def generate_graph(
+    environment: BuildEnvironment,
+) -> ReferenceGraph:
+    default_concept_graph: dict[str, BuildConcept] = {}
+    return generate_adhoc_graph(
+        list(environment.concepts.values())
+        + list(environment.alias_origin_lookup.values()),
+        list(environment.datasources.values()),
+        default_concept_graph=default_concept_graph,
+    )
