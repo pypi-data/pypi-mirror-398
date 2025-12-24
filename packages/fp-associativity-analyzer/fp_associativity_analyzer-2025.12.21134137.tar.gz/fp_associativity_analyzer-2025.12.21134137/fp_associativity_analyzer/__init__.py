a3d74a45bea2820a9530bada5df2244e441ad334
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 20:04:49 +0000

    Add package fp-associativity-analyzer/2025.12.21134137/fp_associativity_analyzer-2025.12.21134137.tar.gz

---
+++ packages/fp-associativity-analyzer/fp_associativity_analyzer-2025.12.21134137.tar.gz/fp_associativity_analyzer-2025.12.21134137/fp_associativity_analyzer/__init__.py
@@ -0,0 +1,4 @@
+from .main import fp_associativity_analyzer
+from .prompts import human_prompt, pattern, system_prompt
+
+__all__ = ["fp_associativity_analyzer", "system_prompt", "human_prompt", "pattern"]
