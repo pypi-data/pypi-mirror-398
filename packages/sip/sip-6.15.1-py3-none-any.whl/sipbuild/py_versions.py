1ec4eca335018989427dc7fe91f4a4690ff8d94f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 17:04:22 +0000

    Add package sip/6.15.1/sip-6.15.1-py3-none-any.whl

---
+++ packages/sip/sip-6.15.1-py3-none-any.whl/sipbuild/py_versions.py
@@ -0,0 +1,17 @@
+# SPDX-License-Identifier: BSD-2-Clause
+
+# Copyright (c) 2025 Phil Thompson <phil@riverbankcomputing.com>
+
+
+# The major number of the default target ABI.  Remember to update the
+# documentation.
+DEFAULT_ABI_MAJOR = 13
+
+# The minimum required version of setuptools.  This is the earliest version
+# that generates correct wheel names for PyPI.  Remember to update
+# pyproject.toml in the root directory.
+MINIMUM_SETUPTOOLS = '75.8.1'
+
+# The oldest supported minor version of Python v3.  Remember to update
+# pyproject.toml in the root directory.
+OLDEST_SUPPORTED_MINOR = 10
