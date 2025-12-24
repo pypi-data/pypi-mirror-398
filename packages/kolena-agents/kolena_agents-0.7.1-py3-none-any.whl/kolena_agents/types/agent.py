405faad56d36acf593432b06988e9661ffd34f00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 14:04:26 +0000

    Add package kolena-agents/0.7.1/kolena_agents-0.7.1-py3-none-any.whl

---
+++ packages/kolena-agents/kolena_agents-0.7.1-py3-none-any.whl/kolena_agents/types/agent.py
@@ -0,0 +1,5 @@
+from kolena_agents._generated.openapi_client import Agent  # type: ignore
+from kolena_agents._generated.openapi_client import CopyAgentRequest  # type: ignore
+from kolena_agents._generated.openapi_client import ListAgentsResponse  # type: ignore
+
+__all__ = ["Agent", "ListAgentsResponse", "CopyAgentRequest"]
