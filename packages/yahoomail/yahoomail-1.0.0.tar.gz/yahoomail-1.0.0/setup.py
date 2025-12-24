2bef8f564a4d773e9ca94e11be6b9b2949a5b028
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 16:05:52 +0000

    Add package yahoomail/1.0.0/yahoomail-1.0.0.tar.gz

---
+++ packages/yahoomail/yahoomail-1.0.0.tar.gz/yahoomail-1.0.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="yahoomail",
+    version="1.0.0",
+    author="anarchy223",
+    description="A high-performance, async Yahoo Mail login automator.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/anarchy223/yahoomail",
+    packages=find_packages(),
+    license="CC BY-NC-SA 4.0",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Free For Home Use",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        "httpx>=0.24.0",
+        "h2>=4.1.0",
+    ],
+)
