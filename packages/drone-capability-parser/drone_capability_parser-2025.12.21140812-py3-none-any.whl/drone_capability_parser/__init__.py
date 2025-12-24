23cf277732fc1010788f53dfb93832d34c712fc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 20:04:54 +0000

    Add package drone-capability-parser/2025.12.21140812/drone_capability_parser-2025.12.21140812-py3-none-any.whl

---
+++ packages/drone-capability-parser/drone_capability_parser-2025.12.21140812-py3-none-any.whl/drone_capability_parser/__init__.py
@@ -0,0 +1,4 @@
+from .main import drone_capability_parser
+from .prompts import human_prompt, pattern, system_prompt
+
+__all__ = ["drone_capability_parser", "system_prompt", "human_prompt", "pattern"]
