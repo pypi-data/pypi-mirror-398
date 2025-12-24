61d5e0c2f3f7aca606ded96e6c8408e2385d61f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 20:04:45 +0000

    Add package devtools-list-to-json/1.0.1/devtools_list_to_json-1.0.1.tar.gz

---
+++ packages/devtools-list-to-json/devtools_list_to_json-1.0.1.tar.gz/devtools_list_to_json-1.0.1/devtools_list_to_json/__init__.py
@@ -0,0 +1,27 @@
+"""
+List to JSON array converter
+
+Online tool: https://devtools.at/tools/list-to-json
+"""
+
+__version__ = "1.0.1"
+__author__ = "DevTools.at"
+__url__ = "https://devtools.at/tools/list-to-json"
+
+
+def process(input_data: str) -> str:
+    """Process input data."""
+    return input_data
+
+
+def encode(data: str) -> str:
+    """Encode data."""
+    return data
+
+
+def decode(data: str) -> str:
+    """Decode data."""
+    return data
+
+
+__all__ = ["process", "encode", "decode", "__version__", "__author__", "__url__"]
