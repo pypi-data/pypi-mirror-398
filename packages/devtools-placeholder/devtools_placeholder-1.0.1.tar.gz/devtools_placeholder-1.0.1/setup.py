dff34f1466e621f677736d78186db0b70e3d7812
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 20:04:46 +0000

    Add package devtools-placeholder/1.0.1/devtools_placeholder-1.0.1.tar.gz

---
+++ packages/devtools-placeholder/devtools_placeholder-1.0.1.tar.gz/devtools_placeholder-1.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="devtools_placeholder",
+    version="1.0.1",
+    description="Placeholder image generator",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="DevTools.at",
+    author_email="hello@devtools.at",
+    url="https://devtools.at/tools/placeholder-image",
+    project_urls={
+        "Homepage": "https://devtools.at/tools/placeholder-image",
+        "Repository": "https://github.com/devtools-at/placeholder-image",
+    },
+    packages=find_packages(),
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+)
