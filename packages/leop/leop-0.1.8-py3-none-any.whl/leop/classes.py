ba10f0f3d6ddc52c9172d940ba8f386a3b794cc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 15:04:34 +0000

    Add package leop/0.1.8/leop-0.1.8-py3-none-any.whl

---
+++ packages/leop/leop-0.1.8-py3-none-any.whl/leop/classes.py
@@ -0,0 +1,38 @@
+import time
+class pos():
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+class clock():
+    def __init__(self, h=0, m=0, s=0):
+        self.h = h
+        self.m = m
+        self.s = s
+    def str(self, a='0:0:0'):
+        x=a.split(':')
+        self.h = int(x[0])
+        self.m = int(x[1])
+        self.s = int(x[2])
+    def put(self):
+        return f'{self.h}:{self.m}:{self.s}'
+    #更新为实时时间
+    def get(self):
+        self.h = time.localtime().tm_hour
+        self.m = time.localtime().tm_min
+        self.s = time.localtime().tm_sec
+class date():
+    def __init__(self, y=0, m=0, d=0):
+        self.y = y
+        self.m = m
+        self.d = d
+    def str(self, a='0-0-0',s='-'):
+        x=a.split(s)
+        self.y = int(x[0])
+        self.m = int(x[1])
+        self.d = int(x[2])
+    def put(self,s='-'):
+        return f'{self.y}{s}{self.m}{s}{self.d}'
+    def get(self):
+        self.y = time.localtime().tm_year
+        self.m = time.localtime().tm_mon
+        self.d = time.localtime().tm_mday
