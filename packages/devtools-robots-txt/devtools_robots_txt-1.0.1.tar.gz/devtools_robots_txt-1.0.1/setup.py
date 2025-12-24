285c24497f2b4e20eaaf039f19322a80e029e55f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 20:04:47 +0000

    Add package devtools-robots-txt/1.0.1/devtools_robots_txt-1.0.1.tar.gz

---
+++ packages/devtools-robots-txt/devtools_robots_txt-1.0.1.tar.gz/devtools_robots_txt-1.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="devtools_robots_txt",
+    version="1.0.1",
+    description="robots.txt generator",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="DevTools.at",
+    author_email="hello@devtools.at",
+    url="https://devtools.at/tools/robots-txt-generator",
+    project_urls={
+        "Homepage": "https://devtools.at/tools/robots-txt-generator",
+        "Repository": "https://github.com/devtools-at/robots-txt-generator",
+    },
+    packages=find_packages(),
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+)
