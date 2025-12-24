8ddf3a56f03daf2edab92ef5c9a8abda3833ad40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 18:05:30 +0000

    Add package MTC-HandTracker/1.0.1/mtc_handtracker-1.0.1.tar.gz

---
+++ packages/MTC-HandTracker/mtc_handtracker-1.0.1.tar.gz/mtc_handtracker-1.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MTC-HandTracker",
+    version="1.0.1",
+    description="A professional, modular hand tracking library for robotics.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Majd Aburas (McMaster Technology Club)",
+    author_email="aburasm@mcmaster.ca",
+    url="https://github.com/McMaster-Technology-Club/Hand-Tracking",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "opencv-python",
+        "mediapipe",
+        "numpy",
+        "pyyaml"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Image Recognition",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires='>=3.8, <3.13',
+)
