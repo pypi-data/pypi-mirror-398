2f75174f1fbaed1d0848d9688908346da4a1c425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 20:04:47 +0000

    Add package devtools-random-string/1.0.1/devtools_random_string-1.0.1.tar.gz

---
+++ packages/devtools-random-string/devtools_random_string-1.0.1.tar.gz/devtools_random_string-1.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="devtools_random_string",
+    version="1.0.1",
+    description="Random string generator",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="DevTools.at",
+    author_email="hello@devtools.at",
+    url="https://devtools.at/tools/random-string",
+    project_urls={
+        "Homepage": "https://devtools.at/tools/random-string",
+        "Repository": "https://github.com/devtools-at/random-string",
+    },
+    packages=find_packages(),
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+)
