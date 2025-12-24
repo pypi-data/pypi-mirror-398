e61f241d59dcc8802f38614f54ba7da2ea579283
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 18:05:21 +0000

    Add package ai-data-science-team/0.0.0.9017/ai_data_science_team-0.0.0.9017-py3-none-any.whl

---
+++ packages/ai-data-science-team/ai_data_science_team-0.0.0.9017-py3-none-any.whl/ai_data_science_team/orchestration.py
@@ -0,0 +1,17 @@
+# BUSINESS SCIENCE UNIVERSITY
+# AI DATA SCIENCE TEAM
+# ***
+# Orchestration
+# ai_data_science_team/orchestration.py
+
+from ai_data_science_team.agents import data_cleaning_agent
+
+# TODO - add orchestration
+
+# def model_pipeline(model, log=True, log_path=None):
+    
+#     return "todo"
+    
+    
+    
+    
