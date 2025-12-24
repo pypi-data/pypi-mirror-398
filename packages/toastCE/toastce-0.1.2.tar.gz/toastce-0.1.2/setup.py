43e92f64e2a6cf5092039bb0b984229bc147289d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 18:04:52 +0000

    Add package toastCE/0.1.2/toastce-0.1.2.tar.gz

---
+++ packages/toastCE/toastce-0.1.2.tar.gz/toastce-0.1.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="toastCE",
+    version="0.1.2",
+    description="A easy and simple way to make custom exceptions.",
+    author="toasta (toastman32)",
+    author_email="toastman32@toastreal.xyz",
+    packages=find_packages(),
+    python_requires=">=3.7", 
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
