ef9db21d2dc53340e3d6bc9b1425e4556dd95bb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 15:04:00 +0000

    Add package powl/2.2.8/powl-2.2.8-py3-none-any.whl

---
+++ packages/powl/powl-2.2.8-py3-none-any.whl/powl/__init__.py
@@ -0,0 +1,17 @@
+from powl.main import (
+    convert_to_bpmn,
+    convert_to_petri_net,
+    discover,
+    discover_from_dfg,
+    discover_from_partially_ordered_log,
+    discover_petri_net_from_ocel,
+    import_event_log,
+    import_ocel,
+    save_visualization,
+    save_visualization_net,
+    view,
+    view_net,
+)
+
+__name__ = "powl"
+__version__ = "2.2.8"
