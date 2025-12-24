3c401a00b2f393baa1b6be0d3b22bbbec36d80ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 20:05:58 +0000

    Add package text-snippet-summarizer/2025.12.21113112/text_snippet_summarizer-2025.12.21113112-py3-none-any.whl

---
+++ packages/text-snippet-summarizer/text_snippet_summarizer-2025.12.21113112-py3-none-any.whl/text_snippet_summarizer/__init__.py
@@ -0,0 +1,4 @@
+from .main import text_snippet_summarizer
+from .prompts import human_prompt, pattern, system_prompt
+
+__all__ = ["text_snippet_summarizer", "system_prompt", "human_prompt", "pattern"]
