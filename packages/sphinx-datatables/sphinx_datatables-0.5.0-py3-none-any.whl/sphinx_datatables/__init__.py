d7c63bab2bb1977d88cbe670f17a873bb8a1dd45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 17:04:22 +0000

    Add package sphinx-datatables/0.5.0/sphinx_datatables-0.5.0-py3-none-any.whl

---
+++ packages/sphinx-datatables/sphinx_datatables-0.5.0-py3-none-any.whl/sphinx_datatables/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2023 Varun Sharma
+#
+# SPDX-License-Identifier: MIT
+
+"""Main __init__ file."""
+
+__version__ = "0.5.0"
+
+from .sphinx_datatables import setup
+
+__all__ = ["setup"]
