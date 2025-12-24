5fc262d91ff31350cafc906e23c17eb41c18418c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 20:04:46 +0000

    Add package tech-sentiment-analyzer/2025.12.21152823/tech_sentiment_analyzer-2025.12.21152823.tar.gz

---
+++ packages/tech-sentiment-analyzer/tech_sentiment_analyzer-2025.12.21152823.tar.gz/tech_sentiment_analyzer-2025.12.21152823/tech_sentiment_analyzer/__init__.py
@@ -0,0 +1,4 @@
+from .main import tech_sentiment_analyzer
+from .prompts import human_prompt, pattern, system_prompt
+
+__all__ = ["tech_sentiment_analyzer", "system_prompt", "human_prompt", "pattern"]
