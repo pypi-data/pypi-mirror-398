849de5044dae4e26970a7061ce8619a1fe88668c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 17:05:43 +0000

    Add package adsx/1.0.0/adsx-1.0.0.tar.gz

---
+++ packages/adsx/adsx-1.0.0.tar.gz/adsx-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="adsx",
+    version="1.0.0",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={"adsx": ["payload/*"]},
+    entry_points={
+        "console_scripts": [
+            "adsx=adsx.cli:run"
+        ]
+    },
+)
