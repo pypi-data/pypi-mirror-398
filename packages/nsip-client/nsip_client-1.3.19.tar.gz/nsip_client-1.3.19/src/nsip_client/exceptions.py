27aa294b746d329dfc570d58ec209614abf0e690
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 19:06:20 +0000

    Add package nsip-client/1.3.19/nsip_client-1.3.19.tar.gz

---
+++ packages/nsip-client/nsip_client-1.3.19.tar.gz/nsip_client-1.3.19/src/nsip_client/exceptions.py
@@ -0,0 +1,44 @@
+"""
+Custom exceptions for the NSIP API client
+"""
+
+
+class NSIPError(Exception):
+    """Base exception for all NSIP client errors"""
+
+    pass
+
+
+class NSIPAPIError(NSIPError):
+    """Raised when the API returns an error response"""
+
+    def __init__(self, message: str, status_code: int | None = None, response: dict | None = None):
+        super().__init__(message)
+        self.status_code = status_code
+        self.response = response
+
+
+class NSIPNotFoundError(NSIPAPIError):
+    """Raised when an animal or resource is not found"""
+
+    def __init__(self, message: str, search_string: str | None = None):
+        super().__init__(message, status_code=404)
+        self.search_string = search_string
+
+
+class NSIPConnectionError(NSIPError):
+    """Raised when connection to the API fails"""
+
+    pass
+
+
+class NSIPTimeoutError(NSIPError):
+    """Raised when a request times out"""
+
+    pass
+
+
+class NSIPValidationError(NSIPError):
+    """Raised when request parameters are invalid"""
+
+    pass
