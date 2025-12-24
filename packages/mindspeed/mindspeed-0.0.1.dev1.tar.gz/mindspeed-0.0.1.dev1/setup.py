7295b6de91ded8b3c57ab3bff219dfea9b62fd19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 20:05:52 +0000

    Add package mindspeed/0.0.1.dev1/mindspeed-0.0.1.dev1.tar.gz

---
+++ packages/mindspeed/mindspeed-0.0.1.dev1.tar.gz/mindspeed-0.0.1.dev1/setup.py
@@ -0,0 +1,44 @@
+# Copyright 2025 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===========================================================================
+import sys
+import setuptools
+
+readme = """
+*WARNING*:This project is not functional and is a placeholder form HUAWEI Ascend
+Please refer to homepage https://gitcode.com/Ascend/MindSpeed
+"""
+
+setuptools.setup(
+    name='mindspeed',
+    version='0.0.1.dev1',
+    description='MindSpeed for LLMs of Ascend',
+    url='https://gitcode.com/Ascend/MindSpeed',
+    long_description_content_type="text/markdown",
+    long_description=readme,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Information Technology',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent'
+    ],
+    license='Apache',
+    python_requires='>= 3.7',
+    include_package_data=True
+)
+
+if len(sys.argv) !=2 or sys.argv[1] != "sdist":
+    raise ValueError(readme)
