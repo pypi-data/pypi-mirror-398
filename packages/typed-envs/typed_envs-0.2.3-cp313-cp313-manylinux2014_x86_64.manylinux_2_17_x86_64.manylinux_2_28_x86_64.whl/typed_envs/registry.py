cd33a8868c959f2e6cb340d425565ead4ba1a07b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 19:06:27 +0000

    Add package typed-envs/0.2.3/typed_envs-0.2.3-cp313-cp313-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/typed-envs/typed_envs-0.2.3-cp313-cp313-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl/typed_envs/registry.py
@@ -0,0 +1,24 @@
+from typing import Final
+
+from typed_envs._env_var import EnvironmentVariable
+from typed_envs.typing import EnvRegistry, VarName
+
+
+ENVIRONMENT: Final[EnvRegistry] = EnvRegistry({})
+_ENVIRONMENT_VARIABLES_SET_BY_USER: Final[EnvRegistry] = EnvRegistry({})
+_ENVIRONMENT_VARIABLES_USING_DEFAULTS: Final[EnvRegistry] = EnvRegistry({})
+
+
+def _register_new_env(name: VarName, instance: EnvironmentVariable) -> None:
+    ENVIRONMENT[name] = instance
+    if instance._using_default:
+        _ENVIRONMENT_VARIABLES_USING_DEFAULTS[name] = instance
+    else:
+        _ENVIRONMENT_VARIABLES_SET_BY_USER[name] = instance
+
+
+__all__ = [
+    "ENVIRONMENT",
+    "_ENVIRONMENT_VARIABLES_SET_BY_USER",
+    "_ENVIRONMENT_VARIABLES_USING_DEFAULTS",
+]
