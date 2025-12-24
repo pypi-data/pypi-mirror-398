c16b97c44fddfc90401519ab40dd3f7a3f2dc323
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 17:04:20 +0000

    Add package mastodon-ui/1.0.4/mastodon_ui-1.0.4.tar.gz

---
+++ packages/mastodon-ui/mastodon_ui-1.0.4.tar.gz/mastodon_ui-1.0.4/src/mui/styles/__init__.py
@@ -0,0 +1,39 @@
+from mui.styles.elements import (
+    ComponentStyle,
+    element_style as ElementStyle,
+)
+from mui.styles.plain_css import (
+    CssRuleValidator,
+    CssRule,
+    CssAnimatable,
+    CssSelector,
+    css_style as Css_Style,
+    css_comment as Css_Comment,
+    box_model as Box_Model,
+    make_important as Make_Important,
+    Animation,
+    MediaQueries
+)
+from mui.styles.frameworks.bs5 import (
+    BS5,
+    BS5ElementStyle,
+    BS5Element,
+    )
+
+__all__ = [
+    'ComponentStyle',
+    'ElementStyle',
+    'CssRuleValidator',
+    'CssRule',
+    'CssAnimatable',
+    'CssSelector',
+    'Css_Style',
+    'BS5',
+    'Css_Comment',
+    'BS5ElementStyle',
+    'Box_Model',
+    'Make_Important',
+    'Animation',
+    'MediaQueries',
+    'BS5Element',
+]
---
+++ packages/mastodon-ui/mastodon_ui-1.0.4.tar.gz/mastodon_ui-1.0.4/src/mui/styles/frameworks/bs5/components/__init__.py
@@ -0,0 +1,28 @@
+'''from mui.styles.frameworks.bs5.components.bs5_comp import (
+    Accordion,
+    Alert,
+    Badge,
+    Breadcrumb,
+    Button,
+    CloseButton,
+    ButtonGroup,
+    Card,
+    Carousel,
+    Collapse,
+    Dropdown,
+)
+
+__all__ = [
+    'Accordion',
+    'Alert',
+    'Badge',
+    'Breadcrumb',
+    'Button',
+    'Card',
+    'Carousel',
+    'CloseButton',
+    'ButtonGroup',
+    'Collapse',
+    'Dropdown',
+]
+'''
