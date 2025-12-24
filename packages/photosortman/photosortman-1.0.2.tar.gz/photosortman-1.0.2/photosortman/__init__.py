50c733002fa987cec4fcf6d2542c3cd990784f87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 17:04:03 +0000

    Add package photosortman/1.0.2/photosortman-1.0.2.tar.gz

---
+++ packages/photosortman/photosortman-1.0.2.tar.gz/photosortman-1.0.2/photosortman/__init__.py
@@ -0,0 +1,11 @@
+"""
+Photosortman - A photo sorting tool with game controller support and AI quality analysis
+"""
+
+__version__ = "1.0.2"
+__author__ = "Rania Amina"
+__license__ = "GPL-3.0-or-later"
+
+from .photoman import PhotoSorterApp
+
+__all__ = ['PhotoSorterApp']
