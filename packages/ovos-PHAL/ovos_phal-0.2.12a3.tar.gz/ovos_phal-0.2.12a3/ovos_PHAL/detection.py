3e6527d28269af3feabc71449b061bc605bd8a81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 14:04:05 +0000

    Add package ovos-PHAL/0.2.12a3/ovos_phal-0.2.12a3.tar.gz

---
+++ packages/ovos-PHAL/ovos_phal-0.2.12a3.tar.gz/ovos_phal-0.2.12a3/ovos_PHAL/detection.py
@@ -0,0 +1,56 @@
+import subprocess
+from ovos_utils.log import deprecated
+
+
+@deprecated("Use from ovos-i2c-detection import is_sj201_v6", "0.2.0")
+def is_mycroft_sj201():
+    cmd = 'i2cdetect -y -a 1 0x04 0x04 | egrep "(04|UU)" | awk \'{print $2}\''
+    out = subprocess.check_output(cmd, shell=True).strip()
+    if out == b"04" or out == b"UU":
+        return True
+    return False
+
+
+@deprecated("Use from ovos-i2c-detection import is_wm8960", "0.2.0")
+def is_respeaker_2mic():
+    cmd = 'i2cdetect -y -a 1 0x1a 0x1a | egrep "(1a|UU)" | awk \'{print $2}\''
+    out = subprocess.check_output(cmd, shell=True).strip()
+    if out == b"1a" or out == b"UU":
+        return True
+    return False
+
+
+@deprecated("Use from ovos-i2c-detection import is_respeaker_4mic", "0.2.0")
+def is_respeaker_4mic():
+    cmd = 'i2cdetect -y -a 0x35 0x35 | egrep "(35|UU)" | awk \'{print $2}\''
+    out = subprocess.check_output(cmd, shell=True).strip()
+    if out == b"35" or out == b"UU":
+        return True
+    return False
+
+
+@deprecated("Use from ovos-i2c-detection import is_respeaker_6mic", "0.2.0")
+def is_respeaker_6mic():
+    cmd = 'i2cdetect -y -a 0x3b 0x3b | egrep "(3b|UU)" | awk \'{print $2}\''
+    out = subprocess.check_output(cmd, shell=True).strip()
+    if out == b"3b" or out == b"UU":
+        return True
+    return False
+
+
+@deprecated("Use from ovos-i2c-detection import is_adafruit_amp", "0.2.0")
+def is_adafruit():
+    cmd = 'i2cdetect -y -a 0x4b 0x4b | egrep "(4b|UU)" | awk \'{print $2}\''
+    out = subprocess.check_output(cmd, shell=True).strip()
+    if out == b"4b" or out == b"UU":
+        return True
+    return False
+
+
+@deprecated("Use from ovos-i2c-detection import is_tas5806", "0.2.0")
+def is_texas_tas5806():
+    cmd = 'i2cdetect -y -a 0x2f 0x2f | egrep "(2f|UU)" | awk \'{print $2}\''
+    out = subprocess.check_output(cmd, shell=True).strip()
+    if out == b"2f" or out == b"UU":
+        return True
+    return False
