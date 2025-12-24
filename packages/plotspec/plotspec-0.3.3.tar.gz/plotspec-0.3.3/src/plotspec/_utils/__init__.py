c8924fcbad60da8e7bddcfc08c0b6c13a7effb47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 19:06:25 +0000

    Add package plotspec/0.3.3/plotspec-0.3.3.tar.gz

---
+++ packages/plotspec/plotspec-0.3.3.tar.gz/plotspec-0.3.3/src/plotspec/_utils/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Utility modules for plotspec."""
+
+from ._numpy_io import load_array, save_array
+from ._diff import get_non_default_kwargs, is_default_value
+from ._units import mm_to_inch, inch_to_mm, mm_to_pt, pt_to_mm
+
+__all__ = [
+    "save_array",
+    "load_array",
+    "get_non_default_kwargs",
+    "is_default_value",
+    "mm_to_inch",
+    "inch_to_mm",
+    "mm_to_pt",
+    "pt_to_mm",
+]
+
+# Optional: image comparison (requires PIL)
+try:
+    from ._image_diff import compare_images, create_comparison_figure
+    __all__.extend(["compare_images", "create_comparison_figure"])
+except ImportError:
+    pass
