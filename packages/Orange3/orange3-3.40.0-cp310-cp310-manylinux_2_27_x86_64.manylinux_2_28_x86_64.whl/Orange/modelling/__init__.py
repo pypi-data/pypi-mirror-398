da0d9bf4fd963cb0221422d8df19ce410e310a06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 17:05:29 +0000

    Add package Orange3/3.40.0/orange3-3.40.0-cp310-cp310-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/Orange3/orange3-3.40.0-cp310-cp310-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/Orange/modelling/__init__.py
@@ -0,0 +1,23 @@
+# Pull members from modules to Orange.modelling namespace
+# pylint: disable=wildcard-import
+
+from .base import *
+
+from .ada_boost import *
+from .constant import *
+from .knn import *
+from .linear import *
+from .neural_network import *
+from .randomforest import *
+from .svm import *
+from .tree import *
+from .column import *
+try:
+    from .catgb import *
+except ImportError:
+    pass
+from .gb import *
+try:
+    from .xgb import *
+except ImportError:
+    pass
