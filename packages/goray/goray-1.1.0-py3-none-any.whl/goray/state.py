8102b4c4033f09094f38145d5391fbd044895818
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 19:05:28 +0000

    Add package goray/1.1.0/goray-1.1.0-py3-none-any.whl

---
+++ packages/goray/goray-1.1.0-py3-none-any.whl/goray/state.py
@@ -0,0 +1,16 @@
+"""global state"""
+
+import os
+
+from . import consts, utils
+
+# values are raycore.go2py.PyActorWrapper / x.actor.GoActorWrapper
+actors = utils.ThreadSafeLocalStore()
+
+# value is ray object ref
+futures = utils.ThreadSafeLocalStore()
+
+golibpath: str = os.environ.get(consts.GORAY_BIN_PATH_ENV, "")
+pymodulepath: str = os.environ.get(consts.GORAY_PY_MUDULE_PATH_ENV, "")
+
+debug = False
