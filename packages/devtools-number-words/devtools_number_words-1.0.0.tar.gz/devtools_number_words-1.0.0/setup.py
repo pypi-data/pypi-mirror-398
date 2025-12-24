16c0bc777403da298dc7daaf7d67434ea63872ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 19:05:34 +0000

    Add package devtools-number-words/1.0.0/devtools_number_words-1.0.0.tar.gz

---
+++ packages/devtools-number-words/devtools_number_words-1.0.0.tar.gz/devtools_number_words-1.0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="devtools_number_words",
+    version="1.0.0",
+    description="Number to words converter",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="DevTools.at",
+    author_email="hello@devtools.at",
+    url="https://devtools.at/tools/number-to-words",
+    project_urls={
+        "Homepage": "https://devtools.at/tools/number-to-words",
+        "Repository": "https://github.com/devtools-at/number-to-words",
+    },
+    packages=find_packages(),
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+)
