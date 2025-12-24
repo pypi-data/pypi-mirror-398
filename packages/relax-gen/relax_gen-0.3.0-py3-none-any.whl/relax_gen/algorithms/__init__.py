fa72dc8aea4cf706000f54187b5a7b131ccc21cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 19:06:22 +0000

    Add package relax-gen/0.3.0/relax_gen-0.3.0-py3-none-any.whl

---
+++ packages/relax-gen/relax_gen-0.3.0-py3-none-any.whl/relax_gen/algorithms/__init__.py
@@ -0,0 +1,7 @@
+# __init__.py dentro de algoritmos/
+from .alg_bin import cl_alg_stn_bin
+from .alg_quantum import cl_alg_quantum
+from .alg_eda import cl_alg_eda
+from .alg_gp import cl_alg_gp
+
+__all__ = ["cl_alg_stn_bin", "cl_alg_quantum", "cl_alg_eda", "cl_alg_gp"]
