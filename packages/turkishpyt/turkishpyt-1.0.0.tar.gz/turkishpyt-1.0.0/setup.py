38d16b5f2730602b25344c2f97b255457fc7cc53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 19:05:26 +0000

    Add package turkishpyt/1.0.0/turkishpyt-1.0.0.tar.gz

---
+++ packages/turkishpyt/turkishpyt-1.0.0.tar.gz/turkishpyt-1.0.0/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="turkishpyt",
+    version="1.0.0",
+    packages=find_packages(),
+    description="Türkçe Python kütüphanesi",
+    license="MIT",
+    python_requires=">=3.7",
+)
