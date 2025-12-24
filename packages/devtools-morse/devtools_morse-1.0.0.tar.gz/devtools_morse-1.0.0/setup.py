7363d07652da8ef0fb00231866696c1fcd727cc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 19:05:34 +0000

    Add package devtools-morse/1.0.0/devtools_morse-1.0.0.tar.gz

---
+++ packages/devtools-morse/devtools_morse-1.0.0.tar.gz/devtools_morse-1.0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="devtools_morse",
+    version="1.0.0",
+    description="Morse code translator",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="DevTools.at",
+    author_email="hello@devtools.at",
+    url="https://devtools.at/tools/morse-code",
+    project_urls={
+        "Homepage": "https://devtools.at/tools/morse-code",
+        "Repository": "https://github.com/devtools-at/morse-code",
+    },
+    packages=find_packages(),
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+)
