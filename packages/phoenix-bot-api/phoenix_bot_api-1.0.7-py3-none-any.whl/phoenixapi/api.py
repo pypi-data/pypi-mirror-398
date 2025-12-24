4dae971dc15dd38d602794f3ea22410d31454b54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 19:05:16 +0000

    Add package phoenix-bot-api/1.0.7/phoenix_bot_api-1.0.7-py3-none-any.whl

---
+++ packages/phoenix-bot-api/phoenix_bot_api-1.0.7-py3-none-any.whl/phoenixapi/api.py
@@ -0,0 +1,24 @@
+from .clients.client_socket import ClientSocket
+from .clients.player_manager import PlayerObjManagerClient
+from .clients.scene_manager import SceneManagerClient
+from .clients.packet_manager import PacketManagerClient
+from .clients.skill_manager import SkillManagerClient
+from .clients.pet_manager import PetManagerClient
+from .clients.inventory_manager import InventoryManagerClient
+from .clients.bot_controller import BotControllerClient
+from .clients.friend_manager import FriendManagerClient
+from .clients.login_manager import LoginClient
+    
+class PhoenixApi:
+    def __init__(self, port: int):
+        socket = ClientSocket(port)
+        
+        self.player_obj_manager = PlayerObjManagerClient(socket)
+        self.scene_manager = SceneManagerClient(socket)
+        self.packet_manager = PacketManagerClient(socket)
+        self.skill_manager = SkillManagerClient(socket)
+        self.pet_manager = PetManagerClient(socket)
+        self.inventory_manager = InventoryManagerClient(socket)
+        self.bot_controller = BotControllerClient(socket)
+        self.friend_manager = FriendManagerClient(socket)
+        self.login_manager = LoginClient(socket)
