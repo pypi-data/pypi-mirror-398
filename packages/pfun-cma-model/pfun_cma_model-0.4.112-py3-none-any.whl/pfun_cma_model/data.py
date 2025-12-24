8369ce533b4f4902fd5466bfe32f21d7efcbbc97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 16:04:49 +0000

    Add package pfun-cma-model/0.4.112/pfun_cma_model-0.4.112-py3-none-any.whl

---
+++ packages/pfun-cma-model/pfun_cma_model-0.4.112-py3-none-any.whl/pfun_cma_model/data.py
@@ -0,0 +1,30 @@
+from pfun_cma_model.engine.data_utils import format_data
+from pfun_cma_model.misc.pathdefs import PFunDataPaths
+import pandas as pd
+
+__all__ = [
+    "format_data",
+    "PFunDataPaths",
+    "read_sample_data",
+]
+
+
+def read_sample_data(convert2json: bool) -> pd.DataFrame | str:
+    """Read the sample dataset from the PFunDataPaths.
+
+    Args:
+        convert2json (bool): If True, convert the DataFrame to JSON string format.
+
+    Returns:
+        pd.DataFrame or str: The sample dataset as a DataFrame or JSON string.
+
+    Example:
+        >>> df = read_sample_data()
+        >>> print(df.head())
+
+    """
+    from pfun_cma_model.misc.pathdefs import PFunDataPaths
+    df = PFunDataPaths().read_sample_data()
+    if convert2json is False:
+        return df
+    return df.to_json(orient='records')
