e29e233b4f751e71b7c2175c52e1bdc69a7c58e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 16:05:27 +0000

    Add package manageconfig/1.1.2/manageconfig-1.1.2-py3-none-any.whl

---
+++ packages/manageconfig/manageconfig-1.1.2-py3-none-any.whl/manageconfig/config.py
@@ -0,0 +1,21 @@
+import yaml
+import json
+from typing import Dict
+from pathlib import Path
+
+
+class Config:
+    def __init__(self, config_items: Dict) -> None:
+        for key, value in config_items.items():
+            attr_value = Config(value) if isinstance(value, dict) else value
+            self.__setattr__(key, attr_value)
+
+    @staticmethod
+    def load_from_yml(path: Path):
+        with open(path) as f:
+            return Config(yaml.safe_load(f))
+
+    @staticmethod
+    def load_from_json(path: Path):
+        with open(path) as f:
+            return Config(json.load(f))
