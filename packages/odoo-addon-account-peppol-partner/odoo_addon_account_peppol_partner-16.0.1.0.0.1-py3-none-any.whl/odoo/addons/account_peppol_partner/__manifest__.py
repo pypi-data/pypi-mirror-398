652715884b18c72140d78331a5b76d785d8c056e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 19:05:55 +0000

    Add package odoo-addon-account-peppol-partner/16.0.1.0.0.1/odoo_addon_account_peppol_partner-16.0.1.0.0.1-py3-none-any.whl

---
+++ packages/odoo-addon-account-peppol-partner/odoo_addon_account_peppol_partner-16.0.1.0.0.1-py3-none-any.whl/odoo/addons/account_peppol_partner/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Account Peppol Partner",
+    "summary": """Peppol information on partners.""",
+    "version": "16.0.1.0.0",
+    "license": "LGPL-3",
+    "author": "Odoo S.A.,ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/acsone/odoo-peppol-backport",
+    "depends": [
+        "account",
+        "mail",
+    ],
+    "data": ["views/res_partner_views.xml"],
+}
---
+++ packages/odoo-addon-account-peppol-partner/odoo_addon_account_peppol_partner-16.0.1.0.0.1-py3-none-any.whl/odoo/addons/account_peppol_partner/readme/DESCRIPTION.md
@@ -0,0 +1,3 @@
+Peppol information on partners.
+
+This is a partial backport of `account_edi_ubl_cii` from Odoo 17.
