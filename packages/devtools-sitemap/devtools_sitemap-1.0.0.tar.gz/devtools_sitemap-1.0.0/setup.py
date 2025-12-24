b06eee3fa593ece3e484e6d9cd729a51228b094d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 19:05:36 +0000

    Add package devtools-sitemap/1.0.0/devtools_sitemap-1.0.0.tar.gz

---
+++ packages/devtools-sitemap/devtools_sitemap-1.0.0.tar.gz/devtools_sitemap-1.0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="devtools_sitemap",
+    version="1.0.0",
+    description="XML sitemap generator",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="DevTools.at",
+    author_email="hello@devtools.at",
+    url="https://devtools.at/tools/sitemap-generator",
+    project_urls={
+        "Homepage": "https://devtools.at/tools/sitemap-generator",
+        "Repository": "https://github.com/devtools-at/sitemap-generator",
+    },
+    packages=find_packages(),
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+)
