3fa288e65a4e96263bc6e51ca7398a1cfa2d57ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 18:05:22 +0000

    Add package lego-museum-structurizer/2025.12.20202603/lego_museum_structurizer-2025.12.20202603-py3-none-any.whl

---
+++ packages/lego-museum-structurizer/lego_museum_structurizer-2025.12.20202603-py3-none-any.whl/lego_museum_structurizer/__init__.py
@@ -0,0 +1,4 @@
+from .main import lego_museum_structurizer
+from .prompts import human_prompt, pattern, system_prompt
+
+__all__ = ["lego_museum_structurizer", "system_prompt", "human_prompt", "pattern"]
