450e8ba8a92cc2d38deee440a57b938c1878046f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 17:05:30 +0000

    Add package Orange3/3.40.0/orange3-3.40.0-cp311-cp311-macosx_10_9_x86_64.whl

---
+++ packages/Orange3/orange3-3.40.0-cp311-cp311-macosx_10_9_x86_64.whl/Orange/classification/knn.py
@@ -0,0 +1,10 @@
+import sklearn.neighbors as skl_neighbors
+from Orange.base import KNNBase
+from Orange.classification import SklLearner
+
+__all__ = ["KNNLearner"]
+
+
+class KNNLearner(KNNBase, SklLearner):
+    __wraps__ = skl_neighbors.KNeighborsClassifier
+    supports_weights = False
