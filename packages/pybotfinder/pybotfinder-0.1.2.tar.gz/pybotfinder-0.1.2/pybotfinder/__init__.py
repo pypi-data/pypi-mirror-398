e718779bb128f967fdae17c4a23ccd28adb50652
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 17:04:05 +0000

    Add package pybotfinder/0.1.2/pybotfinder-0.1.2.tar.gz

---
+++ packages/pybotfinder/pybotfinder-0.1.2.tar.gz/pybotfinder-0.1.2/pybotfinder/__init__.py
@@ -0,0 +1,25 @@
+"""
+pybotfinder - 微博社交机器人检测工具包
+
+基于随机森林的微博社交机器人检测系统，实现从数据采集到模型预测的完整流程。
+"""
+
+__version__ = "0.1.2"
+__author__ = "Xiao MENG"
+__email__ = "xiaomeng7-c@my.cityu.edu.hk"
+
+from .collector import WeiboCollector, get_profile, get_recent_weibos, crawl_account
+from .feature_extractor import FeatureExtractor
+from .model_trainer import ModelTrainer
+from .predictor import BotPredictor
+
+__all__ = [
+    "WeiboCollector",
+    "get_profile",
+    "get_recent_weibos",
+    "crawl_account",
+    "FeatureExtractor",
+    "ModelTrainer",
+    "BotPredictor",
+]
+
