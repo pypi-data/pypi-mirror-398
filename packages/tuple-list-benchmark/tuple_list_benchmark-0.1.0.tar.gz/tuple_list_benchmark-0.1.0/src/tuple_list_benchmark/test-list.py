cee2842e81f2f1894dd44f00c34f3b78fa4367fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 17:04:06 +0000

    Add package tuple-list-benchmark/0.1.0/tuple_list_benchmark-0.1.0.tar.gz

---
+++ packages/tuple-list-benchmark/tuple_list_benchmark-0.1.0.tar.gz/tuple_list_benchmark-0.1.0/src/tuple_list_benchmark/test-list.py
@@ -0,0 +1,10 @@
+import pyperf
+from tuple_list_benchmark.base import funcs
+
+if __name__ == "__main__":
+    runner = pyperf.Runner()
+    sizes = [10, 50, 100, 500, 1000, 10000, 100000]
+
+    for n in sizes:
+        for func in funcs['list']:
+            runner.bench_func(f'{func.__name__.split("_")[-1]}_{n}', func, n)
