02a513b8b1b44d79e8649e885f8bb5f3e2d05ced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 17:05:51 +0000

    Add package trading-sdk/0.2.0/trading_sdk-0.2.0.tar.gz

---
+++ packages/trading-sdk/trading_sdk-0.2.0.tar.gz/trading_sdk-0.2.0/src/trading_sdk/market/trading/__init__.py
@@ -0,0 +1,8 @@
+from typing_extensions import Protocol
+from .cancel_order import CancelOrder
+from .open_orders import OpenOrders
+from .place_order import PlaceOrder
+from .query_order import QueryOrder
+
+class Trading(CancelOrder, OpenOrders, PlaceOrder, QueryOrder, Protocol):
+  ...
---
+++ packages/trading-sdk/trading_sdk-0.2.0.tar.gz/trading_sdk-0.2.0/src/trading_sdk/market/types/__init__.pyi
@@ -0,0 +1,9 @@
+from .order import (
+  LimitOrder, MarketOrder, Order, Side,
+  OrderStatus, OrderState,
+)
+
+__all__ = [
+  'LimitOrder', 'MarketOrder', 'Order', 'Side',
+  'OrderStatus', 'OrderState',
+]
