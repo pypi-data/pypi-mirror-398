b4f97f9f36571a32b79b993c47c362ddb93fe45b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 20:04:47 +0000

    Add package devtools-regex-tester/1.0.1/devtools_regex_tester-1.0.1.tar.gz

---
+++ packages/devtools-regex-tester/devtools_regex_tester-1.0.1.tar.gz/devtools_regex_tester-1.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="devtools_regex_tester",
+    version="1.0.1",
+    description="Regex tester",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="DevTools.at",
+    author_email="hello@devtools.at",
+    url="https://devtools.at/tools/regex-tester",
+    project_urls={
+        "Homepage": "https://devtools.at/tools/regex-tester",
+        "Repository": "https://github.com/devtools-at/regex-tester",
+    },
+    packages=find_packages(),
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+)
