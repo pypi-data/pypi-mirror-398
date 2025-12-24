3dfd93359efffe8ac892d64e016744bbeb0d8ba8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 15:03:52 +0000

    Add package npygenx/0.2.0/npygenx-0.2.0-py3-none-any.whl

---
+++ packages/npygenx/npygenx-0.2.0-py3-none-any.whl/numpy/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+import argparse
+
+from .linear_equation_ga import run_linear_equation_ga
+from .tsp_ga import run_tsp_ga
+from .eight_queens_ga import run_eight_queens_ga
+from .graph_coloring_ga import run_graph_coloring_ga
+from .continuous_opt_ga import run_continuous_opt_ga
+from .knapsack_ga import run_knapsack_ga
+from .symbolic_gp import run_symbolic_gp
