018b91706982257a64b24e719fded45850b33781
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 19:05:27 +0000

    Add package turkishpythonV2/2.0.0/turkishpythonv2-2.0.0.tar.gz

---
+++ packages/turkishpythonV2/turkishpythonv2-2.0.0.tar.gz/turkishpythonv2-2.0.0/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="turkishpythonV2",
+    version="2.0.0",
+    packages=find_packages(),
+    description="Tam Türkçe Python (V2)",
+    license="MIT",
+    python_requires=">=3.7",
+)
