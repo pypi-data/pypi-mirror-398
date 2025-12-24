d3b3b68b052547791c00dc892c1917c53c3bb87c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 16:04:47 +0000

    Add package marearts-anpr/0.1220.1107/marearts_anpr-0.1220.1107-cp312-cp312-macosx_10_13_universal2.whl

---
+++ packages/marearts-anpr/marearts_anpr-0.1220.1107-cp312-cp312-macosx_10_13_universal2.whl/marearts_anpr/__init__.py
@@ -0,0 +1,26 @@
+"""MareArts ANPR - License Plate Detection and Recognition"""
+
+from ._version import __version__
+
+from .marearts_anpr import (
+    marearts_anpr_from_pil,
+    marearts_anpr_from_image_file, 
+    marearts_anpr_from_cv2
+)
+
+from .marearts_anpr_d import ma_anpr_detector
+from .marearts_anpr_d_v14 import ma_anpr_detector_v14
+from .marearts_anpr_r import ma_anpr_ocr
+from .marearts_anpr_p import validate_user_key, validate_user_key_with_signature
+
+__all__ = [
+    "__version__",
+    "marearts_anpr_from_pil",
+    "marearts_anpr_from_image_file", 
+    "marearts_anpr_from_cv2",
+    "ma_anpr_detector",
+    "ma_anpr_detector_v14",
+    "ma_anpr_ocr",
+    "validate_user_key",
+    "validate_user_key_with_signature"
+]
