2d6a1ab54e54e96262f281b32b6b7810041bee2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 18:05:23 +0000

    Add package autonomous-app/0.3.26/autonomous_app-0.3.26.tar.gz

---
+++ packages/autonomous-app/autonomous_app-0.3.26.tar.gz/autonomous_app-0.3.26/src/autonomous/ai/audioagent.py
@@ -0,0 +1,28 @@
+import filters
+from autonomous import log
+from autonomous.ai.baseagent import BaseAgent
+from autonomous.model.autoattr import StringAttr
+
+
+class AudioAgent(BaseAgent):
+    name = StringAttr(default="audioagent")
+    instructions = StringAttr(
+        default="You are highly skilled AI trained to assist with generating audio files."
+    )
+    description = StringAttr(
+        default="A helpful AI assistant trained to assist with generating audio files."
+    )
+
+    def generate(self, prompt, **kwargs):
+        return self.get_client().generate_audio(prompt, **kwargs)
+
+    ## DEPRECATED - use transcribe instead
+    def generate_text(self, audio, **kwargs):
+        log("AudioAgent.generate_text is deprecated; use transcribe instead.")
+        return self.get_client().generate_audio_text(audio, **kwargs)
+
+    def transcribe(self, audio, **kwargs):
+        return self.get_client().generate_audio_text(audio, **kwargs)
+
+    def available_voices(self, filters=[]):
+        return self.get_client().list_voices(filters=filters)
