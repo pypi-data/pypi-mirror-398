a81305f6185358175ced9e36bee3886be27487fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 17:05:44 +0000

    Add package genvarloader/0.19.1/genvarloader-0.19.1-pp310-pypy310_pp73-manylinux_2_17_s390x.manylinux2014_s390x.whl

---
+++ packages/genvarloader/genvarloader-0.19.1-pp310-pypy310_pp73-manylinux_2_17_s390x.manylinux2014_s390x.whl/genvarloader/_variants/_utils.py
@@ -0,0 +1,14 @@
+import re
+from pathlib import Path
+
+VCF_SUFFIX = re.compile(r"\.[vb]cf(\.(gz|bgz))?$")
+
+
+def path_is_vcf(path: str | Path) -> bool:
+    path = Path(path)
+    return VCF_SUFFIX.search(path.name) is not None
+
+
+def path_is_pgen(path: str | Path) -> bool:
+    path = Path(path)
+    return path.suffix == ".pgen" or Path(str(path) + ".pgen").exists()
