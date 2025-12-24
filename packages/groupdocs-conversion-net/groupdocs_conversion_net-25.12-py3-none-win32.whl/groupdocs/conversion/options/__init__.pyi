4977c64296bedd9bd5eef4bc7c91c1b8c9f2ef30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 15:03:51 +0000

    Add package groupdocs-conversion-net/25.12/groupdocs_conversion_net-25.12-py3-none-win32.whl

---
+++ packages/groupdocs-conversion-net/groupdocs_conversion_net-25.12-py3-none-win32.whl/groupdocs/conversion/options/__init__.pyi
@@ -0,0 +1,19 @@
+from typing import List, Optional, Dict, Iterable, Any, overload
+import io
+import collections.abc
+from collections.abc import Sequence
+from datetime import datetime
+from aspose.pyreflection import Type
+import aspose.pycore
+import aspose.pydrawing
+from uuid import UUID
+import groupdocs.conversion
+import groupdocs.conversion.caching
+import groupdocs.conversion.contracts
+import groupdocs.conversion.exceptions
+import groupdocs.conversion.filetypes
+import groupdocs.conversion.logging
+import groupdocs.conversion.options
+import groupdocs.conversion.options.convert
+import groupdocs.conversion.options.load
+
