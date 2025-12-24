3743df0ffbe83c234b970d08dee49e83d0c7794e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 20:05:55 +0000

    Add package moat-db/0.2.5/moat_db-0.2.5.tar.gz

---
+++ packages/moat-db/moat_db-0.2.5.tar.gz/moat_db-0.2.5/README.md
@@ -0,0 +1,8 @@
+# MoaT-DB
+
+% start synopsis
+
+This module handles storing stuff in a database. It depends on sqlalchemy
+and is **not yet** asynchronous.
+
+% end synopsis
---
+++ packages/moat-db/moat_db-0.2.5.tar.gz/moat_db-0.2.5/src/moat/db/__init__.py
@@ -0,0 +1,22 @@
+"""
+MoaT database supprt
+"""
+
+from __future__ import annotations
+
+
+def load(cfg):  # noqa:D103
+    from .util import load as load_  # noqa: PLC0415
+
+    return load_(cfg)
+
+
+def database(cfg):  # noqa:D103
+    from .util import database as database_  # noqa: PLC0415
+
+    return database_(cfg)
+
+
+from moat.util.config import CfgStore  # noqa:E402
+
+CfgStore.with_(__name__)
