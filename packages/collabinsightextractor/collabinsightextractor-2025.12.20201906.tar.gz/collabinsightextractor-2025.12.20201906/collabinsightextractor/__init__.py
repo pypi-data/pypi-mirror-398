be5ad8d4173178bdc62fbbf8bda26263a9e7ad9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 18:05:21 +0000

    Add package collabinsightextractor/2025.12.20201906/collabinsightextractor-2025.12.20201906.tar.gz

---
+++ packages/collabinsightextractor/collabinsightextractor-2025.12.20201906.tar.gz/collabinsightextractor-2025.12.20201906/collabinsightextractor/__init__.py
@@ -0,0 +1,4 @@
+from .main import collabinsightextractor
+from .prompts import human_prompt, pattern, system_prompt
+
+__all__ = ["collabinsightextractor", "system_prompt", "human_prompt", "pattern"]
