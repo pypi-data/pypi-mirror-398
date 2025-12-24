c51ae13ff77f615bcd82c6d849b70f84938f13d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 15:04:39 +0000

    Add package lk-moe/1.2.6/lk_moe-1.2.6-cp312-cp312-manylinux_2_34_x86_64.whl

---
+++ packages/lk-moe/lk_moe-1.2.6-cp312-cp312-manylinux_2_34_x86_64.whl/lk_moe/__init__.py
@@ -0,0 +1,16 @@
+from ._version import __version__, __version_tuple__
+ 
+from ._dynamic_loader import _load_library
+ 
+MOE, MOEConfig, MOE_FP8, MOE_FP8Config, MOE_WNA16, MOE_WNA16Config = _load_library()
+
+__all__ = [
+    "__version__",
+    "__version_tuple__",
+    "MOE",
+    "MOEConfig",
+    "MOE_FP8",
+    "MOE_FP8Config",
+    "MOE_WNA16",
+    "MOE_WNA16Config",
+]
---
+++ packages/lk-moe/lk_moe-1.2.6-cp312-cp312-manylinux_2_34_x86_64.whl/lk_moe/_version.py
@@ -0,0 +1,2 @@
+__version_tuple__ = (1, 2, 6)
+__version__ = "1.2.6"
