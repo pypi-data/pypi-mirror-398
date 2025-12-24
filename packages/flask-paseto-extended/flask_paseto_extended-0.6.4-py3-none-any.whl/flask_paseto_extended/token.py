cc0602d5a30537656bf3b6b71beb8eec14c949c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 19:06:22 +0000

    Add package flask-paseto-extended/0.6.4/flask_paseto_extended-0.6.4-py3-none-any.whl

---
+++ packages/flask-paseto-extended/flask_paseto_extended-0.6.4-py3-none-any.whl/flask_paseto_extended/token.py
@@ -0,0 +1,120 @@
+import typing as t
+from functools import wraps
+
+from flask import current_app, has_request_context
+from flask.globals import request_ctx
+from werkzeug.local import LocalProxy
+
+from .exceptions import ConfigError
+
+
+class Token:
+    """
+    The parsed token object which can be referred as ``current_paseto`` in the
+    views decorated by ``@token_required``.
+    """
+
+    def __init__(
+        self,
+        is_verified: bool,
+        version: str = "",
+        purpose: str = "",
+        payload: bytes | dict = b"",
+        footer: bytes | dict = b"",
+        error: Exception | None = None,
+    ):
+        self._is_verified = is_verified
+        self._version = version
+        self._purpose = purpose
+        self._payload = payload
+        self._footer = footer
+        self._error = error
+        self._header = (version + "." + purpose + ".").encode("utf-8")
+        return
+
+    @property
+    def is_verified(self) -> bool:
+        """
+        If it is ``True``, the token has been verified.
+        """
+        return self._is_verified
+
+    @property
+    def version(self) -> str:
+        """
+        The version of the token. It will be ``"v1"``, ``"v2"``, ``"v3"`` or ``"v4"``.
+        """
+        return self._version
+
+    @property
+    def purpose(self) -> str:
+        """
+        The purpose of the token. It will be ``"local"`` or ``"public"``.
+        """
+        return self._purpose
+
+    @property
+    def header(self) -> bytes:
+        """
+        The header of the token. It will be ``"<version>.<type>."``.
+        For example, ``"v1.local."``.
+        """
+        return self._header
+
+    @property
+    def payload(self) -> bytes | dict:
+        """
+        The payload of the token which is a decoded binary string. It's not Base64 encoded data.
+        """
+        return self._payload
+
+    @property
+    def footer(self) -> bytes | dict:
+        """
+        The footer of the token which is a decoded binary string. It's not Base64 encoded data.
+        """
+        return self._footer
+
+    @property
+    def error(self) -> Exception | None:
+        """
+        If ``is_verified`` is ``True``, this property will be specified.
+        """
+        return self._error
+
+
+current_paseto: LocalProxy[Token] = LocalProxy(lambda: _get_token())
+
+
+def paseto_required():
+    """ """
+
+    def wrapper(func):
+        @wraps(func)
+        def decorated_view(*args, **kwargs):
+            if not hasattr(current_app, "paseto_verifier"):
+                raise ConfigError("paseto_verifier is not set in the current_app.")
+
+            if not t.cast(Token, current_paseto).is_verified:
+                return current_app.paseto_verifier.verification_error_handler_callback()
+
+            if not hasattr(current_app, "ensure_sync"):
+                return func(*args, **kwargs)
+            # ensure_sync is available in Flask >= 2.0
+            return current_app.ensure_sync(func)(*args, **kwargs)
+
+        return decorated_view
+
+    return wrapper
+
+
+def _get_token() -> Token:
+    """ """
+    if has_request_context() and not hasattr(request_ctx, "paseto"):
+        verifier = getattr(current_app, "paseto_verifier", None)
+        if verifier is not None:
+            verifier._load_and_verify()
+    token = getattr(request_ctx, "paseto", None)
+    if isinstance(token, Token):
+        return token
+    return Token(False)
