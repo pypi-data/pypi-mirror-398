1244f40bfc64b865eb4822bcf17abc1fafa28afe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 11:05:35 +0000

    Add package ry/0.0.75/ry-0.0.75-cp314-cp314t-musllinux_1_2_aarch64.whl

---
+++ packages/ry/ry-0.0.75-cp314-cp314t-musllinux_1_2_aarch64.whl/ry/ryo3/_which.pyi
@@ -0,0 +1,9 @@
+"""ryo3-which types"""
+
+from pathlib import Path
+
+from ry.ryo3._regex import Regex
+
+def which(cmd: str, path: None | str = None) -> Path | None: ...
+def which_all(cmd: str, path: None | str = None) -> list[Path]: ...
+def which_re(regex: str | Regex, path: None | str = None) -> list[Path]: ...
