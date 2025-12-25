818da461db9a7ae18dfc6ac024aa9291433c9bcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 07:06:28 +0000

    Add package essen_BJS/0.2.0/essen_bjs-0.2.0.tar.gz

---
+++ packages/essen_BJS/essen_bjs-0.2.0.tar.gz/essen_bjs-0.2.0/essen_BJS/mult.py
@@ -0,0 +1,19 @@
+from .spec_err import *
+import sys
+
+def mult(*numbers):
+  try:
+    if not numbers:
+        mathy__BJS___NullfuncError()
+    
+    total = numbers[0]
+    for num in numbers[1:]:
+        if not isinstance(num, (float, int)):
+            mathy__BJS___TypeError()
+        total *= num
+    
+    return total
+  except OverflowError:
+     mathy__BJS___OverboundError()
+  except Exception as err:
+     raise err
