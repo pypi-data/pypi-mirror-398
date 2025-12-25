5323f73f85850fc0e157c98c55a643f3f7d9a3de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 06:05:48 +0000

    Add package eqfm/0.1.0/eqfm-0.1.0.tar.gz

---
+++ packages/eqfm/eqfm-0.1.0.tar.gz/eqfm-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="eqfm",
+    version="0.1.0",
+    long_description="eqfm",
+    long_description_content_type="text/markdown",
+    packages=["eqfm"],
+    install_requires=["numpy",  "h5py", "matplotlib", "pandas"],
+)
