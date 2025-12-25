49d5d9ef9d7500901fd7de1cdce5907186dee5fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 07:06:14 +0000

    Add package TurnPDFtoText/1.0/turnpdftotext-1.0-py3-none-any.whl

---
+++ packages/TurnPDFtoText/turnpdftotext-1.0-py3-none-any.whl/attachpdf/pdf2text.py
@@ -0,0 +1,14 @@
+"""This module provides functions to convert a PDF to text"""
+
+
+def convert(path):
+    """ 
+    Convert the given PDF to text.
+
+    Parameters:
+    path (str): path of the PDF
+
+    Returns: 
+    str: The content in the PDF file
+    """
+    print("pdf2text")
