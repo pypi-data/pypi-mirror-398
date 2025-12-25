0d5c0c23ad3dd60a70136d81854b7122c49d218a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 11:05:08 +0000

    Add package easy-csv-logging/0.1.0/easy_csv_logging-0.1.0.tar.gz

---
+++ packages/easy-csv-logging/easy_csv_logging-0.1.0.tar.gz/easy_csv_logging-0.1.0/test_logging.py
@@ -0,0 +1,27 @@
+from easy_csv_logging import log,view_log
+
+
+
+log("a",1,"b",3) 
+log("2")
+log()
+log()
+log()
+log("patrik",999)
+log("patrik",999)
+log("patrik",999)
+log("patrik","999öööööööö")
+log("29999")
+log("29999")
+log("29999")
+log()
+log()
+log()
+log((2,2),(3,3),(4,4))
+log(["2",2],["3",3],["4",4])
+log([("2",2),("3",3),("4",4)])
+
+
+
+
+view_log()
