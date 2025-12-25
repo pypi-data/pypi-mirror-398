ad547f9befd034d0f6cc63ecec6f713be1bec27f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 03:32:08 +0000

    Add package chronos-forecaster/0.2.1/chronos_forecaster-0.2.1.tar.gz

---
+++ packages/chronos-forecaster/chronos_forecaster-0.2.1.tar.gz/chronos_forecaster-0.2.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="chronos_forecaster",
+    version="0.2.1",
+    description="Unified interface for Amazon Chronos and Chronos-2 time series forecasting engines.",
+    author="Sebastião Santos Lessa",
+    author_email="sebastiao.lessa@inesctec.pt",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        "chronos-forecasting",
+        "setuptools",
+        "pandas",
+        "torch",
+    ], 
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+)
