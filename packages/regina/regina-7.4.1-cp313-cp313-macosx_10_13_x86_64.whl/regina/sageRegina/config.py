68d826e0ce46e6e35c7afc54d269473baf5e700c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 12:06:40 +0000

    Add package regina/7.4.1/regina-7.4.1-cp313-cp313-macosx_10_13_x86_64.whl

---
+++ packages/regina/regina-7.4.1-cp313-cp313-macosx_10_13_x86_64.whl/regina/sageRegina/config.py
@@ -0,0 +1,9 @@
+tokyocabinet_uri  = 'https://dbmx.net/tokyocabinet/tokyocabinet-1.4.48.tar.gz'
+tokyocabinet_dir  = 'tokyocabinet-1.4.48'
+
+libxml_uri        = 'http://xmlsoft.org/sources/libxml2-2.9.3.tar.gz'
+libxml_dir        = 'libxml2-2.9.3'
+
+regina_hash      = '219d658e5e51cc003807fc8'  # regina-7.4.1
+regina_uri       = 'https://github.com/regina-normal/regina.git'
+regina_dir       = 'regina_%s' % regina_hash
