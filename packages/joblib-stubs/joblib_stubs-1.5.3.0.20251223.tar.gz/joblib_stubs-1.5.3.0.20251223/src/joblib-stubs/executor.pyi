ee0b8950db5520a7aa1005590114107a222f68ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 12:06:01 +0000

    Add package joblib-stubs/1.5.3.0.20251223/joblib_stubs-1.5.3.0.20251223.tar.gz

---
+++ packages/joblib-stubs/joblib_stubs-1.5.3.0.20251223.tar.gz/joblib_stubs-1.5.3.0.20251223/src/joblib-stubs/executor.pyi
@@ -0,0 +1,39 @@
+from collections.abc import Callable, Iterable
+from concurrent import futures
+from typing import Any
+
+from joblib._memmapping_reducer import (
+    TemporaryResourcesManager as TemporaryResourcesManager,
+)
+from joblib._memmapping_reducer import (
+    get_memmapping_reducers as get_memmapping_reducers,
+)
+from joblib.externals.loky.reusable_executor import _ReusablePoolExecutor
+from typing_extensions import TypeVar
+
+_T = TypeVar("_T")
+
+def get_memmapping_executor(n_jobs: int, **kwargs: Any) -> futures.Executor: ...
+
+class MemmappingExecutor(_ReusablePoolExecutor):
+    @classmethod
+    def get_memmapping_executor(
+        cls,
+        n_jobs: int,
+        timeout: float = ...,
+        initializer: Callable[..., Any] | None = ...,
+        initargs: tuple[Any, ...] = ...,
+        env: dict[str, str] | None = ...,
+        temp_folder: str | None = ...,
+        context_id: tuple[str, ...] | None = ...,
+        **backend_args: Any,
+    ) -> futures.Executor: ...
+    def terminate(self, kill_workers: bool = ...) -> None: ...
+
+class _TestingMemmappingExecutor(MemmappingExecutor):
+    def apply_async(
+        self, func: Callable[..., _T], args: tuple[Any, ...]
+    ) -> futures.Future[_T]: ...
+    def map(  # pyright: ignore[reportIncompatibleMethodOverride]
+        self, f: Callable[..., _T], *args: Iterable[Any]
+    ) -> list[_T]: ...
---
+++ packages/joblib-stubs/joblib_stubs-1.5.3.0.20251223.tar.gz/joblib_stubs-1.5.3.0.20251223/src/joblib-stubs/externals/loky/backend/_win_reduction.pyi
@@ -0,0 +1,1 @@
+from joblib.externals.loky.backend.reduction import register as register
