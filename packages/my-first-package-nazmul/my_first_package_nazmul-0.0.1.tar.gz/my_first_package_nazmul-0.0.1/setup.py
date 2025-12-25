6287b04d5426a61656f045d75d665f3d878a1690
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 05:06:22 +0000

    Add package my-first-package-nazmul/0.0.1/my_first_package_nazmul-0.0.1.tar.gz

---
+++ packages/my-first-package-nazmul/my_first_package_nazmul-0.0.1.tar.gz/my_first_package_nazmul-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    description = fh.read()
+
+setuptools.setup(
+    name="my_first_package_nazmul",
+    version="0.0.1",
+    author="Nazmul Islam",
+    author_email="nazmulislam45213@gmail.com",
+    description="A small example package",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Nazmul0005/my-first-package.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
