5be8dfe81dc0f58c6fc98a002e70863fe18ad76a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 03:31:53 +0000

    Add package pre-v7-unix-summarizer/2025.12.21164816/pre_v7_unix_summarizer-2025.12.21164816.tar.gz

---
+++ packages/pre-v7-unix-summarizer/pre_v7_unix_summarizer-2025.12.21164816.tar.gz/pre_v7_unix_summarizer-2025.12.21164816/pre_v7_unix_summarizer/__init__.py
@@ -0,0 +1,4 @@
+from .main import pre_v7_unix_summarizer
+from .prompts import human_prompt, pattern, system_prompt
+
+__all__ = ["pre_v7_unix_summarizer", "system_prompt", "human_prompt", "pattern"]
