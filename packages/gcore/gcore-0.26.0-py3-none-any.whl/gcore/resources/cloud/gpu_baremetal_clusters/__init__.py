5bb0952a0cc3145e6d3eb6ef9d0e679836fb497b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 13:15:18 +0000

    Add package gcore/0.26.0/gcore-0.26.0-py3-none-any.whl

---
+++ packages/gcore/gcore-0.26.0-py3-none-any.whl/gcore/resources/cloud/gpu_baremetal_clusters/__init__.py
@@ -0,0 +1,75 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .images import (
+    ImagesResource,
+    AsyncImagesResource,
+    ImagesResourceWithRawResponse,
+    AsyncImagesResourceWithRawResponse,
+    ImagesResourceWithStreamingResponse,
+    AsyncImagesResourceWithStreamingResponse,
+)
+from .flavors import (
+    FlavorsResource,
+    AsyncFlavorsResource,
+    FlavorsResourceWithRawResponse,
+    AsyncFlavorsResourceWithRawResponse,
+    FlavorsResourceWithStreamingResponse,
+    AsyncFlavorsResourceWithStreamingResponse,
+)
+from .servers import (
+    ServersResource,
+    AsyncServersResource,
+    ServersResourceWithRawResponse,
+    AsyncServersResourceWithRawResponse,
+    ServersResourceWithStreamingResponse,
+    AsyncServersResourceWithStreamingResponse,
+)
+from .interfaces import (
+    InterfacesResource,
+    AsyncInterfacesResource,
+    InterfacesResourceWithRawResponse,
+    AsyncInterfacesResourceWithRawResponse,
+    InterfacesResourceWithStreamingResponse,
+    AsyncInterfacesResourceWithStreamingResponse,
+)
+from .gpu_baremetal_clusters import (
+    GPUBaremetalClustersResource,
+    AsyncGPUBaremetalClustersResource,
+    GPUBaremetalClustersResourceWithRawResponse,
+    AsyncGPUBaremetalClustersResourceWithRawResponse,
+    GPUBaremetalClustersResourceWithStreamingResponse,
+    AsyncGPUBaremetalClustersResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "InterfacesResource",
+    "AsyncInterfacesResource",
+    "InterfacesResourceWithRawResponse",
+    "AsyncInterfacesResourceWithRawResponse",
+    "InterfacesResourceWithStreamingResponse",
+    "AsyncInterfacesResourceWithStreamingResponse",
+    "ServersResource",
+    "AsyncServersResource",
+    "ServersResourceWithRawResponse",
+    "AsyncServersResourceWithRawResponse",
+    "ServersResourceWithStreamingResponse",
+    "AsyncServersResourceWithStreamingResponse",
+    "FlavorsResource",
+    "AsyncFlavorsResource",
+    "FlavorsResourceWithRawResponse",
+    "AsyncFlavorsResourceWithRawResponse",
+    "FlavorsResourceWithStreamingResponse",
+    "AsyncFlavorsResourceWithStreamingResponse",
+    "ImagesResource",
+    "AsyncImagesResource",
+    "ImagesResourceWithRawResponse",
+    "AsyncImagesResourceWithRawResponse",
+    "ImagesResourceWithStreamingResponse",
+    "AsyncImagesResourceWithStreamingResponse",
+    "GPUBaremetalClustersResource",
+    "AsyncGPUBaremetalClustersResource",
+    "GPUBaremetalClustersResourceWithRawResponse",
+    "AsyncGPUBaremetalClustersResourceWithRawResponse",
+    "GPUBaremetalClustersResourceWithStreamingResponse",
+    "AsyncGPUBaremetalClustersResourceWithStreamingResponse",
+]
