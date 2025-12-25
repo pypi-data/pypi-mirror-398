d5092ab8da026775930a4b9efe98cfa578d6a8be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 12:06:02 +0000

    Add package Products.validation/4.0.0/products_validation-4.0.0-py3-none-any.whl

---
+++ packages/Products.validation/products_validation-4.0.0-py3-none-any.whl/Products/validation/exceptions.py
@@ -0,0 +1,14 @@
+class ValidatorError(Exception):
+    pass
+
+
+class UnknowValidatorError(ValidatorError):
+    pass
+
+
+class FalseValidatorError(ValidatorError):
+    pass
+
+
+class AlreadyRegisteredValidatorError(ValidatorError):
+    pass
