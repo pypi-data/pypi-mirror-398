e764931b4830d03efb15bedfae15817260016097
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 07:06:16 +0000

    Add package nijigen-backtrader/1.9.78.124/nijigen_backtrader-1.9.78.124-py3-none-any.whl

---
+++ packages/nijigen-backtrader/nijigen_backtrader-1.9.78.124-py3-none-any.whl/backtrader/__init__.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; py-indent-offset:4 -*-
+###############################################################################
+#
+# Copyright (C) 2015-2023 Daniel Rodriguez
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from . import analyzers as analyzers
+from . import broker as broker
+from . import brokers as brokers
+from . import commissions as commissions
+from . import commissions as comms
+from . import errors as errors
+from . import feeds as feeds
+from . import filters as filters
+from . import indicators as ind
+from . import indicators as indicators
+from . import observers as obs
+from . import observers as observers
+from . import signals as signals
+from . import sizers as sizers
+from . import stores as stores
+from . import strategies as strategies
+from . import strategies as strats
+from . import studies as studies
+from . import talib as talib
+from . import timer as timer
+from . import utils as utils
+from .analyzer import *
+from .broker import *
+from .cerebro import *
+from .comminfo import *
+from .dataseries import *
+from .errors import *
+from .feed import *
+from .flt import *
+from .functions import *
+from .indicator import *
+from .linebuffer import *
+from .lineiterator import *
+from .lineseries import *
+from .observer import *
+from .order import *
+from .position import *
+from .resamplerfilter import *
+from .signal import *
+from .sizer import *
+from .sizers import SizerFix  # old sizer for compatibility
+from .store import Store
+from .strategy import *
+from .timer import *
+from .trade import *
+from .utils import date2num, num2date, num2time, time2num
+from .version import __btversion__, __version__
+from .writer import *
+
+# Load contributed indicators and studies (must be at end to avoid circular imports)
+from . import indicators as _  # noqa: ensure indicators module is loaded
+from . import studies as _  # noqa: ensure studies module is loaded
+import backtrader.indicators.contrib
+import backtrader.studies.contrib
