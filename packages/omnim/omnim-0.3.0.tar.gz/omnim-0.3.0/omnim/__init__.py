2e994304fbb109895868dea33c746510292c4bf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 07:06:25 +0000

    Add package omnim/0.3.0/omnim-0.3.0.tar.gz

---
+++ packages/omnim/omnim-0.3.0.tar.gz/omnim-0.3.0/omnim/__init__.py
@@ -0,0 +1,15 @@
+from . import delegate
+from . import rx
+from . import nullable
+from . import readonly
+from . import linq
+from . import task
+
+__all__ = [
+    "delegate",
+    "rx",
+    "nullable",
+    "readonly",
+    "linq",
+    "task",
+]
---
+++ packages/omnim/omnim-0.3.0.tar.gz/omnim-0.3.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="omnim",
+    url="https://github.com/cet-t/omnim",
+    version="0.3.0",
+    description="useful toolkit",
+    author="cet",
+    packages=["omnim"],
+    include_package_data=True,
+    zip_safe=True,
+)
