504819ab7b65b88d553b6054e40e0f7ebfd5e98d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 03:31:41 +0000

    Add package conflict-zone-extractor/2025.12.21180055/conflict_zone_extractor-2025.12.21180055.tar.gz

---
+++ packages/conflict-zone-extractor/conflict_zone_extractor-2025.12.21180055.tar.gz/conflict_zone_extractor-2025.12.21180055/conflict_zone_extractor/__init__.py
@@ -0,0 +1,4 @@
+from .main import conflict_zone_extractor
+from .prompts import human_prompt, pattern, system_prompt
+
+__all__ = ["conflict_zone_extractor", "system_prompt", "human_prompt", "pattern"]
