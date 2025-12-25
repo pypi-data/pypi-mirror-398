4a32065b2cfd937b5bb3a7f849fa9d797b35b031
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 11:05:16 +0000

    Add package pycdo/0.1.3/pycdo-0.1.3-py2.py3-none-any.whl

---
+++ packages/pycdo/pycdo-0.1.3-py2.py3-none-any.whl/pycdo/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2025-present Elio Campitelli <eliocampitelli@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+from .cdo_operation import cdo
+from .cdo_options import cdo_options
+from .cdo_cache import cdo_cache
