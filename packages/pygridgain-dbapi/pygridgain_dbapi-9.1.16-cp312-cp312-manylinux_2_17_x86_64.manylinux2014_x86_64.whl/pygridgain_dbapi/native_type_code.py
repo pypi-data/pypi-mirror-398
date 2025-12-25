16c49724d7253550f56220bbc413eee6a7b360ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 13:15:10 +0000

    Add package pygridgain-dbapi/9.1.16/pygridgain_dbapi-9.1.16-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pygridgain-dbapi/pygridgain_dbapi-9.1.16-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pygridgain_dbapi/native_type_code.py
@@ -0,0 +1,46 @@
+# Copyright (C) GridGain Systems. All Rights Reserved.
+# _________        _____ __________________        _____
+# __  ____/___________(_)______  /__  ____/______ ____(_)_______
+# _  / __  __  ___/__  / _  __  / _  / __  _  __ `/__  / __  __ \
+# / /_/ /  _  /    _  /  / /_/ /  / /_/ /  / /_/ / _  /  _  / / /
+# \____/   /_/     /_/   \_,__/   \____/   \__,_/  /_/   /_/ /_/
+
+NIL = 0
+
+BOOLEAN = 1
+
+INT8 = 2
+
+INT16 = 3
+
+INT32 = 4
+
+INT64 = 5
+
+FLOAT = 6
+
+DOUBLE = 7
+
+DECIMAL = 8
+
+DATE = 9
+
+TIME = 10
+
+DATETIME = 11
+
+TIMESTAMP = 12
+
+UUID = 13
+
+BITMASK = 14
+
+STRING = 15
+
+BYTE_ARRAY = 16
+
+PERIOD = 17
+
+DURATION = 18
+
+NUMBER = 19
