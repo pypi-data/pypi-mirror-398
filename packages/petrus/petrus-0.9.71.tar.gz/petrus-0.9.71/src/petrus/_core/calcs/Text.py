ba0f30a5b91d9e6f3f1a9bae1f3a9437e34478bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 06:05:15 +0000

    Add package petrus/0.9.71/petrus-0.9.71.tar.gz

---
+++ packages/petrus/petrus-0.9.71.tar.gz/petrus-0.9.71/src/petrus/_core/calcs/Text.py
@@ -0,0 +1,81 @@
+from typing import *
+
+from .BaseCalc import BaseCalc
+
+__all__ = ["Text"]
+
+
+class Text(BaseCalc):
+    def __getattr__(self: Self, name: Any) -> Any:
+        ans: Any
+        name_: str
+        name_ = str(name)
+        if hasattr(type(self), name_):
+            return object.__getattribute__(self, name_)
+        if name_.startswith("_"):
+            raise AttributeError(name_)
+        if name_ in self._lock:
+            raise Exception
+        self._lock.add(name_)
+        try:
+            ans = self._calc(name_)
+            object.__setattr__(self, name_, ans)
+        finally:
+            self._lock.remove(name_)
+        return ans
+
+    def __post_init__(self: Self) -> None:
+        self._lock = set()
+
+    def _calc(self: Self, name: Any) -> Any:
+        file: Any
+        lines: Optional[list]
+        method: Callable
+        stream: Any
+        file = getattr(self.prog.file, name)
+        try:
+            with open(file, "r") as stream:
+                lines = stream.readlines()
+        except FileNotFoundError:
+            lines = None
+        if lines is not None:
+            lines = list(map(str.rstrip, lines))
+            lines = "\n".join(lines)
+            return lines
+        try:
+            method = getattr(self, "_calc_" + name)
+        except Exception:
+            method = str
+        return method()
+
+    def _calc_core(self: Self) -> Any:
+        return self.prog.draft.getitem("core").format(
+            project=self.prog.project.name,
+        )
+
+    def _calc_gitignore(self: Self) -> Any:
+        return self.prog.draft.getitem("gitignore")
+
+    def _calc_init(self: Self) -> Any:
+        return self.prog.draft.getitem("init").format(
+            project=self.prog.project.name,
+        )
+
+    def _calc_license(self: Self) -> Any:
+        return self.prog.draft.getitem("license").format(
+            year=self.prog.year,
+            author=self.prog.author[0],
+        )
+
+    def _calc_main(self: Self) -> Any:
+        return self.prog.draft.getitem("main").format(
+            project=self.prog.project.name,
+        )
+
+    def _calc_manifest(self: Self) -> Any:
+        return self.prog.draft.getitem("manifest").format(
+            project=self.prog.project.name,
+        )
+
+    def _calc_readme(self: Self) -> Any:
+        return self.prog.block.text
