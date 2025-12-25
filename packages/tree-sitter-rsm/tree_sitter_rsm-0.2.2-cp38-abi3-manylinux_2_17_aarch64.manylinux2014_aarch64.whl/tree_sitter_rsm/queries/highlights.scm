cb287df45ea0094df3ac7d9993e8af828b6857c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 06:05:22 +0000

    Add package tree-sitter-rsm/0.2.2/tree_sitter_rsm-0.2.2-cp38-abi3-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/tree-sitter-rsm/tree_sitter_rsm-0.2.2-cp38-abi3-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/tree_sitter_rsm/queries/highlights.scm
@@ -0,0 +1,11 @@
+;; Sections
+(source_file
+ ":rsm:" @punctuation
+ title: (text) @heading
+ "::" @punctuation)
+(section
+ title: (text) @heading)
+(subsection
+ title: (text) @heading)
+(subsubsection
+ title: (text) @heading)
