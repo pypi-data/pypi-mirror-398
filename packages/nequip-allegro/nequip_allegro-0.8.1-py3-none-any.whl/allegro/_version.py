db105178a1645976f4a36259962bae59b4a12caf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 11:05:14 +0000

    Add package nequip-allegro/0.8.1/nequip_allegro-0.8.1-py3-none-any.whl

---
+++ packages/nequip-allegro/nequip_allegro-0.8.1-py3-none-any.whl/allegro/_version.py
@@ -0,0 +1,6 @@
+# This file is a part of the `allegro` package. Please see LICENSE and README at the root for information on using it.
+# Allegro package version file
+# See Python packaging guide
+# https://packaging.python.org/guides/single-sourcing-package-version/
+
+__version__ = "0.8.1"
