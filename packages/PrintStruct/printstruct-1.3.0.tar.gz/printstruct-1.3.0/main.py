a634ae1508d4fd6517e04aa4d49edaf2c2d9e978
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 07:06:28 +0000

    Add package PrintStruct/1.3.0/printstruct-1.3.0.tar.gz

---
+++ packages/PrintStruct/printstruct-1.3.0.tar.gz/printstruct-1.3.0/main.py
@@ -0,0 +1,72 @@
+# main.py
+from __future__ import annotations
+import sys, os, io, pyperclip
+if sys.platform.startswith('win'):      # fix windows unicode error on CI
+    sys.stdout.reconfigure(encoding='utf-8')
+
+from pathlib import Path
+from services.draw_tree import draw_tree
+from services.zip_project import zip_project
+from services.parser import parse_args
+from utilities.utils import get_project_version
+
+
+def main() -> None:
+    args = parse_args()
+
+    if args.version:
+        print(get_project_version())
+        return
+
+    root = Path(args.path).resolve()
+    if not root.exists():
+        print(f"Error: path not found: {root}", file=sys.stderr)
+        raise SystemExit(1)
+
+    # If --no-limit is set, disable max_items
+    max_items = None if args.no_limit else args.max_items
+
+    if args.output is not None:     # TODO: relocate this code for file output
+        # Determine filename
+        filename = args.output
+        if not filename.endswith(('.txt', '.md')):
+            filename += '.txt'
+
+    if args.copy or args.output is not None:
+        # Capture stdout
+        output_buffer = io.StringIO()
+        original_stdout = sys.stdout
+        sys.stdout = output_buffer
+
+    # if zipping is requested
+    if args.zip is not None:
+        zip_project(
+            root=root,
+            zip_stem=args.zip,
+            show_all=args.all,
+            extra_ignores=args.ignore,
+            respect_gitignore=not args.no_gitignore,
+            gitignore_depth=args.gitignore_depth,
+            max_depth=args.max_depth,
+        )
+    else:       # else, print the tree normally
+        draw_tree(
+            root=root,
+            max_depth=args.max_depth,
+            show_all=args.all,
+            extra_ignores=args.ignore,
+            respect_gitignore=not args.no_gitignore,
+            gitignore_depth=args.gitignore_depth,
+            max_items=max_items,
+        )
+        if args.output is not None:     # that file output code again
+            # Write to file
+            with open(filename, 'w', encoding='utf-8') as f:
+                f.write(output_buffer.getvalue())
+
+        if args.copy:       # Capture output if needed for clipboard
+            pyperclip.copy(output_buffer.getvalue() + "\n")
+
+
+if __name__ == "__main__":
+    main()
