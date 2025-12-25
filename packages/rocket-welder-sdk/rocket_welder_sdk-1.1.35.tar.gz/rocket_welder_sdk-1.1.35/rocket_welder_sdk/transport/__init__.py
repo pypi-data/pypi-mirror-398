b4e4df825f8639e7e999c9e131686ee7627818aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 03:31:52 +0000

    Add package rocket-welder-sdk/1.1.35/rocket_welder_sdk-1.1.35.tar.gz

---
+++ packages/rocket-welder-sdk/rocket_welder_sdk-1.1.35.tar.gz/rocket_welder_sdk-1.1.35/rocket_welder_sdk/transport/__init__.py
@@ -0,0 +1,30 @@
+"""
+Transport layer for RocketWelder SDK.
+
+Provides transport-agnostic frame sink/source abstractions for protocols.
+"""
+
+from .frame_sink import IFrameSink
+from .frame_source import IFrameSource
+from .nng_transport import NngFrameSink, NngFrameSource
+from .stream_transport import StreamFrameSink, StreamFrameSource
+from .tcp_transport import TcpFrameSink, TcpFrameSource
+from .unix_socket_transport import (
+    UnixSocketFrameSink,
+    UnixSocketFrameSource,
+    UnixSocketServer,
+)
+
+__all__ = [
+    "IFrameSink",
+    "IFrameSource",
+    "NngFrameSink",
+    "NngFrameSource",
+    "StreamFrameSink",
+    "StreamFrameSource",
+    "TcpFrameSink",
+    "TcpFrameSource",
+    "UnixSocketFrameSink",
+    "UnixSocketFrameSource",
+    "UnixSocketServer",
+]
