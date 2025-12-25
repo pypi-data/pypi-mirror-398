99fbb1ffa1426f59976c441ea80399d47f0c8649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 05:06:32 +0000

    Add package ipyplines/0.0.1/ipyplines-0.0.1-py2.py3-none-any.whl

---
+++ packages/ipyplines/ipyplines-0.0.1-py2.py3-none-any.whl/pypelines/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Pypelines: pipeline syntax for IPython and Jupyter.
+Just run `%load_ext pypelines` to begin using powerful new pipe operators, placeholder syntax, and more.
+"""
+
+from . import _version  # noqa: E402
+__version__ = _version.get_versions()['version']
