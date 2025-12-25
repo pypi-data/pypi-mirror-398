c46e0c968de2685b0152d587ac719a4229b823e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 03:31:52 +0000

    Add package kuaijs-ios/0.2.50/kuaijs_ios-0.2.50.tar.gz

---
+++ packages/kuaijs-ios/kuaijs_ios-0.2.50.tar.gz/kuaijs_ios-0.2.50/kuaijs/utils.py
@@ -0,0 +1,16 @@
+"""工具函数"""
+
+
+def timeFormat(format: str) -> str:
+    """时间格式化（如 "yyyy-MM-dd HH:mm:ss"）"""
+    return ""
+
+
+def random(min: int, max: int) -> int:
+    """生成随机数（闭区间）"""
+    return min
+
+
+def takeMeToFront() -> bool:
+    """将应用切到前台"""
+    return True
