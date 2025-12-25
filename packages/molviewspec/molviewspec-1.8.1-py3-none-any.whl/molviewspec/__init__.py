2bc7dea789ed8590ad1ab3d55d754f89bc56b1ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 06:05:18 +0000

    Add package molviewspec/1.8.1/molviewspec-1.8.1-py3-none-any.whl

---
+++ packages/molviewspec/molviewspec-1.8.1-py3-none-any.whl/molviewspec/__init__.py
@@ -0,0 +1,23 @@
+"""MolViewSpec"""
+
+__version__ = "1.8.1"
+
+from molviewspec.builder import create_builder
+from molviewspec.molstar_widgets import molstar_html, molstar_notebook, molstar_streamlit
+from molviewspec.mvsx_converter import mvsj_to_mvsx
+from molviewspec.nodes import (
+    MVSJ,
+    MVSX,
+    CategoricalPalette,
+    ComponentExpression,
+    ContinuousPalette,
+    DiscretePalette,
+    GlobalMetadata,
+    MVSData,
+    PrimitiveComponentExpressions,
+    Snapshot,
+    SnapshotMetadata,
+    State,
+    States,
+    validate_state_tree,
+)
