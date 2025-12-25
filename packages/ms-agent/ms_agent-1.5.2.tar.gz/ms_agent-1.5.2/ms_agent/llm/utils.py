d1371e07d42b8c70ecb2384e989f57a2281650b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 04:16:43 +0000

    Add package ms-agent/1.5.2/ms_agent-1.5.2.tar.gz

---
+++ packages/ms-agent/ms_agent-1.5.2.tar.gz/ms_agent-1.5.2/ms_agent/llm/utils.py
@@ -0,0 +1,80 @@
+# Copyright (c) Alibaba, Inc. and its affiliates.
+from dataclasses import asdict, dataclass, field
+from typing import Any, Dict, List, Optional, Union
+
+import json
+from typing_extensions import Literal, Required, TypedDict
+
+
+class ToolCall(TypedDict, total=False):
+    id: str = 'default_id'
+    index: int = 0
+    type: str = 'function'
+    tool_name: str = ''
+    arguments: str = '{}'
+
+
+class Tool(TypedDict, total=False):
+    server_name: str = None
+
+    tool_name: Required[str]
+
+    description: Required[str]
+
+    parameters: Dict[str, Any] = dict()
+
+
+@dataclass
+class Message:
+    role: Literal['system', 'user', 'assistant', 'tool']
+
+    content: Union[str, List[Dict[str, str]]] = ''
+
+    tool_calls: List[ToolCall] = field(default_factory=list)
+
+    tool_call_id: Optional[str] = None
+
+    name: Optional[str] = None
+
+    # needed for output
+    reasoning_content: str = ''
+
+    # request id
+    id: str = ''
+
+    # continue generation mode
+    partial: bool = False
+    prefix: bool = False
+
+    # usage
+    completion_tokens: int = 0
+    prompt_tokens: int = 0
+    api_calls: int = 1
+
+    def to_dict(self):
+        return asdict(self)
+
+    def to_dict_clean(self):
+        raw_dict = asdict(self)
+        if raw_dict.get('tool_calls'):
+            for idx, tool_call in enumerate(raw_dict['tool_calls']):
+                try:
+                    if tool_call['arguments']:
+                        json.loads(tool_call['arguments'])
+                except Exception:
+                    tool_call['arguments'] = '{}'
+                raw_dict['tool_calls'][idx] = {
+                    'id': tool_call['id'],
+                    'type': tool_call['type'],
+                    'function': {
+                        'name': tool_call['tool_name'],
+                        'arguments': tool_call['arguments'],
+                    }
+                }
+        required = ['content', 'role']
+        rm = ['completion_tokens', 'prompt_tokens', 'api_calls']
+        return {
+            key: value
+            for key, value in raw_dict.items()
+            if (value or key in required) and key not in rm
+        }
---
+++ packages/ms-agent/ms_agent-1.5.2.tar.gz/ms_agent-1.5.2/ms_agent/utils/constants.py
@@ -0,0 +1,111 @@
+from dataclasses import dataclass
+from typing import Dict, Optional
+
+# The default output dir
+DEFAULT_OUTPUT_DIR = './output'
+
+DEFAULT_INDEX_DIR = '.index'
+
+DEFAULT_LOCK_DIR = '.locks'
+
+# The key of user defined tools in the agent.yaml
+TOOL_PLUGIN_NAME = 'plugins'
+
+# Default agent config file
+AGENT_CONFIG_FILE = 'agent.yaml'
+
+# Default agent code file
+DEFAULT_AGENT_FILE = 'agent.py'
+
+# Default memory folder
+DEFAULT_MEMORY_DIR = '.memory'
+
+# DEFAULT_WORKFLOW_YAML
+WORKFLOW_CONFIG_FILE = 'workflow.yaml'
+
+# A base config of ms-agent
+DEFAULT_YAML = 'ms-agent/simple_agent'
+
+# The default tag of agent
+DEFAULT_TAG = 'Agent-default'
+
+# The default id of user
+DEFAULT_USER = 'User-default'
+
+DEFAULT_OUTPUT_WRAPPER = ['<result>', '</result>']
+
+DEFAULT_RETRY_COUNT = 3
+
+DEFAULT_SEARCH_LIMIT = 3
+
+MS_AGENT_ASCII = """
+╔═══════════════════════════════════════════════════════════════════════════╗
+║                                                                           ║
+║   ███╗   ███╗███████╗       █████╗  ██████╗ ███████╗███╗   ██╗████████╗   ║
+║   ████╗ ████║██╔════╝      ██╔══██╗██╔════╝ ██╔════╝████╗  ██║╚══██╔══╝   ║
+║   ██╔████╔██║███████╗ ████ ███████║██║  ███╗█████╗  ██╔██╗ ██║   ██║      ║
+║   ██║╚██╔╝██║╚════██║      ██╔══██║██║   ██║██╔══╝  ██║╚██╗██║   ██║      ║
+║   ██║ ╚═╝ ██║███████║      ██║  ██║╚██████╔╝███████╗██║ ╚████║   ██║      ║
+║   ╚═╝     ╚═╝╚══════╝      ╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝  ╚═══╝   ╚═╝      ║
+║                                                                           ║
+║               ( •̀ ω •́ )✧  ～(つˆДˆ)つ｡☆  (｡♥‿♥｡)  ٩(◕‿◕｡)۶                  ║
+║                     🙋‍From the ModelScope Team 💁‍                         ║
+╚═══════════════════════════════════════════════════════════════════════════╝
+"""
+
+
+@dataclass
+class ServiceConfig:
+    base_url: Optional[str] = None
+
+
+@dataclass
+class ModelscopeConfig(ServiceConfig):
+
+    def __init__(self):
+        super().__init__(base_url='https://api-inference.modelscope.cn/v1')
+
+
+@dataclass
+class DashscopeConfig(ServiceConfig):
+
+    def __init__(self):
+        super().__init__(
+            base_url='https://dashscope.aliyuncs.com/compatible-mode/v1')
+
+
+@dataclass
+class DeepseekConfig(ServiceConfig):
+
+    def __init__(self):
+        super().__init__(base_url='https://api.deepseek.com/v1')
+
+
+@dataclass
+class AnthropicConfig(ServiceConfig):
+
+    def __init__(self):
+        # without /v1, using Anthropic API
+        super().__init__(base_url='https://api.anthropic.com')
+
+
+class OpenaiConfig(ServiceConfig):
+
+    def __init__(self):
+        super().__init__(base_url='https://api.openai.com/v1')
+
+
+SERVICE_MAPPING: Dict[str, ServiceConfig] = {
+    'modelscope': ModelscopeConfig(),
+    'dashscope': DashscopeConfig(),
+    'deepseek': DeepseekConfig(),
+    'anthropic': AnthropicConfig(),
+    'openai': OpenaiConfig(),
+}
+
+
+def get_service_config(service_name: str) -> ServiceConfig:
+    if service_name.lower() in SERVICE_MAPPING:
+        return SERVICE_MAPPING[service_name.lower()]
+    else:
+        return ServiceConfig()
