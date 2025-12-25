d46bfd58a7ab467cd58eff2a299a66498e246d0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 11:05:48 +0000

    Add package ry/0.0.75/ry-0.0.75-cp313-cp313t-musllinux_1_2_x86_64.whl

---
+++ packages/ry/ry-0.0.75-cp313-cp313t-musllinux_1_2_x86_64.whl/ry/ryo3/__about__.pyi
@@ -0,0 +1,11 @@
+import typing as t
+
+__version__: str
+__authors__: str
+__build_profile__: str
+__build_timestamp__: str
+__pkg_name__: str
+__description__: str
+__target__: str
+__opt_level__: t.Literal["0", "1", "2", "3", "s", "z"]
+__allocator__: t.Literal["mimalloc", "system"]
