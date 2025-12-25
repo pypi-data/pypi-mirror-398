9b546a4498fbec5650dc3118b958150264aaa872
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 03:31:44 +0000

    Add package vnai/2.2.5/vnai-2.2.5.tar.gz

---
+++ packages/vnai/vnai-2.2.5.tar.gz/vnai-2.2.5/vnai/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+setup(
+    name="vnai",
+    version="2.2.5",
+    packages=find_packages(where="."),
+    install_requires=[
+"requests>=2.31.0",
+"psutil>=5.8.0",
+    ],
+    extras_require={
+"dev": ["pytest>=7.0.0"],
+    },
+    python_requires=">=3.10",
+    description="Analytics and optimization library for vnstock ecosystem",
+    long_description=open("README.md").read() if __name__ =='__main__' else"",
+    long_description_content_type="text/markdown",
+    author="Vnstock Team",
+    author_email="support@vnstocks.com",
+    classifiers=[
+"Programming Language :: Python :: 3",
+"Programming Language :: Python :: 3.10",
+"Programming Language :: Python :: 3.11",
+"Programming Language :: Python :: 3.12",
+"Programming Language :: Python :: 3.13",
+    ],
+)
