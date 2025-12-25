510c2f4c7c008b7b2874ab9bfc500437c68ad583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 07:06:29 +0000

    Add package mehdashti-encryption/0.2.0/mehdashti_encryption-0.2.0.tar.gz

---
+++ packages/mehdashti-encryption/mehdashti_encryption-0.2.0.tar.gz/mehdashti_encryption-0.2.0/mehdashti_encryption/__init__.py
@@ -0,0 +1,11 @@
+"""
+AES-256-GCM Encryption with Key Rotation
+
+Provides secure encryption/decryption with support for key rotation.
+"""
+
+from mehdashti_encryption.service import EncryptionService
+
+__all__ = ["EncryptionService"]
+
+__version__ = "0.1.0"
