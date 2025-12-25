6dede1a7bd70ae86e0ab314246d9dfac8ad12ae9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 14:06:18 +0000

    Add package writer/1.26.0rc7/writer-1.26.0rc7.tar.gz

---
+++ packages/writer/writer-1.26.0rc7.tar.gz/writer-1.26.0rc7/src/writer/static/assets/BuilderInsertionLabel-lq5R-8P7.js
@@ -0,0 +1,1 @@
+import{_ as s,o as t,f as n,J as o}from"./index-dyx4v2bZ.js";const r={},a={class:"BuilderInsertionLabel"};function c(e,d){return t(),n("div",a,[o(e.$slots,"default",{},void 0,!0)])}const _=s(r,[["render",c],["__scopeId","data-v-9cd536ed"]]);export{_ as default};
