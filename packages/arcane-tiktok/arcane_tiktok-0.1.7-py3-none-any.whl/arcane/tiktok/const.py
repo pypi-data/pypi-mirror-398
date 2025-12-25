001d9955177fc424f99059017dddd2619325ce26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 13:15:13 +0000

    Add package arcane-tiktok/0.1.7/arcane_tiktok-0.1.7-py3-none-any.whl

---
+++ packages/arcane-tiktok/arcane_tiktok-0.1.7-py3-none-any.whl/arcane/tiktok/const.py
@@ -0,0 +1,2 @@
+TIKTOK_SERVER_URL = "https://business-api.tiktok.com/open_api/v1.3"
+TIKTOK_OAUTH_CREDENTIALS_KIND = "tiktok-oauth-credentials"
---
+++ packages/arcane-tiktok/arcane_tiktok-0.1.7-py3-none-any.whl/arcane/tiktok/exceptions.py
@@ -0,0 +1,7 @@
+class TikTokAuthError(Exception):
+    """Raised when there is an authentication error with TikTok API"""
+    pass
+
+class TikTokApiError(Exception):
+    """Raised when there is a general API error with TikTok API"""
+    pass
