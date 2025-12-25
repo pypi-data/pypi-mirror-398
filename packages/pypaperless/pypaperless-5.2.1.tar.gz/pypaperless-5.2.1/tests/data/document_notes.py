29513a7c4e264f7145ef134df34f38b970fe6672
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 06:05:35 +0000

    Add package pypaperless/5.2.1/pypaperless-5.2.1.tar.gz

---
+++ packages/pypaperless/pypaperless-5.2.1.tar.gz/pypaperless-5.2.1/tests/data/document_notes.py
@@ -0,0 +1,37 @@
+"""Document notes snapshot."""
+
+DATA_DOCUMENT_NOTES = [
+    {
+        "id": 1,
+        "note": "Sample note 1.",
+        "created": "2023-12-21T18:08:11.481206+00:00",
+        "user": {
+            "id": 1,
+            "username": "test",
+            "first_name": "Peter",
+            "last_name": "Patch",
+        },
+    },
+    {
+        "id": 2,
+        "note": "Sample note 2.",
+        "created": "2023-12-21T08:26:33.260968+00:00",
+        "user": {
+            "id": 2,
+            "username": "test",
+            "first_name": "Peter",
+            "last_name": "Patch",
+        },
+    },
+    {
+        "id": 3,
+        "note": "Sample note 3.",
+        "created": "2023-12-21T08:26:31.782811+00:00",
+        "user": {
+            "id": 3,
+            "username": "test",
+            "first_name": "Peter",
+            "last_name": "Patch",
+        },
+    },
+]
