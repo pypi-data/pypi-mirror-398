a1b1d1568f855cf5579548e817840485fa1db6df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 11:05:42 +0000

    Add package ry/0.0.75/ry-0.0.75-cp312-cp312-musllinux_1_2_i686.whl

---
+++ packages/ry/ry-0.0.75-cp312-cp312-musllinux_1_2_i686.whl/ry/JSON.pyi
@@ -0,0 +1,21 @@
+"""ry.JSON"""
+
+from ry.ryo3.JSON import cache_clear as cache_clear
+from ry.ryo3.JSON import cache_usage as cache_usage
+from ry.ryo3.JSON import dumps as dumps
+from ry.ryo3.JSON import fmt as fmt
+from ry.ryo3.JSON import loads as loads
+from ry.ryo3.JSON import minify as minify
+from ry.ryo3.JSON import parse as parse
+from ry.ryo3.JSON import stringify as stringify
+
+__all__ = (
+    "cache_clear",
+    "cache_usage",
+    "dumps",
+    "fmt",
+    "loads",
+    "minify",
+    "parse",
+    "stringify",
+)
