ba1702e68a2b42da7c4af0ff38554e7523eae9d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 06:05:48 +0000

    Add package dasformer/0.1.0/dasformer-0.1.0.tar.gz

---
+++ packages/dasformer/dasformer-0.1.0.tar.gz/dasformer-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="dasformer",
+    version="0.1.0",
+    long_description="dasformer",
+    long_description_content_type="text/markdown",
+    packages=["dasformer"],
+    install_requires=["numpy",  "h5py", "matplotlib", "pandas"],
+)
