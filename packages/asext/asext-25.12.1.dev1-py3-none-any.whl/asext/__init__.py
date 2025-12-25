f9cc27a8be1f26e0e0bbf70964a3956fd1f2d696
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 11:06:37 +0000

    Add package asext/25.12.1.dev1/asext-25.12.1.dev1-py3-none-any.whl

---
+++ packages/asext/asext-25.12.1.dev1-py3-none-any.whl/asext/__init__.py
@@ -0,0 +1,18 @@
+"""ASEXT: Python package extends functions of ASE (Atomic Simulation Environment).
+
+<img src="https://thangckt.github.io/asext/1images/kde_color_128x128.png" style="float: left; margin-right: 20px" width="120"/>
+
+Developed and maintained by [C.Thang Nguyen](https://thangckt.github.io)
+"""
+
+from pathlib import Path
+
+ASEXT_ROOT = Path(__file__).parent
+
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "0.1_fallback"
+
+__author__ = "C.Thang Nguyen"
+__contact__ = "http://thangckt.github.io/email"
