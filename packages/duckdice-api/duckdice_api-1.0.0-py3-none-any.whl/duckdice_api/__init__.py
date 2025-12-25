09ee8346faf6aee3d6f844234564e069b64e9554
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 14:06:19 +0000

    Add package duckdice-api/1.0.0/duckdice_api-1.0.0-py3-none-any.whl

---
+++ packages/duckdice-api/duckdice_api-1.0.0-py3-none-any.whl/duckdice_api/__init__.py
@@ -0,0 +1,5 @@
+"""DuckDice API Python Wrapper"""
+from .api import DuckDiceAPI, DuckDiceAPIException, DuckDiceConfig
+
+__version__ = "1.0.0"
+__all__ = ["DuckDiceAPI", "DuckDiceAPIException", "DuckDiceConfig"]
