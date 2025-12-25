ef553aeb4478a87a54e4ccfb2f9cf103beb7538f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 05:06:26 +0000

    Add package pointwise/0.1.0.dev2/pointwise-0.1.0.dev2.tar.gz

---
+++ packages/pointwise/pointwise-0.1.0.dev2.tar.gz/pointwise-0.1.0.dev2/README.rst
@@ -0,0 +1,5 @@
+=========
+pointwise
+=========
+
+Visit the website `https://pointwise.johannes-programming.online/ <https://pointwise.johannes-programming.online/>`_ for more information.
---
+++ packages/pointwise/pointwise-0.1.0.dev2.tar.gz/pointwise-0.1.0.dev2/src/pointwise/core/__init__.py
@@ -0,0 +1,28 @@
+import functools
+from types import FunctionType
+from typing import *
+
+__all__ = ["pointwise"]
+
+
+def pointwise(
+    outer: Callable,
+    /,
+    *args: Callable,
+    **kwargs: Callable,
+) -> FunctionType:
+    @functools.wraps(outer)
+    def ans(*args_: Any, **kwargs_: Any) -> Any:
+        x: Any
+        y: Any
+        args__: list
+        kwargs__: dict[str, Any]
+        args__ = list()
+        for y in args:
+            args__.append(y(*args_, **kwargs_))
+        kwargs__ = dict()
+        for x, y in kwargs.items():
+            kwargs__[x] = y(*args_, **kwargs_)
+        return outer(*args__, **kwargs__)
+
+    return ans
