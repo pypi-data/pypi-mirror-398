5f601e7706cd594568cea6f39eb448af3ab97c13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 11:05:45 +0000

    Add package ry/0.0.75/ry-0.0.75-cp314-cp314t-musllinux_1_2_i686.whl

---
+++ packages/ry/ry-0.0.75-cp314-cp314t-musllinux_1_2_i686.whl/ry/__about__.py
@@ -0,0 +1,29 @@
+"""Package metadata/info"""
+
+from __future__ import annotations
+
+from ry.ryo3 import (
+    __allocator__,
+    __authors__,
+    __build_profile__,
+    __build_timestamp__,
+    __opt_level__,
+    __target__,
+    __version__,
+)
+
+__all__ = (
+    "__allocator__",
+    "__authors__",
+    "__build_profile__",
+    "__build_timestamp__",
+    "__description__",
+    "__opt_level__",
+    "__pkgroot__",
+    "__target__",
+    "__title__",
+    "__version__",
+)
+__title__ = "ry"
+__description__ = "ry = rust + python"
+__pkgroot__ = __file__.replace("__about__.py", "").rstrip("/\\")
