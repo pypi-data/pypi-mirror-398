eaae92367f61b40676f1af4417c2a8d38a94b656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 05:06:32 +0000

    Add package reckomate-sdk/1.0.7/reckomate_sdk-1.0.7.tar.gz

---
+++ packages/reckomate-sdk/reckomate_sdk-1.0.7.tar.gz/reckomate_sdk-1.0.7/__init__.py
@@ -0,0 +1,11 @@
+# reckomate_sdk/__init__.py
+
+"""
+Reckomate SDK
+Proxy-only Python SDK for Reckomate backend APIs
+"""
+
+from .client import ReckomateSDK
+
+__all__ = ["ReckomateSDK"]
+__version__ = "1.0.7"
