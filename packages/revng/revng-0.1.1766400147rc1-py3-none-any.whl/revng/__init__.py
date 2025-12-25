95856de603fc3bb183b6a5799db6f404bf62070c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 06:04:58 +0000

    Add package revng/0.1.1766400147rc1/revng-0.1.1766400147rc1-py3-none-any.whl

---
+++ packages/revng/revng-0.1.1766400147rc1-py3-none-any.whl/revng/__init__.py
@@ -0,0 +1,5 @@
+#
+# This file is distributed under the MIT License. See LICENSE.md for details.
+#
+
+__version__ = "0.1.1766400147rc1"
---
+++ packages/revng/revng-0.1.1766400147rc1-py3-none-any.whl/revng/model/external.py
@@ -0,0 +1,7 @@
+#
+# This file is distributed under the MIT License. See LICENSE.md for details.
+#
+
+from revng.model.metaaddress import MetaAddress
+
+__all__ = ["MetaAddress"]
