da31e158c9e09167d3450330e1c0662fc8cce525
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 09:06:29 +0000

    Add package rofl/0.0.1/rofl-0.0.1.tar.gz

---
+++ packages/rofl/rofl-0.0.1.tar.gz/rofl-0.0.1/setup.py
@@ -0,0 +1,32 @@
+
+import setuptools
+from setuptools import find_packages
+import re
+
+with open("./rofl/__init__.py", 'r') as f:
+    content = f.read()
+    version = re.search(r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]', content).group(1)
+    
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rofl",
+    version=version,
+    author="Roboflow",
+    author_email="support@roboflow.com",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[],
+    packages=find_packages(exclude=("tests",)),
+    extras_require={
+        "dev": ["flake8", "black==22.3.0", "isort", "twine", "pytest", "wheel"],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
