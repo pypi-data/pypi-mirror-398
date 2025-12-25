3e1a05c50c568d8720793d07196dc81f114d3199
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 03:31:53 +0000

    Add package Pyro5/5.16/pyro5-5.16.tar.gz

---
+++ packages/Pyro5/pyro5-5.16.tar.gz/pyro5-5.16/examples/streaming/client.py
@@ -0,0 +1,23 @@
+import sys
+import Pyro5.errors
+import Pyro5.api
+
+
+sys.excepthook = Pyro5.errors.excepthook
+
+
+uri = input("Enter streaming server uri: ").strip()
+with Pyro5.api.Proxy(uri) as p:
+    print("\nnormal list:")
+    print(p.list())
+    print("\nvia iterator:")
+    print(list(p.iterator()))
+    print("\nvia generator:")
+    print(list(p.generator()))
+    print("\nslow generator:")
+    for number in p.slow_generator():
+        print(number)
+    print("\nproperty iterator:")
+    print(list(p.prop_iter))
+    print("\nproperty generator:")
+    print(list(p.prop_generator))
