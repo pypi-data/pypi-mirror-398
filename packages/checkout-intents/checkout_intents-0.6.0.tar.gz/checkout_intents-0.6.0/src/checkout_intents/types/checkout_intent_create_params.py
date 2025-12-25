78480c41a5e7d984b8de7995e4527e7c7c3bfaa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 06:05:35 +0000

    Add package checkout-intents/0.6.0/checkout_intents-0.6.0.tar.gz

---
+++ packages/checkout-intents/checkout_intents-0.6.0.tar.gz/checkout_intents-0.6.0/src/checkout_intents/types/checkout_intent_create_params.py
@@ -0,0 +1,25 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import Iterable
+from typing_extensions import Required, Annotated, TypedDict
+
+from .._types import SequenceNotStr
+from .._utils import PropertyInfo
+from .buyer_param import BuyerParam
+from .variant_selection_param import VariantSelectionParam
+
+__all__ = ["CheckoutIntentCreateParams"]
+
+
+class CheckoutIntentCreateParams(TypedDict, total=False):
+    buyer: Required[BuyerParam]
+
+    product_url: Required[Annotated[str, PropertyInfo(alias="productUrl")]]
+
+    quantity: Required[float]
+
+    promo_codes: Annotated[SequenceNotStr[str], PropertyInfo(alias="promoCodes")]
+
+    variant_selections: Annotated[Iterable[VariantSelectionParam], PropertyInfo(alias="variantSelections")]
