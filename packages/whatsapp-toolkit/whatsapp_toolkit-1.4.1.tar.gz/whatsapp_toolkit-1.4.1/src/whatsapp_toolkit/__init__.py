c632e850b1a954d39cc785a5c102d9ca380cf549
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 05:06:40 +0000

    Add package whatsapp-toolkit/1.4.1/whatsapp_toolkit-1.4.1.tar.gz

---
+++ packages/whatsapp-toolkit/whatsapp_toolkit-1.4.1.tar.gz/whatsapp_toolkit-1.4.1/src/whatsapp_toolkit/__init__.py
@@ -0,0 +1,5 @@
+from .audio import generar_audio
+from .client import WhatsappClient
+from .media import PDFGenerator, obtener_gif_base64, obtener_imagen_base64
+from .devtools import *
+from .types import *
