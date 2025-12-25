70f4c367e8a411820e28384d51290cf11bd5a026
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 05:06:39 +0000

    Add package specular-differentiation/0.3.5/specular_differentiation-0.3.5-py3-none-any.whl

---
+++ packages/specular-differentiation/specular_differentiation-0.3.5-py3-none-any.whl/specular/__init__.py
@@ -0,0 +1,22 @@
+from .core import (
+    A,
+    derivative,
+    directional_derivative,
+    partial_derivative,
+    gradient
+)
+
+from . import ode
+from . import tools
+
+__version__ = "0.3.5"
+
+__all__ = [
+    "A",
+    "derivative",
+    "directional_derivative", 
+    "partial_derivative",
+    "gradient", 
+    "ode", 
+    "tools"
+]
