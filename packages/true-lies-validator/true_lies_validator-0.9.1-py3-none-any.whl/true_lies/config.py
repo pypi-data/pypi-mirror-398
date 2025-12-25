bf896faee7b29a4fc3640666b0989ed5c2900278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 03:32:10 +0000

    Add package true-lies-validator/0.9.1/true_lies_validator-0.9.1-py3-none-any.whl

---
+++ packages/true-lies-validator/true_lies_validator-0.9.1-py3-none-any.whl/true_lies/config.py
@@ -0,0 +1,11 @@
+POSITIVE_MARKERS = [
+    "can", "allowed", "unlimited", "earn", "earns", "accrues", "compounds",
+    "applied", "applies", "calculated", "credited", "added", "gains", "yields", "returns", "benefits"
+]
+
+NEGATIVE_MARKERS = [
+    "cannot", "not allowed", "no ", "not", "limited", "does not", "can't",
+    "not supposed to", "should not", "not expected to", "never"
+]
+
+SIMILARITY_THRESHOLD = 0.7
