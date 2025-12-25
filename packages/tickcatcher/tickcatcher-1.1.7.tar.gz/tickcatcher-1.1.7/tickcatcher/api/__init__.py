eea4a0354f6d7d45b410f6090ad66da835d52b34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 04:16:38 +0000

    Add package tickcatcher/1.1.7/tickcatcher-1.1.7.tar.gz

---
+++ packages/tickcatcher/tickcatcher-1.1.7.tar.gz/tickcatcher-1.1.7/tickcatcher/api/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+
+# import apis into api package
+from tickcatcher.api.calendar_api import CalendarApi
+from tickcatcher.api.candles_api import CandlesApi
+from tickcatcher.api.general_api import GeneralApi
+from tickcatcher.api.indicators_api import IndicatorsApi
+
