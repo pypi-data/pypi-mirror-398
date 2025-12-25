4fcc4544633f84542425ac3df35883682c405287
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 03:31:59 +0000

    Add package text2markdown-formatter/2025.12.21173214/text2markdown_formatter-2025.12.21173214-py3-none-any.whl

---
+++ packages/text2markdown-formatter/text2markdown_formatter-2025.12.21173214-py3-none-any.whl/text2markdown_formatter/__init__.py
@@ -0,0 +1,4 @@
+from .main import text2markdown_formatter
+from .prompts import human_prompt, pattern, system_prompt
+
+__all__ = ["text2markdown_formatter", "system_prompt", "human_prompt", "pattern"]
