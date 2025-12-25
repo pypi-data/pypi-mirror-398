6515af0f8d2227af7757786672637bbd1171e17f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 05:06:21 +0000

    Add package dfsx/1.1/dfsx-1.1.tar.gz

---
+++ packages/dfsx/dfsx-1.1.tar.gz/dfsx-1.1/README.md
@@ -0,0 +1,4 @@
+## NitroExpose
+
+**Letest Version:** 1.1  
+**Developer:** We Don't Know  
---
+++ packages/dfsx/dfsx-1.1.tar.gz/dfsx-1.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="dfsx",
+    version="1.1",
+    author="DFSX Team",
+    description="A CLI tool for setting up and running Telegram bot scripts",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dfsxteam/dfsx",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires=">=3.8",
+    entry_points={
+        "console_scripts": [
+            "dfsx=dfsx.cli:main",
+        ],
+    },
+    include_package_data=True,
+    package_data={
+        "dfsx": ["script/*"],
+    },
+)
