0b1de78dba760b96cc9a4120f8ec3929108d596c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 13:15:01 +0000

    Add package textformatter-plus/1.0.1/textformatter_plus-1.0.1.tar.gz

---
+++ packages/textformatter-plus/textformatter_plus-1.0.1.tar.gz/textformatter_plus-1.0.1/my_feature_package/__init__.py
@@ -0,0 +1,10 @@
+"""
+My Feature Package - A demo Python package with useful features
+"""
+
+from .text_formatter import TextFormatter
+from .validator import Validator
+
+__version__ = "0.1.0"
+__all__ = ["TextFormatter", "Validator"]
+
