9718b5c786106590065427941c6d1c801e743d2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 08:04:58 +0000

    Add package pyjess/0.9.1/pyjess-0.9.1-cp37-cp37m-win_amd64.whl

---
+++ packages/pyjess/pyjess-0.9.1-cp37-cp37m-win_amd64.whl/pyjess/tests/__init__.py
@@ -0,0 +1,20 @@
+from . import (
+    test_atom,
+    test_doctest,
+    test_hit,
+    test_jess,
+    test_molecule,
+    test_template_atom,
+    test_template
+)
+
+def load_tests(loader, suite, pattern):
+    suite.addTests(loader.loadTestsFromModule(test_doctest))
+    suite.addTests(loader.loadTestsFromModule(test_atom))
+    suite.addTests(loader.loadTestsFromModule(test_hit))
+    suite.addTests(loader.loadTestsFromModule(test_jess))
+    suite.addTests(loader.loadTestsFromModule(test_molecule))
+    suite.addTests(loader.loadTestsFromModule(test_template_atom))
+    suite.addTests(loader.loadTestsFromModule(test_template))
+    # test_doctest.load_tests(loader, suite, pattern)
+    return suite
