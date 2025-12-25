efa10ff8534b651bf3cbb1b6aa91e5ccd37420d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 08:04:41 +0000

    Add package agbcloud-sdk/0.7.1/agbcloud_sdk-0.7.1.tar.gz

---
+++ packages/agbcloud-sdk/agbcloud_sdk-0.7.1.tar.gz/agbcloud_sdk-0.7.1/agb/model/__init__.py
@@ -0,0 +1,19 @@
+from .response import (
+    ApiResponse,
+    BoolResult,
+    DeleteResult,
+    DownloadResult,
+    OperationResult,
+    SessionResult,
+    UploadResult,
+)
+
+__all__ = [
+    "ApiResponse",
+    "SessionResult",
+    "DeleteResult",
+    "OperationResult",
+    "BoolResult",
+    "UploadResult",
+    "DownloadResult",
+]
