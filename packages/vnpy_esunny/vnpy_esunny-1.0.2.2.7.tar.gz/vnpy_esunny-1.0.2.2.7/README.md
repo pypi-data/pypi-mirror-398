0d58397883fbd1737bc033ec9391e3dbff155239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 11:05:07 +0000

    Add package vnpy_esunny/1.0.2.2.7/vnpy_esunny-1.0.2.2.7.tar.gz

---
+++ packages/vnpy_esunny/vnpy_esunny-1.0.2.2.7.tar.gz/vnpy_esunny-1.0.2.2.7/README.md
@@ -0,0 +1,69 @@
+# VeighNa框架的易盛底层接口
+
+<p align="center">
+  <img src ="https://vnpy.oss-cn-shanghai.aliyuncs.com/vnpy-logo.png"/>
+</p>
+
+<p align="center">
+    <img src ="https://img.shields.io/badge/version-1.0.2.2.7-blueviolet.svg"/>
+    <img src ="https://img.shields.io/badge/platform-windows|linux-yellow.svg"/>
+    <img src ="https://img.shields.io/badge/python-3.7|3.8|3.9|3.10-blue.svg" />
+    <img src ="https://img.shields.io/github/license/vnpy/vnpy.svg?color=orange"/>
+</p>
+
+## 说明
+
+基于易盛的启明星/北斗星兼容交易API的1.0.2.2接口封装开发，支持启明星、北斗星后台系统，适用于内盘和香港、新加坡以及其他境外交易所交易。
+
+## 安装
+
+安装环境推荐基于3.3.0版本以上的【[**VeighNa Studio**](https://www.vnpy.com)】。
+
+直接使用pip命令：
+
+```
+pip install vnpy_esunny
+```
+
+
+或者下载源代码后，解压后在cmd中运行：
+
+```
+pip install .
+```
+
+使用源代码安装时需要进行C++编译，因此在执行上述命令之前请确保已经安装了【Visual Studio（Windows）】或者【GCC（Linux）】编译器。
+
+## 使用
+
+以脚本方式启动（script/run.py）：
+
+```
+from vnpy.event import EventEngine
+from vnpy.trader.engine import MainEngine
+from vnpy.trader.ui import MainWindow, create_qapp
+
+from vnpy_esunny import EsunnyGateway
+
+
+def main():
+    """主入口函数"""
+    qapp = create_qapp()
+
+    event_engine = EventEngine()
+    main_engine = MainEngine(event_engine)
+    main_engine.add_gateway(EsunnyGateway)
+    
+    main_window = MainWindow(main_engine, event_engine)
+    main_window.showMaximized()
+
+    qapp.exec()
+
+
+if __name__ == "__main__":
+    main()
+```
+
+## 连接
+
+连接前请先复制vnpy_esunny.vnpy_esunny.api文件夹下的ITapTdAPI.dll、ITapSETdAPI.dll和TapTdAPI.dll，放置在VN Trader运行目录（.vntrader同级文件夹）下。
---
+++ packages/vnpy_esunny/vnpy_esunny-1.0.2.2.7.tar.gz/vnpy_esunny-1.0.2.2.7/vnpy_esunny/api/vnesunny/vnesunny.h
@@ -0,0 +1,169 @@
+#include <string>
+#include <queue>
+#include <thread>
+#include <mutex>
+#include <iostream>
+#include <codecvt>
+#include <condition_variable>
+#include <locale>
+
+#include "pybind11/pybind11.h"
+
+
+using namespace std;
+using namespace pybind11;
+
+
+//����ṹ��
+struct Task
+{
+    int task_name;		//�ص��������ƶ�Ӧ�ĳ���
+    void *task_data;	//����ָ��
+    int task_error;	//����ָ��
+	unsigned int task_id;		//����id
+    char task_last;		//�Ƿ�Ϊ��󷵻�
+
+	int task_int; // ����int ����
+	string task_string; // ���� string����
+	void *task_extra;
+};
+
+class TerminatedError : std::exception
+{};
+
+class TaskQueue
+{
+private:
+    queue<Task> queue_;						//��׼�����
+    mutex mutex_;							//������
+    condition_variable cond_;				//��������
+
+    bool _terminate = false;
+
+public:
+
+    //�����µ�����
+    void push(const Task &task)
+    {
+        unique_lock<mutex > mlock(mutex_);
+        queue_.push(task);					//������д�������
+        mlock.unlock();						//�ͷ���
+        cond_.notify_one();					//֪ͨ���������ȴ����߳�
+    }
+
+    //ȡ���ϵ�����
+    Task pop()
+    {
+        unique_lock<mutex> mlock(mutex_);
+        cond_.wait(mlock, [&]() {
+            return !queue_.empty() || _terminate;
+        });				//�ȴ���������֪ͨ
+        if (_terminate)
+            throw TerminatedError();
+        Task task = queue_.front();			//��ȡ�����е����һ������
+        queue_.pop();						//ɾ��������
+        return task;						//���ظ�����
+    }
+
+    void terminate()
+    {
+        _terminate = true;
+        cond_.notify_all();					//֪ͨ���������ȴ����߳�
+    }
+};
+
+
+//���ֵ��л�ȡĳ����ֵ��Ӧ������������ֵ������ṹ������ֵ��
+void getInt(const dict &d, const char *key, int *value)
+{
+    if (d.contains(key))		//����ֵ����Ƿ���ڸü�ֵ
+    {
+        object o = d[key];		//��ȡ�ü�ֵ
+        *value = o.cast<int>();
+    }
+};
+
+void getUnsignedInt(const dict &d, const char *key, unsigned int *value)
+{
+	if (d.contains(key))		//����ֵ����Ƿ���ڸü�ֵ
+	{
+		object o = d[key];		//��ȡ�ü�ֵ
+		*value = o.cast<unsigned int>();
+	}
+};
+
+void getUnsignedChar(const dict &d, const char *key, unsigned char *value)
+{
+	if (d.contains(key))		//����ֵ����Ƿ���ڸü�ֵ
+	{
+		object o = d[key];		//��ȡ�ü�ֵ
+        char c = o.cast<char>();
+		*value = (unsigned char)c;
+	}
+};
+
+
+//���ֵ��л�ȡĳ����ֵ��Ӧ�ĸ�����������ֵ������ṹ������ֵ��
+void getDouble(const dict &d, const char *key, double *value)
+{
+    if (d.contains(key))
+    {
+        object o = d[key];
+        *value = o.cast<double>();
+    }
+};
+
+
+//���ֵ��л�ȡĳ����ֵ��Ӧ���ַ�������ֵ������ṹ������ֵ��
+void getChar(const dict &d, const char *key, char *value)
+{
+    if (d.contains(key))
+    {
+        object o = d[key];
+        *value = o.cast<char>();
+    }
+};
+
+
+template <size_t size>
+using string_literal = char[size];
+
+//���ֵ��л�ȡĳ����ֵ��Ӧ���ַ���������ֵ������ṹ������ֵ��
+template <size_t size>
+void getString(const pybind11::dict &d, const char *key, string_literal<size> &value)
+{
+    if (d.contains(key))
+    {
+        object o = d[key];
+        string s = o.cast<string>();
+        const char *buf = s.c_str();
+        strcpy(value, buf);
+    }
+};
+
+//��GBK������ַ���ת��ΪUTF8
+inline string toUtf(const string &gb2312)
+{
+#ifdef _MSC_VER
+    const static locale loc("zh-CN");
+#else
+    const static locale loc("zh_CN.GB18030");
+#endif
+
+    vector<wchar_t> wstr(gb2312.size());
+    wchar_t* wstrEnd = nullptr;
+    const char* gbEnd = nullptr;
+    mbstate_t state = {};
+    int res = use_facet<codecvt<wchar_t, char, mbstate_t> >
+        (loc).in(state,
+            gb2312.data(), gb2312.data() + gb2312.size(), gbEnd,
+            wstr.data(), wstr.data() + wstr.size(), wstrEnd);
+
+    if (codecvt_base::ok == res)
+    {
+        wstring_convert<codecvt_utf8<wchar_t>> cutf8;
+        return cutf8.to_bytes(wstring(wstr.data(), wstrEnd));
+    }
+
+    return string();
+}
