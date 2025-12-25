58c91812481036fe86573cd2401057afe717f0a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 11:06:23 +0000

    Add package postgres-wire/0.1.0/postgres_wire-0.1.0-py3-none-any.whl

---
+++ packages/postgres-wire/postgres_wire-0.1.0-py3-none-any.whl/postgres_wire/fields.py
@@ -0,0 +1,12 @@
+class IntField:
+  def __init__(self, name):
+    self.name = name
+    self.type_id = 23
+    self.type_size = 4
+
+# Based on https://github.com/jeroenrinzema/psql-wire/blob/029cb6a10c1b8d5a9076e12f714c57d672f3e898/examples/copy/main.go#L22
+class TextField:
+  def __init__(self, name):
+    self.name = name
+    self.type_id = 25
+    self.type_size = 256
