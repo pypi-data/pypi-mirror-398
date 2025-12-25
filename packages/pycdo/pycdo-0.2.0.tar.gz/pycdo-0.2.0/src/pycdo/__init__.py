181b3af06dde3a180d1b9e3d2327c736c8ecf108
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 11:06:36 +0000

    Add package pycdo/0.2.0/pycdo-0.2.0.tar.gz

---
+++ packages/pycdo/pycdo-0.2.0.tar.gz/pycdo-0.2.0/src/pycdo/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2025-present Elio Campitelli <eliocampitelli@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+from .cdo_operation import cdo
+from .cdo_options import cdo_options
+from .cdo_cache import cdo_cache
+from .example_data import geopotential
