b1f64411c4d3abe37d524d45dff0ba8bdf8bb66b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 03:31:44 +0000

    Add package cipherforge/1.0/cipherforge-1.0.tar.gz

---
+++ packages/cipherforge/cipherforge-1.0.tar.gz/cipherforge-1.0/cipherforge/__init__.py
@@ -0,0 +1,11 @@
+"""
+CipherForge - Secure File Encryption Tool
+"""
+
+__version__ = "1.0.0"
+__author__ = "gtk-gg"
+__license__ = "MIT"
+
+from .cipherforge import main, CipherForge
+
+__all__ = ['main', 'CipherForge']
