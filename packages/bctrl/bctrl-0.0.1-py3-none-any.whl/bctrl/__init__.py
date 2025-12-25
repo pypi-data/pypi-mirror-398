a4da1f2087212a50310b1a8bef5b6aa29fd7693b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 08:04:42 +0000

    Add package bctrl/0.0.1/bctrl-0.0.1-py3-none-any.whl

---
+++ packages/bctrl/bctrl-0.0.1-py3-none-any.whl/bctrl/__init__.py
@@ -0,0 +1,13 @@
+"""
+bctrl - Remote browser automation for AI agents.
+
+This package is under development. Full Python SDK coming soon.
+
+For now, check out the TypeScript/JavaScript SDK:
+    npm install bctrl
+
+Learn more: https://github.com/bctrl/bctrl
+"""
+
+__version__ = "0.0.1"
+__all__ = ["__version__"]
