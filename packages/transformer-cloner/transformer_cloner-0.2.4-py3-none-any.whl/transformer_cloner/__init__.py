107204445d559633b070d8512d5718aae27a3101
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 08:04:31 +0000

    Add package transformer-cloner/0.2.4/transformer_cloner-0.2.4-py3-none-any.whl

---
+++ packages/transformer-cloner/transformer_cloner-0.2.4-py3-none-any.whl/transformer_cloner/__init__.py
@@ -0,0 +1,20 @@
+"""
+Transformer Cloner - Clone and prune transformer models with new tokenizers.
+
+A library for cloning transformer models with new tokenizers, including
+support for vocabulary mapping, embedding strategies, and model pruning.
+"""
+
+from transformer_cloner.embedding_strategy import EmbeddingStrategy
+from transformer_cloner.pruning_config import PruningConfig
+from transformer_cloner.cloner import TransformerCloner
+from transformer_cloner.vocab_pruned_tokenizer import VocabPrunedTokenizer
+
+__version__ = "0.2.4"
+__all__ = [
+    "TransformerCloner",
+    "EmbeddingStrategy",
+    "PruningConfig",
+    "VocabPrunedTokenizer",
+    "__version__",
+]
