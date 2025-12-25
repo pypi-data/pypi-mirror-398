3fd7ecda091bf3f9927456f94a49fa8feb6b6e66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 13:14:58 +0000

    Add package saltpeter/0.9.30/saltpeter-0.9.30.tar.gz

---
+++ packages/saltpeter/saltpeter-0.9.30.tar.gz/saltpeter-0.9.30/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+exec(open('saltpeter/version.py').read())
+setup(name='saltpeter',
+      version=__version__,
+      description='Distributed cron based on salt',
+      long_description='Distributed cron based on salt',
+      url='http://github.com/syscollective/saltpeter',
+      author='Marin Vintila',
+      author_email='marin.vintila@syscollective.com',
+      license='MIT',
+      packages=['saltpeter'],
+      entry_points = {
+          'console_scripts': ['saltpeter=saltpeter.main:main'],
+      },
+      install_requires=[
+          'websocket',
+          'crontab',
+          'pyyaml',
+          'elasticsearch',
+          'opensearch-py',
+      ],
+      zip_safe=False)
