0ab0cddc0fe88bb72f2e3fdb462b90fc651c9cc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 11:05:56 +0000

    Add package fastapi-fullstack/0.1.7/fastapi_fullstack-0.1.7-py3-none-any.whl

---
+++ packages/fastapi-fullstack/fastapi_fullstack-0.1.7-py3-none-any.whl/fastapi_gen/template/{{cookiecutter.project_slug}}/backend/app/worker/tasks/__init__.py
@@ -0,0 +1,25 @@
+{%- if cookiecutter.use_celery or cookiecutter.use_taskiq %}
+"""Background tasks."""
+
+{%- if cookiecutter.use_celery %}
+from app.worker.tasks.examples import example_task, long_running_task
+{%- endif %}
+
+{%- if cookiecutter.use_taskiq %}
+from app.worker.tasks.taskiq_examples import example_task as taskiq_example_task
+from app.worker.tasks.taskiq_examples import long_running_task as taskiq_long_running_task
+{%- endif %}
+
+__all__ = [
+{%- if cookiecutter.use_celery %}
+    "example_task",
+    "long_running_task",
+{%- endif %}
+{%- if cookiecutter.use_taskiq %}
+    "taskiq_example_task",
+    "taskiq_long_running_task",
+{%- endif %}
+]
+{%- else %}
+# Background tasks not enabled
+{%- endif %}
