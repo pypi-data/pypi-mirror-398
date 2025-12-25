b0fc37fe304eb376f7ac895b9c2b0108fabd973a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 11:06:35 +0000

    Add package basedpyright/1.36.2/basedpyright-1.36.2.tar.gz

---
+++ packages/basedpyright/basedpyright-1.36.2.tar.gz/basedpyright-1.36.2/packages/pyright-internal/jest.config.js
@@ -0,0 +1,21 @@
+/*
+ * jest.config.js
+ *
+ * Configuration for jest tests.
+ */
+
+module.exports = {
+    testEnvironment: 'node',
+    roots: ['<rootDir>/src/tests'],
+    transform: {
+        '^.+\\.tsx?$': [
+            'ts-jest',
+            {
+                tsconfig: 'src/tests/tsconfig.json',
+            },
+        ],
+    },
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+    setupFiles: ['./src/tests/setupTests.ts'],
+};
