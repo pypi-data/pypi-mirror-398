23b7415d88de8b3a453a1f86e4e3056745cb1078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 07:06:08 +0000

    Add package soprano-tts/0.0.2/soprano_tts-0.0.2.tar.gz

---
+++ packages/soprano-tts/soprano_tts-0.0.2.tar.gz/soprano_tts-0.0.2/README.md
@@ -0,0 +1,166 @@
+<!-- Version 0.0.2 -->
+<div align="center">
+  
+  # Soprano: Instant, Ultra‑Realistic Text‑to‑Speech 
+
+  [![Alt Text](https://img.shields.io/badge/HuggingFace-Model-orange?logo=huggingface)](https://huggingface.co/ekwek/Soprano-80M)
+  [![Alt Text](https://img.shields.io/badge/HuggingFace-Demo-yellow?logo=huggingface)](https://huggingface.co/spaces/ekwek/Soprano-TTS)
+</div>
+
+https://github.com/user-attachments/assets/525cf529-e79e-4368-809f-6be620852826
+
+---
+
+## Overview
+
+**Soprano** is an ultra‑lightweight, open‑source text‑to‑speech (TTS) model designed for real‑time, high‑fidelity speech synthesis at unprecedented speed, all while remaining compact and easy to deploy at **under 1 GB VRAM usage**.
+
+With only **80M parameters**, Soprano achieves a real‑time factor (RTF) of **~2000×**, capable of generating **10 hours of audio in under 20 seconds**. Soprano uses a **seamless streaming** technique that enables true real‑time synthesis in **<15 ms**, multiple orders of magnitude faster than existing TTS pipelines.
+
+---
+
+## Installation
+
+**Requirements**: Linux or Windows, CUDA‑enabled GPU required (CPU support coming soon!).
+
+### Install with wheel
+
+```bash
+pip install soprano-tts
+pip uninstall -y torch
+pip install torch==2.8.0 --index-url https://download.pytorch.org/whl/cu126
+```
+
+### Install from source
+
+```bash
+git clone https://github.com/ekwek1/soprano.git
+cd soprano
+pip install -e .
+pip uninstall -y torch
+pip install torch==2.8.0 --index-url https://download.pytorch.org/whl/cu126
+```
+
+> **Note**: Soprano uses **LMDeploy** to accelerate inference by default. If LMDeploy cannot be installed in your environment, Soprano can fall back to the HuggingFace **transformers** backend (with slower performance). To enable this, pass `backend='transformers'` when creating the TTS model.
+
+---
+
+## Usage
+
+```python
+from soprano import SopranoTTS
+
+model = SopranoTTS(backend='auto', device='cuda', cache_size_mb=10, decoder_batch_size=1)
+```
+
+> **Tip**: You can increase cache_size_mb and decoder_batch_size to increase inference speed at the cost of higher memory usage.
+
+### Basic inference
+
+```python
+out = model.infer("Soprano is an extremely lightweight text to speech model.") # can achieve 2000x real-time with sufficiently long input!
+```
+
+### Save output to a file
+
+```python
+out = model.infer("Soprano is an extremely lightweight text to speech model.", "out.wav")
+```
+
+### Custom sampling parameters
+
+```python
+out = model.infer(
+    "Soprano is an extremely lightweight text to speech model.",
+    temperature=0.3,
+    top_p=0.95,
+    repetition_penalty=1.2,
+)
+```
+
+### Batched inference
+
+```python
+out = model.infer_batch(["Soprano is an extremely lightweight text to speech model."] * 10) # can achieve 2000x real-time with sufficiently large input size!
+```
+
+#### Save batch outputs to a directory
+
+```python
+out = model.infer_batch(["Soprano is an extremely lightweight text to speech model."] * 10, "/dir")
+```
+
+### Streaming inference
+
+```python
+import torch
+
+stream = model.infer_stream("Soprano is an extremely lightweight text to speech model.", chunk_size=1)
+
+# Audio chunks can be accessed via an iterator
+chunks = []
+for chunk in stream:
+    chunks.append(chunk) # first chunk arrives in <15 ms!
+
+out = torch.cat(chunks)
+```
+
+---
+
+## Key Features
+
+### 1. High‑fidelity 32 kHz audio
+
+Soprano synthesizes speech at **32 kHz**, delivering quality that is perceptually indistinguishable from 44.1/48 kHz audio and significantly sharper and clearer than the 24 kHz output used by many existing TTS models.
+
+### 2. Vocoder‑based neural decoder
+
+Instead of slow diffusion decoders, Soprano uses a **vocoder‑based decoder** with a Vocos architecture, enabling **orders‑of‑magnitude faster** waveform generation while maintaining comparable perceptual quality.
+
+### 3. Seamless Streaming
+
+Soprano leverages the decoder’s finite receptive field to losslessly stream audio with ultra‑low latency. The streamed output is acoustically identical to offline synthesis, and streaming can begin after generating just 5 audio tokens, enabling **<15 ms latency**.
+
+### 4. State‑of‑the‑art neural audio codec
+
+Speech is represented using a **neural codec** that compresses audio to **~15 tokens/sec** at just **0.2 kbps**, allowing extremely fast generation and efficient memory usage without sacrificing quality.
+
+### 5. Sentence‑level streaming for infinite context
+
+Each sentence is generated independently, enabling **effectively infinite generation length** while maintaining stability and real‑time performance for long‑form generation.
+
+---
+
+## Limitations
+
+I’m a second-year undergrad who’s just started working on TTS models, so I wanted to start small. Soprano was only pretrained on 1000 hours of audio (~100x less than other TTS models), so its stability and quality will improve tremendously as I train it on more data. Also, I optimized Soprano purely for speed, which is why it lacks bells and whistles like voice cloning, style control, and multilingual support. Now that I have experience creating TTS models, I have a lot of ideas for how to make Soprano even better in the future, so stay tuned for those!
+
+---
+
+## Roadmap
+
+* [x] Add model and inference code
+* [x] Seamless streaming
+* [x] Batched inference
+* [ ] Command-line interface (CLI)
+* [ ] Server / API inference
+* [ ] Additional LLM backends
+* [ ] CPU support
+* [ ] Voice cloning
+* [ ] Multilingual support
+
+---
+
+## Acknowledgements
+
+Soprano uses and/or is inspired by the following projects:
+
+* [Vocos](https://github.com/gemelo-ai/vocos)
+* [XTTS](https://github.com/coqui-ai/TTS)
+* [LMDeploy](https://github.com/InternLM/lmdeploy)
+
+---
+
+## License
+
+This project is licensed under the **Apache-2.0** license. See `LICENSE` for details.
---
+++ packages/soprano-tts/soprano_tts-0.0.2.tar.gz/soprano_tts-0.0.2/soprano/tts.py
@@ -0,0 +1,195 @@
+from .vocos.decoder import SopranoDecoder
+import torch
+import re
+from unidecode import unidecode
+from scipy.io import wavfile
+from huggingface_hub import hf_hub_download
+import os
+import time
+
+
+class SopranoTTS:
+    def __init__(self,
+            backend='auto',
+            device='cuda',
+            cache_size_mb=10,
+            decoder_batch_size=1):
+        RECOGNIZED_DEVICES = ['cuda']
+        RECOGNIZED_BACKENDS = ['auto', 'lmdeploy', 'transformers']
+        assert device in RECOGNIZED_DEVICES, f"unrecognized device {device}, device must be in {RECOGNIZED_DEVICES}"
+        if backend == 'auto':
+            if device == 'cpu':
+                backend = 'transformers'
+            else:
+                try:
+                    import lmdeploy
+                    backend = 'lmdeploy'
+                except ImportError:
+                    backend='transformers'
+            print(f"Using backend {backend}.")
+        assert backend in RECOGNIZED_BACKENDS, f"unrecognized backend {backend}, backend must be in {RECOGNIZED_BACKENDS}"
+
+        if backend == 'lmdeploy':
+            from .backends.lmdeploy import LMDeployModel
+            self.pipeline = LMDeployModel(device=device, cache_size_mb=cache_size_mb)
+        elif backend == 'transformers':
+            from .backends.transformers import TransformersModel
+            self.pipeline = TransformersModel(device=device)
+
+        self.decoder = SopranoDecoder().cuda()
+        decoder_path = hf_hub_download(repo_id='ekwek/Soprano-80M', filename='decoder.pth')
+        self.decoder.load_state_dict(torch.load(decoder_path))
+        self.decoder_batch_size=decoder_batch_size
+        self.RECEPTIVE_FIELD = 4 # Decoder receptive field
+        self.TOKEN_SIZE = 2048 # Number of samples per audio token
+
+        self.infer("Hello world!") # warmup
+
+    def _preprocess_text(self, texts, min_length=30):
+        '''
+        adds prompt format and sentence/part index
+        Enforces a minimum sentence length by merging short sentences.
+        '''
+        res = []
+        for text_idx, text in enumerate(texts):
+            text = text.strip()
+            sentences = re.split(r"(?<=[.!?])\s+", text)
+            processed = []
+            for sentence_idx, sentence in enumerate(sentences):
+                old_len = len(sentence)
+                new_sentence = re.sub(r"[^A-Za-z !\$%&'*+,-./0123456789<>?_]", "", sentence)
+                new_sentence = re.sub(r"[<>/_+]", "", new_sentence)
+                new_sentence = re.sub(r"\.\.[^\.]", ".", new_sentence)
+                new_sentence = re.sub(r"\s+", " ", new_sentence)
+                new_len = len(new_sentence)
+                if old_len != new_len:
+                    print(f"Warning: unsupported characters found in sentence: {sentence}\n\tThese characters have been removed.")
+                new_sentence = unidecode(new_sentence.strip())
+                processed.append({
+                    "text": new_sentence,
+                    "text_idx": text_idx,
+                })
+
+            if min_length > 0 and len(processed) > 1:
+                merged = []
+                i = 0
+                while i < len(processed):
+                    cur = processed[i]
+                    if len(cur["text"]) < min_length:
+                        if merged: merged[-1]["text"] = (merged[-1]["text"] + " " + cur["text"]).strip()
+                        else:
+                            if i + 1 < len(processed): processed[i + 1]["text"] = (cur["text"] + " " + processed[i + 1]["text"]).strip()
+                            else: merged.append(cur)
+                    else: merged.append(cur)
+                    i += 1
+                processed = merged
+            sentence_idxes = {}
+            for item in processed:
+                if item['text_idx'] not in sentence_idxes: sentence_idxes[item['text_idx']] = 0
+                res.append((f'[STOP][TEXT]{item["text"]}[START]', item["text_idx"], sentence_idxes[item['text_idx']]))
+                sentence_idxes[item['text_idx']] += 1
+        return res
+
+    def infer(self,
+            text,
+            out_path=None,
+            top_p=0.95,
+            temperature=0.3,
+            repetition_penalty=1.2):
+        results = self.infer_batch([text],
+            top_p=top_p,
+            temperature=temperature,
+            repetition_penalty=repetition_penalty,
+            out_dir=None)[0]
+        if out_path:
+            wavfile.write(out_path, 32000, results.cpu().numpy())
+        return results
+
+    def infer_batch(self,
+            texts,
+            out_dir=None,
+            top_p=0.95,
+            temperature=0.3,
+            repetition_penalty=1.2):
+        sentence_data = self._preprocess_text(texts)
+        prompts = list(map(lambda x: x[0], sentence_data))
+        responses = self.pipeline.infer(prompts,
+            top_p=top_p,
+            temperature=temperature,
+            repetition_penalty=repetition_penalty)
+        hidden_states = []
+        for i, response in enumerate(responses):
+            if response['finish_reason'] != 'stop':
+                print(f"Warning: some sentences did not complete generation, likely due to hallucination.")
+            hidden_state = response['hidden_state']
+            hidden_states.append(hidden_state)
+        combined = list(zip(hidden_states, sentence_data))
+        combined.sort(key=lambda x: -x[0].size(0))
+        hidden_states, sentence_data = zip(*combined)
+
+        num_texts = len(texts)
+        audio_concat = [[] for _ in range(num_texts)]
+        for sentence in sentence_data:
+            audio_concat[sentence[1]].append(None)
+        for idx in range(0, len(hidden_states), self.decoder_batch_size):
+            batch_hidden_states = []
+            lengths = list(map(lambda x: x.size(0), hidden_states[idx:idx+self.decoder_batch_size]))
+            N = len(lengths)
+            for i in range(N):
+                batch_hidden_states.append(torch.cat([
+                    torch.zeros((1, 512, lengths[0]-lengths[i]), device='cuda'),
+                    hidden_states[idx+i].unsqueeze(0).transpose(1,2).cuda().to(torch.float32),
+                ], dim=2))
+            batch_hidden_states = torch.cat(batch_hidden_states)
+            with torch.no_grad():
+                audio = self.decoder(batch_hidden_states)
+            
+            for i in range(N):
+                text_id = sentence_data[idx+i][1]
+                sentence_id = sentence_data[idx+i][2]
+                audio_concat[text_id][sentence_id] = audio[i].squeeze()[-(lengths[i]*self.TOKEN_SIZE-self.TOKEN_SIZE):]
+        audio_concat = [torch.cat(x).cpu() for x in audio_concat]
+        
+        if out_dir:
+            os.makedirs(out_dir, exist_ok=True)
+            for i in range(len(audio_concat)):
+                wavfile.write(f"{out_dir}/{i}.wav", 32000, audio_concat[i].cpu().numpy())
+        return audio_concat
+
+    def infer_stream(self,
+            text,
+            chunk_size=1,
+            top_p=0.95,
+            temperature=0.3,
+            repetition_penalty=1.2):
+        start_time = time.time()
+        sentence_data = self._preprocess_text([text])
+
+        first_chunk = True
+        for sentence, _, _ in sentence_data:
+            responses = self.pipeline.stream_infer(sentence,
+                top_p=top_p,
+                temperature=temperature,
+                repetition_penalty=repetition_penalty)
+            hidden_states_buffer = []
+            chunk_counter = chunk_size
+            for token in responses:
+                finished = token['finish_reason'] is not None
+                if not finished: hidden_states_buffer.append(token['hidden_state'][-1])
+                hidden_states_buffer = hidden_states_buffer[-(2*self.RECEPTIVE_FIELD+chunk_size):]
+                if finished or len(hidden_states_buffer) >= self.RECEPTIVE_FIELD + chunk_size:
+                    if finished or chunk_counter == chunk_size:
+                        batch_hidden_states = torch.stack(hidden_states_buffer)
+                        inp = batch_hidden_states.unsqueeze(0).transpose(1, 2).cuda().to(torch.float32)
+                        with torch.no_grad():
+                            audio = self.decoder(inp)[0]
+                        if finished:
+                            audio_chunk = audio[-((self.RECEPTIVE_FIELD+chunk_counter-1)*self.TOKEN_SIZE-self.TOKEN_SIZE):]
+                        else:
+                            audio_chunk = audio[-((self.RECEPTIVE_FIELD+chunk_size)*self.TOKEN_SIZE-self.TOKEN_SIZE):-(self.RECEPTIVE_FIELD*self.TOKEN_SIZE-self.TOKEN_SIZE)]
+                        chunk_counter = 0
+                        if first_chunk:
+                            print(f"Streaming latency: {1000*(time.time()-start_time):.2f} ms")
+                            first_chunk = False
+                        yield audio_chunk.cpu()
+                    chunk_counter += 1
