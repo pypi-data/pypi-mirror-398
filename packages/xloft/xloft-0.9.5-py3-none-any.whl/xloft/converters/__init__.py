e7b124c4193a5a8a056c2a16adc070486db7611c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 03:31:56 +0000

    Add package xloft/0.9.5/xloft-0.9.5-py3-none-any.whl

---
+++ packages/xloft/xloft-0.9.5-py3-none-any.whl/xloft/converters/__init__.py
@@ -0,0 +1,19 @@
+"""Collection of tools for converting data.
+
+The module contains the following functions:
+
+- `to_human_size(n_bytes)` - Returns a humanized string: 200 bytes | 1 KB | 1.5 MB etc.
+- `int_to_roman` - Convert an integer to Roman.
+- `roman_to_int` - Convert to integer from Roman.
+"""
+
+from __future__ import annotations
+
+__all__ = (
+    "to_human_size",
+    "int_to_roman",
+    "roman_to_int",
+)
+
+from xloft.converters.human_size import to_human_size
+from xloft.converters.roman import int_to_roman, roman_to_int
