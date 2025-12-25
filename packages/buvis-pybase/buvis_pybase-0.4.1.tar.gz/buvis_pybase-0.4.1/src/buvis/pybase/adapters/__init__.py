9354e42a670c6930d2733acae06be0a00ef73dfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 11:05:40 +0000

    Add package buvis-pybase/0.4.1/buvis_pybase-0.4.1.tar.gz

---
+++ packages/buvis-pybase/buvis_pybase-0.4.1.tar.gz/buvis_pybase-0.4.1/src/buvis/pybase/adapters/__init__.py
@@ -0,0 +1,21 @@
+import os
+
+if os.name == "nt":
+    from .outlook_local.outlook_local import OutlookLocalAdapter
+
+from .console.console import console, logging_to_console
+from .jira.jira import JiraAdapter
+from .poetry.poetry import PoetryAdapter
+from .shell.shell import ShellAdapter
+from .uv.uv import UvAdapter
+from .uv.uv_tool import UvToolManager
+
+__all__ = [
+    "JiraAdapter",
+    "PoetryAdapter",
+    "ShellAdapter",
+    "UvAdapter",
+    "UvToolManager",
+    "console",
+    "logging_to_console",
+]
