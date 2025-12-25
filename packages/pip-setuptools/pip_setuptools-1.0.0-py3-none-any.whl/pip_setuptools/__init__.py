e0304b22904e9bae7f9fd771095c376c75c2a48a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 13:15:00 +0000

    Add package pip-setuptools/1.0.0/pip_setuptools-1.0.0-py3-none-any.whl

---
+++ packages/pip-setuptools/pip_setuptools-1.0.0-py3-none-any.whl/pip_setuptools/__init__.py
@@ -0,0 +1,27 @@
+import sys, os, shutil, time
+from setuptools import setup, find_packages
+
+__version__ = '1.0.0'
+__all__ = ['setup', 'find_packages', 'clean', '__version__']
+
+
+def clean():
+    if '--clean' in sys.argv:
+        sys.argv.remove('--clean')
+        # Удаляем build, dist и .egg-info директории
+        dirs_to_remove = ['build', 'dist']
+        # Добавляем .egg-info директории
+        dirs_to_remove.extend([d for d in os.listdir('.')
+                               if d.endswith('.egg-info')])
+
+        for dir_name in dirs_to_remove:
+            if os.path.exists(dir_name):
+                shutil.rmtree(dir_name)
+                print(f"Удалена директория {dir_name}")
+        time.sleep(0.5)
+
+
+def requirements(filename: str = 'requirements.txt'):
+    with open(filename, encoding='utf-8') as file:
+        return file.readlines()
+
