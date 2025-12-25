77d4fa63049357c6a1a6c16b1d4d1581b829ecd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 03:31:53 +0000

    Add package square_database/3.4.0/square_database-3.4.0-py3-none-any.whl

---
+++ packages/square_database/square_database-3.4.0-py3-none-any.whl/square_database/models/core.py
@@ -0,0 +1,76 @@
+from typing import Dict, Any, List, Optional
+
+from pydantic import BaseModel, conlist, Field, RootModel
+
+
+class FilterConditionsV0(BaseModel):
+    eq: Optional[Any] = None
+    ne: Optional[Any] = None
+    lt: Optional[Any] = None
+    lte: Optional[Any] = None
+    gt: Optional[Any] = None
+    gte: Optional[Any] = None
+    like: Optional[str] = None
+    in_: Optional[List[Any]] = None
+    is_null: Optional[bool] = None
+
+
+class FiltersV0(RootModel):
+    root: Dict[str, FilterConditionsV0]
+
+
+class InsertRowsV0(BaseModel):
+    database_name: str
+    table_name: str
+    schema_name: str
+    data: conlist(Dict[str, Any], min_length=1)
+    skip_conflicts: bool = False
+
+
+class InsertRowsV0Response(BaseModel):
+    main: List[Dict[str, Any]]
+    affected_count: int
+
+
+class GetRowsV0(BaseModel):
+    database_name: str
+    table_name: str
+    schema_name: str
+    filters: FiltersV0
+    apply_filters: bool = True
+    columns: Optional[List[str]] = None
+    order_by: List[str] = Field(default_factory=list)
+    limit: Optional[int] = None
+    offset: int = 0
+
+
+class GetRowsV0Response(BaseModel):
+    main: List[Dict[str, Any]]
+    total_count: int
+
+
+class EditRowsV0(BaseModel):
+    database_name: str
+    table_name: str
+    schema_name: str
+    filters: FiltersV0
+    data: Dict[str, Any]
+    apply_filters: bool = True
+
+
+class EditRowsV0Response(BaseModel):
+    main: List[Dict[str, Any]]
+    affected_count: int
+
+
+class DeleteRowsV0(BaseModel):
+    database_name: str
+    table_name: str
+    schema_name: str
+    filters: FiltersV0
+    apply_filters: bool = True
+
+
+class DeleteRowsV0Response(BaseModel):
+    main: List[Dict[str, Any]]
+    affected_count: int
---
+++ packages/square_database/square_database-3.4.0-py3-none-any.whl/square_database/utils/common_operations.py
@@ -0,0 +1,72 @@
+from enum import Enum
+
+from square_database.configuration import global_object_square_logger
+
+
+@global_object_square_logger.auto_logger()
+def snake_to_capital_camel(snake_str):
+    try:
+        components = snake_str.split("_")
+        # Capitalize the first letter of each component except the first one
+        camel_case = "".join(x.title() for x in components)
+        return camel_case
+    except Exception:
+        raise
+
+
+@global_object_square_logger.auto_logger()
+def apply_order_by(query, order_by, table_class):
+    try:
+        if order_by:
+            order_by_columns = [
+                (
+                    getattr(table_class, col[1:]).desc()
+                    if col.startswith("-")
+                    else getattr(table_class, col).asc()
+                )
+                for col in order_by
+            ]
+            query = query.order_by(*order_by_columns)
+        return query
+    except Exception:
+        raise
+
+
+@global_object_square_logger.auto_logger()
+def apply_filters(query, filters_root, table_class):
+    try:
+        for key, condition in filters_root.items():
+            column = getattr(table_class, key, None)
+            if not column:
+                raise Exception(f"Invalid Column: {key}")
+
+            if condition.eq is not None:
+                query = query.where(column == condition.eq)
+            elif condition.ne is not None:
+                query = query.where(column != condition.ne)
+            elif condition.lt is not None:
+                query = query.where(column < condition.lt)
+            elif condition.lte is not None:
+                query = query.where(column <= condition.lte)
+            elif condition.gt is not None:
+                query = query.where(column > condition.gt)
+            elif condition.gte is not None:
+                query = query.where(column >= condition.gte)
+            elif condition.like is not None:
+                query = query.where(column.like(condition.like))
+            elif condition.in_ is not None:
+                query = query.where(column.in_(condition.in_))
+            elif condition.is_null is not None:
+                if condition.is_null:
+                    query = query.where(column.is_(None))
+                else:
+                    query = query.where(column.is_not(None))
+        return query
+    except Exception:
+        raise
+
+
+def enum_fallback_serializer(obj):
+    if isinstance(obj, Enum):
+        return obj.value
+    return str(obj)
