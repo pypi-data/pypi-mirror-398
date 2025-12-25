dbad7133c5e95547c985c5f3ec00b90fa4ff920f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 07:06:07 +0000

    Add package mcif-bench/1.0/mcif_bench-1.0-py3-none-any.whl

---
+++ packages/mcif-bench/mcif_bench-1.0-py3-none-any.whl/mcif/__init__.py
@@ -0,0 +1,17 @@
+# Copyright 2025 FBK, KIT
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License
+__version__ = '1.0'
+__benchmark_version__ = '1.2'
+
+_HF_BASE_REPO = "https://huggingface.co/datasets/FBK-MT/MCIF/"
