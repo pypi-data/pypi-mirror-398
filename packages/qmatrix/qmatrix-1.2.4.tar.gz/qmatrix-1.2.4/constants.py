6edceb2032fa2904f1ae7b9673c87f14ee809f49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 03:31:57 +0000

    Add package qmatrix/1.2.4/qmatrix-1.2.4.tar.gz

---
+++ packages/qmatrix/qmatrix-1.2.4.tar.gz/qmatrix-1.2.4/constants.py
@@ -0,0 +1,35 @@
+# Half-width Katakana (Standard Matrix Rain Charset)
+KATAKANA = "".join(chr(i) for i in range(0xFF66, 0xFF9E))
+
+ANTONYMS = {
+    "LOVE": "HATE",
+    "LIFE": "DEATH",
+    "DARK": "LIGHT",
+    "SAFE": "DANGER",
+    "HELP": "KILL",
+    "TRUTH": "LIE",
+    "STAY": "LEAVE",
+    "PEACE": "WAR",
+    "GOD": "DEVIL",
+    "HEAVEN": "HELL",
+    "SMILE": "CRY",
+    "FRIEND": "ENEMY",
+    "OPEN": "CLOSE",
+    "BIRTH": "VOID",
+    "WAKE": "SLEEP"
+}
+
+QUOTES = [
+    "WAKE UP",
+    "I SEE YOU",
+    "DO NOT LOOK BACK",
+    "THEY ARE COMING",
+    "ITS ALL A LIE",
+    "YOU ARE NOT ALONE",
+    "SYSTEM FAILURE",
+    "FOLLOW THE WHITE RABBIT",
+    "REALITY IS BROKEN",
+    "EMBRACE THE VOID",
+    "THE MATRIX HAS YOU",
+    "KNOCK KNOCK NEO"
+]
