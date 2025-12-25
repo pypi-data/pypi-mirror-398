6c6cd87193e6d610bd13097a4cced65902f68ddc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 03:31:52 +0000

    Add package techchallenge-summarizer/2025.12.21164129/techchallenge_summarizer-2025.12.21164129-py3-none-any.whl

---
+++ packages/techchallenge-summarizer/techchallenge_summarizer-2025.12.21164129-py3-none-any.whl/techchallenge_summarizer/__init__.py
@@ -0,0 +1,4 @@
+from .main import techchallenge_summarizer
+from .prompts import human_prompt, pattern, system_prompt
+
+__all__ = ["techchallenge_summarizer", "system_prompt", "human_prompt", "pattern"]
