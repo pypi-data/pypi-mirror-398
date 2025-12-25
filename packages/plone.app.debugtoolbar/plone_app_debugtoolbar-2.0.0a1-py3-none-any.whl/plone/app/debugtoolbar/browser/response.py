59972056aec94bdc1ff46b532ea4b8cd74af1a47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 12:07:06 +0000

    Add package plone.app.debugtoolbar/2.0.0a1/plone_app_debugtoolbar-2.0.0a1-py3-none-any.whl

---
+++ packages/plone.app.debugtoolbar/plone_app_debugtoolbar-2.0.0a1-py3-none-any.whl/plone/app/debugtoolbar/browser/response.py
@@ -0,0 +1,28 @@
+from plone.app.debugtoolbar.delayedwrite import delay
+from zope.viewlet.viewlet import ViewletBase
+
+
+def captureHeaders(request):
+    headers = []
+
+    for k in sorted(request.response.headers):
+        header = "-".join([x.capitalize() for x in k.split("-")])
+        headers.append(
+            (
+                header,
+                request.response.headers[k],
+            )
+        )
+
+    return headers
+
+
+def captureStatus(request):
+    return request.response.status
+
+
+class ResponseViewlet(ViewletBase):
+
+    def update(self):
+        delay(self.request, "response_headers", captureHeaders)
+        delay(self.request, "response_status", captureStatus)
