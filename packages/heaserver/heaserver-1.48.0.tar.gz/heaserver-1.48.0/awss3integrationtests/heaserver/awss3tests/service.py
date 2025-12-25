f9713051bd73eaae4412cf322f3e1a2a50aafe22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 08:04:24 +0000

    Add package heaserver/1.48.0/heaserver-1.48.0.tar.gz

---
+++ packages/heaserver/heaserver-1.48.0.tar.gz/heaserver-1.48.0/awss3integrationtests/heaserver/awss3tests/service.py
@@ -0,0 +1,34 @@
+from heaserver.service.runner import routes
+from heaserver.service.db import awsservicelib
+from heaserver.service import response
+from heaserver.service.appproperty import HEA_DB
+from aiohttp import web
+
+
+@routes.get('/accounts/{id}')
+async def get_volume_id(request: web.Request) -> web.Response:
+    volume_id = await awsservicelib.get_volume_id_for_account_id(request)
+    if volume_id is not None:
+        return response.status_ok(body=volume_id)
+    else:
+        return response.status_not_found()
+
+
+@routes.get('/properties/{id}')
+async def get_properties_id(request: web.Request) -> web.Response:
+    property = await request.app[HEA_DB].get_property(request.app, request.match_info['id'])
+    if property is not None:
+        return response.status_ok(body=property.to_json())
+    else:
+        return response.status_not_found()
+
+
+@routes.post('/volumes/{volume_id}/buckets/{bucket_id}/awss3folders/{id}/duplicator')
+async def post_folder_duplicator(request: web.Request) -> web.Response:
+    return await awsservicelib.copy_object(request)
+
+
+@routes.post('/volumes/{volume_id}/buckets/{bucket_id}/awss3folders/')
+@routes.post('/volumes/{volume_id}/buckets/{bucket_id}/awss3folders')
+async def post_folder(request: web.Request) -> web.Response:
+    return await awsservicelib.create_object(request)
---
+++ packages/heaserver/heaserver-1.48.0.tar.gz/heaserver-1.48.0/tests/heaserver/servicetest/test_put_organization_permissions.py
@@ -0,0 +1,8 @@
+from .componenttestcase import ComponentTestCase
+from heaserver.service.testcase.mixin import PutMixin
+from .organizationtestcase import OrganizationTestCase
+
+
+class TestPutOrganization(OrganizationTestCase, PutMixin):  # type: ignore
+    pass
+
