6467ab59fde7564885923c1fddf8c9902de4c63f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 11:05:33 +0000

    Add package topdogalerts/1.0.4/topdogalerts-1.0.4.tar.gz

---
+++ packages/topdogalerts/topdogalerts-1.0.4.tar.gz/topdogalerts-1.0.4/topdogalerts/managers/__init__.py
@@ -0,0 +1,22 @@
+# topdogalerts/managers/__init__.py
+"""
+Database manager functions for topdogalerts.
+
+Provides functions for fetching and updating database records.
+"""
+from .source import fetch_source
+from .event_type import fetch_event_type, set_listener_health
+from .event_trigger import (
+    fetch_event_trigger,
+    fetch_event_triggers_for_eventtype,
+    update_event_trigger_column,
+)
+
+__all__ = [
+    "fetch_source",
+    "fetch_event_type",
+    "set_listener_health",
+    "fetch_event_trigger",
+    "fetch_event_triggers_for_eventtype",
+    "update_event_trigger_column",
+]
