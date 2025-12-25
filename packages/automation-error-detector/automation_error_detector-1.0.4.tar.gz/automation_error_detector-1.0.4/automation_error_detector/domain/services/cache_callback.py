d8d9ca1f4be629888e3a38b2d737d6a9b2a92de5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 05:06:38 +0000

    Add package automation-error-detector/1.0.4/automation_error_detector-1.0.4.tar.gz

---
+++ packages/automation-error-detector/automation_error_detector-1.0.4.tar.gz/automation_error_detector-1.0.4/automation_error_detector/domain/services/cache_callback.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+
+class CacheSaveCallback(ABC):
+    """
+    User-defined cache persistence
+    """
+
+    @abstractmethod
+    def save(self, signature: str, data: dict) -> None:
+        """
+        Persist cache data anywhere (DB, Redis, file, etc.)
+        """
+        pass
+
+    @abstractmethod
+    def load(self, signature: str) -> dict | None:
+        """
+        Load cached data by signature
+        """
+        pass
