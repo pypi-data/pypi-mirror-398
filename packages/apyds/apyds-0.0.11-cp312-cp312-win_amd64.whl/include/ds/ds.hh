c1cfd91a8f188f25035755a2afd16c098c1c222d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 06:05:38 +0000

    Add package apyds/0.0.11/apyds-0.0.11-cp312-cp312-win_amd64.whl

---
+++ packages/apyds/apyds-0.0.11-cp312-cp312-win_amd64.whl/include/ds/ds.hh
@@ -0,0 +1,11 @@
+#ifndef DS_HH
+#define DS_HH
+
+#include <ds/item.hh>
+#include <ds/list.hh>
+#include <ds/rule.hh>
+#include <ds/string.hh>
+#include <ds/term.hh>
+#include <ds/variable.hh>
+
+#endif
