050d8eba87d79f9fc38703d164cb2dbfa18920e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 13:15:23 +0000

    Add package poiidx/0.0.5/poiidx-0.0.5-py3-none-any.whl

---
+++ packages/poiidx/poiidx-0.0.5-py3-none-any.whl/poiidx/geofabrik.py
@@ -0,0 +1,19 @@
+import requests
+
+from .__about__ import __version__
+from .system import System
+
+HEADERS = {"User-Agent": f"poiidx/{__version__} (https://github.com/bytehexe/poiidx)"}
+
+
+def download_region_data() -> None:
+    response = requests.get(
+        "https://download.geofabrik.de/index-v1.json", headers=HEADERS
+    )
+    response.raise_for_status()
+    geofabrik_data = response.text
+
+    # Save the region index to the system model
+    system, created = System.get_or_create(system=True)
+    system.region_index = geofabrik_data
+    system.save()
