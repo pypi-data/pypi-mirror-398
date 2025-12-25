47e8907957fd2b716e6a34e4b83fd5504f9fffd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 03:31:56 +0000

    Add package qmatrix/1.2.3/qmatrix-1.2.3.tar.gz

---
+++ packages/qmatrix/qmatrix-1.2.3.tar.gz/qmatrix-1.2.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="qmatrix",
+    version="1.2.3",
+    description="A high-performance, aesthetically pleasing Matrix rain TUI with horror elements.",
+    author="Rex Ackermann",
+    author_email="rex@example.com",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/rexackermann/qmatrix",
+    py_modules=["qmatrix", "constants"],
+    install_requires=[
+        "requests",
+    ],
+    entry_points={
+        "console_scripts": [
+            "qmatrix=qmatrix:main_wrapper",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    ],
+)
