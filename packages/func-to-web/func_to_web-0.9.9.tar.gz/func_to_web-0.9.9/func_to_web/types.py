fdf407575d52552d4c6473039f1d2328271981ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 12:06:28 +0000

    Add package func-to-web/0.9.9/func_to_web-0.9.9.tar.gz

---
+++ packages/func-to-web/func_to_web-0.9.9.tar.gz/func_to_web-0.9.9/func_to_web/types.py
@@ -0,0 +1,39 @@
+from typing import Annotated
+from pydantic import Field, BaseModel
+from datetime import date, time
+
+COLOR_PATTERN = r'^#(?:[0-9a-fA-F]{3}){1,2}$'
+EMAIL_PATTERN = r'^[^@]+@[^@]+\.[^@]+$'
+ANY_FILE_PATTERN = r'^.+$'
+
+def _file_pattern(*extensions):
+    """Generate regex pattern for file extensions."""
+    exts = [e.lstrip('.').lower() for e in extensions]
+    return r'^.+\.(' + '|'.join(exts) + r')$'
+
+
+Color = Annotated[str, Field(pattern=COLOR_PATTERN)]
+Email = Annotated[str, Field(pattern=EMAIL_PATTERN)]
+ImageFile = Annotated[str, Field(pattern=_file_pattern('png', 'jpg', 'jpeg', 'gif', 'webp', 'bmp', 'tiff', 'svg', 'ico', 'heic', 'avif'))]
+DataFile = Annotated[str, Field(pattern=_file_pattern('csv', 'xlsx', 'xls', 'json', 'xml', 'yaml', 'yml'))]
+TextFile = Annotated[str, Field(pattern=_file_pattern('txt', 'md', 'log', 'rtf'))]
+DocumentFile = Annotated[str, Field(pattern=_file_pattern('pdf', 'doc', 'docx', 'odt', 'ppt', 'pptx', 'odp', 'xls', 'xlsx', 'ods'))]
+File = Annotated[str, Field(pattern=ANY_FILE_PATTERN)]
+
+class _OptionalEnabledMarker:
+    """Internal marker for OptionalEnabled"""
+    pass
+
+class _OptionalDisabledMarker:
+    """Internal marker for OptionalDisabled"""
+    pass
+
+
+class FileResponse(BaseModel):
+    """Model for file response."""
+    data: bytes
+    filename: Annotated[str, Field(max_length=150)]
+
+
+OptionalEnabled = Annotated[None, _OptionalEnabledMarker()]
+OptionalDisabled = Annotated[None, _OptionalDisabledMarker()]
