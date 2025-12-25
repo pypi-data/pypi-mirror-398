ef96d41819f9eb186019e4af6d14ff2ec1a0c454
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 07:06:09 +0000

    Add package mlmonitor/1.0.30/mlmonitor-1.0.30.tar.gz

---
+++ packages/mlmonitor/mlmonitor-1.0.30.tar.gz/mlmonitor-1.0.30/mlmonitor/src/wml/__init__.py
@@ -0,0 +1,37 @@
+# SPDX-License-Identifier: Apache-2.0
+from ibm_watsonx_ai import APIClient as WML_APIClient
+from mlmonitor.src import API_KEY, ENV, logger
+from mlmonitor.config import get_wml_details
+
+WML_INSTANCE_ID, WML_URL, WML_SPACE_ID = get_wml_details(env=ENV)
+WML_CREDENTIALS = {"url": WML_URL}
+
+SUPPORTED_WML_RUNTIMES = {
+    "runtime-22.1-py3.10",
+    "runtime-22.2-py3.10",
+    "runtime-24.1-py3.11",
+    "spark-mllib_3.3",
+    "runtime-24.1-py3.11",
+    "runtime-24.1-py3.11",
+    "tensorflow_rt22.1-py3.9",
+    "tensorflow_rt22.1-py3.10",
+}
+
+if ENV == "prem":
+    from mlmonitor.src import CP4D_VERSION, USERNAME
+
+    WML_CREDENTIALS["username"] = USERNAME
+    # WML_CREDENTIALS["password"] = API_KEY
+    WML_CREDENTIALS["apikey"] = API_KEY
+    WML_CREDENTIALS["instance_id"] = WML_INSTANCE_ID
+    WML_CREDENTIALS["version"] = CP4D_VERSION
+elif ENV == "saas":
+    WML_CREDENTIALS["apikey"] = API_KEY
+
+try:
+    logger.debug("Instantiate WML Client")
+    wml_client = WML_APIClient(credentials=WML_CREDENTIALS)
+    wml_client.set.default_space(WML_SPACE_ID)
+except Exception as e:
+    wml_client = None
+    logger.warning(f"Error to instantiate WML Client : {e}")
