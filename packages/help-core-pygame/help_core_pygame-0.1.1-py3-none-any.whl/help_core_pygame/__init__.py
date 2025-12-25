a152ee0c83266131af29f47f2c1bd5058d1caa35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 06:05:04 +0000

    Add package help-core-pygame/0.1.1/help_core_pygame-0.1.1-py3-none-any.whl

---
+++ packages/help-core-pygame/help_core_pygame-0.1.1-py3-none-any.whl/help_core_pygame/__init__.py
@@ -0,0 +1,8 @@
+# Contenido de src/help_core_pygame/__init__.py
+
+# Re-exportar las funciones/clases principales de help_core.py (se usarán en las demos de examples/)
+from .help_core import open_help_standalone
+from .help_core import HelpConfig, HelpViewer 
+
+# En caso de querer re-exportar otros módulos se podría hacer aquí igualmente 
+
