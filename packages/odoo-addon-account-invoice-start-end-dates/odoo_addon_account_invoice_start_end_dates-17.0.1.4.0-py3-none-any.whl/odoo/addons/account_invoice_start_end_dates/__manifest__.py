8f1ee714fd7c380c3fc6acc6c81c60428c86bf5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 06:05:49 +0000

    Add package odoo-addon-account-invoice-start-end-dates/17.0.1.4.0/odoo_addon_account_invoice_start_end_dates-17.0.1.4.0-py3-none-any.whl

---
+++ packages/odoo-addon-account-invoice-start-end-dates/odoo_addon_account_invoice_start_end_dates-17.0.1.4.0-py3-none-any.whl/odoo/addons/account_invoice_start_end_dates/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2016-2021 Akretion France (http://www.akretion.com/)
+# Copyright 2018-2021 Camptocamp
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# License LGPL-3 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Account Invoice Start End Dates",
+    "version": "17.0.1.4.0",
+    "category": "Accounting & Finance",
+    "license": "LGPL-3",
+    "summary": "Adds start/end dates on invoice/move lines",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "maintainers": ["alexis-via"],
+    "website": "https://github.com/OCA/account-closing",
+    "depends": ["account"],
+    "data": [
+        "views/account_move.xml",
+        "views/account_move_line.xml",
+        "views/product_template.xml",
+    ],
+    "demo": ["demo/product_demo.xml"],
+    "installable": True,
+}
