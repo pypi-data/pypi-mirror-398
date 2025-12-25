e3beebd888a54f2bccb2e359dc77bdffca3af234
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 12:07:03 +0000

    Add package plone.jsonserializer/1.0.0a1/plone_jsonserializer-1.0.0a1.tar.gz

---
+++ packages/plone.jsonserializer/plone_jsonserializer-1.0.0a1.tar.gz/plone_jsonserializer-1.0.0a1/README.rst
@@ -0,0 +1,5 @@
+plone.jsonserializer
+====================
+
+
+JSON Serialization/Deserialization resources for Plone
---
+++ packages/plone.jsonserializer/plone_jsonserializer-1.0.0a1.tar.gz/plone_jsonserializer-1.0.0a1/setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup
+
+
+version = "1.0.0a1"
+
+long_description = open("README.rst").read() + "\n" + open("CHANGES.rst").read() + "\n"
+
+setup(
+    name="plone.jsonserializer",
+    version=version,
+    description="JSON serialization/deserialization adapters for Plone.",
+    long_description=long_description,
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 6.2",
+        "Framework :: Plone :: Addon",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords="plone json serializer",
+    author="Plone Foundation",
+    author_email="foundation@plone.org",
+    url="https://github.com/plone/plone.jsonserializer/",
+    license="gpl",
+    include_package_data=True,
+    python_requires=">=3.10",
+    zip_safe=False,
+    install_requires=[
+        "setuptools",
+    ],
+    extras_require={
+        "test": [
+            "plone.app.testing >= 4.2.2",
+        ]
+    },
+    entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+)
