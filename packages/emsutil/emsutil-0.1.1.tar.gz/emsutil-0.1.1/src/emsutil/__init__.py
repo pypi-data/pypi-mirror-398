db113682700371ff5684ac1926d12a6a6b2c0ad2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 03:31:45 +0000

    Add package emsutil/0.1.1/emsutil-0.1.1.tar.gz

---
+++ packages/emsutil/emsutil-0.1.1.tar.gz/emsutil-0.1.1/src/emsutil/__init__.py
@@ -0,0 +1,6 @@
+from .plot.plot2d import plot, plot_ff, plot_ff_polar, plot_sp, plot_vswr, smith
+from . import lib
+from .const import C0, Z0, PI, MU0, EPS0
+import isola
+import rogers
+from .emdata import FarFieldComponent, FarFieldData, EHField
