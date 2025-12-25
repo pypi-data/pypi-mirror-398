a313c3e61a58c6882fd1ac4e3171ba1414f8bf5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 07:06:20 +0000

    Add package linksocks/1.7.6/linksocks-1.7.6-cp310-cp310-manylinux1_x86_64.manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_5_x86_64.whl

---
+++ packages/linksocks/linksocks-1.7.6-cp310-cp310-manylinux1_x86_64.manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_5_x86_64.whl/linksocks/__init__.py
@@ -0,0 +1,9 @@
+"""linksocks: SOCKS5 over WebSocket proxy library."""
+
+__version__ = "1.7.6"
+
+from ._server import Server
+from ._client import Client
+from ._base import ReverseTokenResult, set_log_level
+
+__all__ = ["Server", "Client", "ReverseTokenResult", "set_log_level"]
