15196aad106442e2601ea19ee365b8e25d5339e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 13:14:58 +0000

    Add package django-invitelink/0.4.0/django_invitelink-0.4.0-py3-none-any.whl

---
+++ packages/django-invitelink/django_invitelink-0.4.0-py3-none-any.whl/django_invitelink/views.py
@@ -0,0 +1,25 @@
+from django.conf import settings
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import get_object_or_404
+from django.views.generic.edit import CreateView
+
+from django_invitelink.models import InviteToken
+from django_invitelink.signals import consume_invite
+
+
+class Invite(CreateView):
+    form_class = UserCreationForm
+    template_name = "django_invitelink/invite.html"
+    success_url = settings.LOGIN_URL
+
+    def setup(self, *args, **kwargs):
+        super().setup(*args, **kwargs)
+        self.invite = get_object_or_404(InviteToken, id=kwargs.get("invite"))
+
+    def form_valid(self, form):
+        ret = super().form_valid(form)
+        self.invite.delete()
+        consume_invite.send(sender=self.object)
+        messages.success(self.request, f"Created user {self.object}")
+        return ret
