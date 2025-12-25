cfd11b335546906179e7db6d8fe1ff903aa5b46e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 06:05:03 +0000

    Add package polytext/0.1.5b3/polytext-0.1.5b3-py3-none-any.whl

---
+++ packages/polytext/polytext-0.1.5b3-py3-none-any.whl/polytext/loader/__init__.py
@@ -0,0 +1,14 @@
+# polytext/loader/__init__.py
+from .document import DocumentLoader
+from .video import VideoLoader
+from .audio import AudioLoader
+from .youtube import YoutubeTranscriptLoader
+from .html import HtmlLoader
+from .ocr import OCRLoader
+from .markdown import MarkdownLoader
+from .document_ocr import DocumentOCRLoader
+from .plain_text import PlainTextLoader
+from .youtube_llm import YoutubeTranscriptLoaderWithLlm
+from .base import BaseLoader
+
+__all__ = ['DocumentLoader', 'VideoLoader', 'AudioLoader', 'HtmlLoader', 'YoutubeTranscriptLoader', 'OCRLoader', 'MarkdownLoader', 'DocumentOCRLoader', 'PlainTextLoader', 'YoutubeTranscriptLoaderWithLlm', 'BaseLoader']
