6f9be6b0463087640036a9bb4471d67c5465a768
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 06:05:15 +0000

    Add package dns-manager/1.0.0/dns_manager-1.0.0-py310-none-any.whl

---
+++ packages/dns-manager/dns_manager-1.0.0-py310-none-any.whl/dns_manager/__main__.py
@@ -0,0 +1,100 @@
+from __future__ import annotations
+
+import asyncio
+import os
+from pathlib import Path
+
+import typer
+from loguru import logger
+from rich.console import Console
+from rich.highlighter import NullHighlighter
+from rich.live import Live
+from rich.logging import RichHandler
+from rich.progress import Progress, TextColumn
+from rich.style import Style
+
+from dns_manager.utils import create_setter_by_config, load_config_from_path
+
+app = typer.Typer()
+
+
+@app.callback()
+def main(
+    log_level: str = "INFO",
+):
+    log_level = os.environ.get("LOG_LEVEL", log_level)
+    handler = RichHandler(
+        console=Console(style=Style()), highlighter=NullHighlighter(), markup=True
+    )
+    logger.remove()
+    logger.add(handler, format="{message}", level=log_level)
+
+
+@app.command()
+def update(
+    path: Path,
+    remove_unmanaged: bool = False,
+):
+    logger.info("Loading dns config from [bold purple]{:}[/].".format(path))
+    configs = load_config_from_path(path)
+    for config in configs:
+        setter_obj = create_setter_by_config(config)
+        setter_obj.update_dns(remove_unmanaged=remove_unmanaged)
+
+
+@app.command()
+def watch(
+    path: Path,
+):
+    path = path.expanduser()
+    asyncio.run(watch_async(path))
+
+
+async def watch_async(path: Path):
+    import watchfiles
+
+    async def watch_config():
+        async for _ in watchfiles.awatch(path):
+            configs = load_config_from_path(path)
+
+            for config in configs:
+                setter_obj = create_setter_by_config(config)
+                setter_obj.update_dns()
+            progress.update(
+                countdown, completed=interval, description="Update Timer([bold]Reloading Config[/])"
+            )
+
+    async def update_dns():
+        while True:
+            for setter_obj in setter_objs:
+                setter_obj.update_dns()
+            progress.reset(countdown, description="Update Timer")
+
+            with Live(progress):
+                while not progress.finished:
+                    progress.update(countdown, advance=1)
+                    await asyncio.sleep(1)
+
+    logger.info("Loading dns config from [bold purple]{:}[/].".format(path))
+    # TODO: configurable interval
+    # interval = int(config.get("interval", 300))
+    interval = 300
+    progress = Progress(
+        TextColumn("{task.description}: [bold blue]{task.completed}s/{task.total}s[/]"),
+        transient=True,
+    )
+    countdown = progress.add_task("Update Timer", total=interval)
+
+    configs = load_config_from_path(path)
+    setter_objs = [create_setter_by_config(config) for config in configs]
+
+    update_task = asyncio.create_task(update_dns())
+    watch_task = asyncio.create_task(watch_config())
+    await asyncio.gather(
+        update_task,
+        watch_task,
+    )
+
+
+if __name__ == "__main__":
+    app()
---
+++ packages/dns-manager/dns_manager-1.0.0-py310-none-any.whl/dns_manager/getter/snmp.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+
+import os
+from collections.abc import Iterable
+
+from pyparsing import Combine, Group, OneOrMore, ParserElement, Suppress, Word, alphanums, nums
+
+from .base import IPGetterBase
+from .utils import get_default_gateway_ip
+
+
+class SnmpGetter(IPGetterBase):
+    mib_prefix = Suppress(Word(alphanums) + "-" + "MIB::" + Word(alphanums) + ".")
+    name_prefix = Suppress("STRING:")
+    integer_prefix = Suppress("INTEGER:")
+    equal_sign = Suppress("=")
+
+    interface_id = Word(nums).set_results_name("interface_id")
+    interface_name = Word(alphanums + "_").set_results_name("interface")
+
+    ifname_pattern = OneOrMore(
+        Group(mib_prefix + interface_id + equal_sign + name_prefix + interface_name)
+    )
+
+    ip_address = Combine(Word(nums) + ("." + Word(nums)) * 3).set_results_name("ip_address")
+    ip_pattern = OneOrMore(
+        Group(mib_prefix + ip_address + equal_sign + integer_prefix + interface_id)
+    )
+
+    def __init__(self, interface: str):
+        self.group = "public"
+        self.host = get_default_gateway_ip()
+
+        self.interface = interface
+
+    def walk(self, oid: str, pattern: ParserElement | None = None) -> Iterable[str]:
+        cmd = "snmpwalk -v 2c -c {:} {:} {:}".format(self.group, self.host, oid)
+        results = os.popen(cmd)
+        if pattern is None:
+            return results
+        else:
+            return pattern.parse_string(results.read())
+
+    def get_ip(self) -> str:
+        mapping_name_to_id = {
+            interface_name: interface_id
+            for interface_id, interface_name in self.walk("ifname", self.ifname_pattern)
+        }
+        mapping_id_to_ip = {
+            interface_id: ip_address
+            for ip_address, interface_id in self.walk(".1.3.6.1.2.1.4.20.1.2", self.ip_pattern)
+        }
+
+        return mapping_id_to_ip[mapping_name_to_id[self.interface]]
