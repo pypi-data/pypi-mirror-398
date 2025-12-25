b2938e0bbd108b2a83eaf1a0379f757cb4d9800f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 03:31:56 +0000

    Add package ujeebu-python/0.1.4b0/ujeebu_python-0.1.4b0-py2.py3-none-any.whl

---
+++ packages/ujeebu-python/ujeebu_python-0.1.4b0-py2.py3-none-any.whl/ujeebu_python/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for Ujeebu Python."""
+
+__author__ = """Yacine Alhyane"""
+__email__ = 'y.alhyane@gmail.com'
+__version__ = '0.1.4-beta'
+
+
+from .ujeebu_python import UjeebuClient
+
+__all__ = ['UjeebuClient']
