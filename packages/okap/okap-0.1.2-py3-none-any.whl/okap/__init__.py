524c8578327f8e143e71abb99b24d1f8c9efbe92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 05:06:18 +0000

    Add package okap/0.1.2/okap-0.1.2-py3-none-any.whl

---
+++ packages/okap/okap-0.1.2-py3-none-any.whl/okap/__init__.py
@@ -0,0 +1,18 @@
+"""OKAP - Open Key Access Protocol Python SDK."""
+
+from okap.client import OkapClient
+from okap.models import (
+    AccessRequest,
+    AccessResponse,
+    OkapToken,
+    Provider,
+)
+
+__version__ = "0.1.2"
+__all__ = [
+    "OkapClient",
+    "AccessRequest",
+    "AccessResponse",
+    "OkapToken",
+    "Provider",
+]
