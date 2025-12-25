2acbf682f7229ce90759249fe58d2332e3af72ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 08:04:56 +0000

    Add package python-sn2/0.3.0/python_sn2-0.3.0.tar.gz

---
+++ packages/python-sn2/python_sn2-0.3.0.tar.gz/python_sn2-0.3.0/sn2/constants.py
@@ -0,0 +1,8 @@
+"""Constants for SN2 device models."""
+
+from typing import Final
+
+SWITCH_MODELS: Final = ["WBR-01"]
+PLUG_MODELS: Final = ["WPR-01", "WPO-01"]
+LIGHT_MODELS: Final = ["WBD-01", "WPD-01"]
+IN_WALL_MODELS: Final = ["WBR-01", "WBD-01"]
