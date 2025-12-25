9559a8a1f94b64358a536e6e56dca14596d10483
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 08:04:33 +0000

    Add package amazon-braket-sdk/1.107.0/amazon_braket_sdk-1.107.0.tar.gz

---
+++ packages/amazon-braket-sdk/amazon_braket_sdk-1.107.0.tar.gz/amazon_braket_sdk-1.107.0/src/braket/program_sets/circuit_binding.py
@@ -0,0 +1,203 @@
+# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+from __future__ import annotations
+
+import warnings
+from collections.abc import Mapping, Sequence
+
+from braket.ir.openqasm import Program
+
+from braket.circuits import Circuit, Gate, Observable
+from braket.circuits.observable import euler_angle_parameter_names
+from braket.circuits.observables import Sum
+from braket.circuits.serialization import IRType
+from braket.program_sets.parameter_sets import ParameterSets, ParameterSetsLike
+from braket.pulse import PulseSequence
+from braket.registers import QubitSet
+
+
+class CircuitBinding:
+    def __init__(
+        self,
+        circuit: Circuit,
+        input_sets: ParameterSetsLike | None = None,
+        observables: Sequence[Observable] | Sum | None = None,
+    ):
+        """
+        A single parametrized circuit and multiple parameter sets and observables.
+
+        In other words, running a circuit binding means running the circuit with each set of input
+        parameters specified. Furthermore, observables are encoded as input parameters by way of
+        Euler angle representation.
+
+        If both input parameters and observables are provided, then each combination is executed,
+        resulting in a total number of runs equal to the product of the two. For example, if there
+        are 3 input sets and 4 parameters (or a Hamiltonian with 4 terms), the circuit will be run
+        12 times.
+
+        Note: Circuits cannot have result types attached.
+
+        Args:
+            circuit (Circuit): The parametrized circuit
+            input_sets (ParameterSetsLike | None): The inputs to the circuit, if specified.
+            observables (Sequence[Observable] | Sum | None): The observables or Hamiltonian
+                to measure, if specified.
+
+        Examples:
+            >>> circuit = Circuit().rx(0, FreeParameter("theta")).cnot(0, 1)
+            >>> observable = 2.1 * X(0) @ Z(1) - 4.5 * Z(0) @ Y(1)  # Sum Hamiltonian
+            >>> # observable = [X(0) @ Z(1), Z(0) @ Y(1)]  # Or a list of single-term observables
+            >>> binding = CircuitBinding(circuit, {"theta": [1.23, 1.73, 0.73]}, observable)
+        """
+        if not input_sets and not observables:
+            raise ValueError("At least one of input_sets and observables must be specified")
+        if observables:
+            terms = observables.summands if isinstance(observables, Sum) else observables
+            if any(isinstance(obs, Sum) for obs in terms):
+                raise TypeError("Cannot have Sum Hamiltonian in list of observables")
+        if circuit.result_types:
+            raise ValueError("Circuit cannot have result types")
+        self._circuit = circuit
+        self._input_sets = ParameterSets(input_sets) if input_sets else None
+        self._observables = observables or None
+
+    @property
+    def circuit(self) -> Circuit:
+        """
+        Circuit: The parametrized circuit
+        """
+        return self._circuit
+
+    @property
+    def input_sets(self) -> ParameterSets | None:
+        """
+        ParameterSets | None: The inputs to the circuit, if specified.
+        """
+        return self._input_sets
+
+    @property
+    def observables(self) -> Sequence[Observable] | Sum | None:
+        """
+        Sequence[Observable] | Sum | None: The observables or qubit Hamiltonian to measure,
+        if specified.
+        """
+        return self._observables
+
+    def to_ir(
+        self,
+        *,
+        gate_definitions: Mapping[tuple[Gate, QubitSet], PulseSequence] | None = None,
+    ) -> Program:
+        """Serializes the circuit binding into a form that can run on a Braket device.
+
+        Observables are treated as input parameters via conversion to Euler angles.
+
+        Args:
+            gate_definitions (dict[tuple[Gate, QubitSet], PulseSequence] | None): The
+                calibration data for the device. default: None.
+
+        Returns:
+            Program: An OpenQASM program containing the serialized circuit and input parameters.
+        """
+        if observables := self._observables:
+            terms = observables.summands if isinstance(observables, Sum) else observables
+            euler_angles = {}
+            for target in {target for obs in terms for target in obs.targets}:
+                for param in euler_angle_parameter_names(target):
+                    euler_angles[param] = [obs.euler_angles.get(param, 0) for obs in terms]
+            return Program(
+                source=self._circuit.with_euler_angles(observables)
+                .to_ir(IRType.OPENQASM, gate_definitions=gate_definitions)
+                .source,
+                inputs=(
+                    self._input_sets * euler_angles
+                    if self._input_sets
+                    else ParameterSets(euler_angles)
+                ).as_dict(),
+            )
+        return Program(
+            source=self._circuit.to_ir(IRType.OPENQASM, gate_definitions=gate_definitions).source,
+            inputs=self._input_sets.as_dict() if self._input_sets else None,
+        )
+
+    def bind_observables_to_inputs(
+        self,
+        inplace: bool = True,
+        add_measure: bool = True,
+    ) -> CircuitBinding:
+        """
+        Bind observables to input sets of parameters.
+
+        Translates observables in a CircuitBinding to local measurement bases via a parameterized
+        quantum circuit. The resulting composite input_set will be indexed to the same index in the
+        CompositeEntry. entry.expectation will no longer work, and data will have to be processed
+        as from a distribution. If using a Sum, information on the coefficients will be lost, as
+        well as CompositeEntry.expectation.
+
+        Kwargs:
+            inplace (bool): whether or not to return a new circuit binding or use the same one
+            add_measure (bool): whether or not to apply Measure instructions to the circuit
+
+        Returns:
+            CircuitBinding: A new circuit binding with the observables bound.
+        """
+        measure = Circuit()
+        parameters = self._input_sets.as_dict() if self._input_sets else None
+        if observables := self._observables:
+            if isinstance(observables, Sum):
+                observables = observables.summands
+                warnings.warn(
+                    "Binding a Sum discards information on observable weights; please "
+                    "distribute your observable in advance using observable.summands.",
+                    stacklevel=2,
+                )
+            euler_angles = {}
+            for target in {target for obs in observables for target in obs.targets}:
+                for param in euler_angle_parameter_names(target):
+                    euler_angles[param] = [obs.euler_angles.get(param, 0) for obs in observables]
+                if add_measure:
+                    measure.measure(target)
+            measure = Circuit().with_euler_angles(observables) + measure
+            parameters = self._input_sets * euler_angles if parameters else euler_angles
+        if inplace:
+            self._circuit.add_circuit(measure)
+            self._observables = None
+            self._input_sets = parameters
+            return self
+        return CircuitBinding(self._circuit + measure, input_sets=parameters)
+
+    def __len__(self):
+        input_sets = self._input_sets
+        observables = self._observables
+        if input_sets and observables:
+            return len(input_sets) * len(observables)
+        if input_sets:
+            return len(input_sets)
+        return len(observables)
+
+    def __eq__(self, other: CircuitBinding):
+        if not isinstance(other, CircuitBinding):
+            return False
+        return (
+            self._circuit == other._circuit
+            and self._input_sets == other._input_sets
+            and self._observables == other._observables
+        )
+
+    def __repr__(self):
+        return (
+            f"CircuitBinding(circuit={self._circuit!r}, "
+            f"input_sets={self._input_sets}, "
+            f"observables={self._observables})"
+        )
---
+++ packages/amazon-braket-sdk/amazon_braket_sdk-1.107.0.tar.gz/amazon_braket_sdk-1.107.0/test/unit_tests/braket/program_sets/test_circuit_binding.py
@@ -0,0 +1,102 @@
+# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+import pytest
+
+from braket.circuits import Circuit
+from braket.circuits.observables import X, Y, Z
+from braket.program_sets import CircuitBinding
+
+
+def test_equality(circuit_rx_parametrized):
+    input_sets = {"theta": [1.23, 3.21]}
+    observable = X(0) @ Z(1) + 3 * Y(0)
+    cb = CircuitBinding(circuit_rx_parametrized, input_sets, observable)
+    assert cb == CircuitBinding(
+        circuit_rx_parametrized, [{"theta": 1.23}, {"theta": 3.21}], observable
+    )
+    assert cb != CircuitBinding(circuit_rx_parametrized, observables=observable)
+    assert cb != CircuitBinding(circuit_rx_parametrized, input_sets)
+    assert cb != circuit_rx_parametrized
+
+
+def test_input_sets_observables_missing():
+    with pytest.raises(ValueError):
+        CircuitBinding(Circuit().h(0))
+
+
+def test_result_type(circuit_rx_parametrized):
+    with pytest.raises(ValueError):
+        CircuitBinding(
+            Circuit(circuit_rx_parametrized).expectation(X(0)),
+            input_sets={"theta": [1.23, 3.21]},
+        )
+
+
+def test_sum_in_observable_list():
+    with pytest.raises(TypeError):
+        CircuitBinding(Circuit().h(0), observables=[X(0) + Y(0)])
+
+
+def test_binding_to_input(circuit_rx_parametrized):
+    input_sets = {"theta": [1.35, 1.58]}
+    observable = [X(0) @ Z(1), Y(0), Z(0)]
+    cb1 = CircuitBinding(circuit_rx_parametrized, input_sets, observable)
+
+    cb2 = cb1.bind_observables_to_inputs(inplace=False)
+    assert cb1 != cb2
+    assert cb1.to_ir() == cb2.to_ir()
+
+    cb1.bind_observables_to_inputs(inplace=True)
+    assert cb1 == cb2
+
+
+def test_binding_to_input_no_inputs(circuit_rx_parametrized):
+    observable = [X(0) @ Z(1), Y(0), Z(0)]
+    cb1 = CircuitBinding(circuit_rx_parametrized, observables=observable)
+
+    cb2 = cb1.bind_observables_to_inputs(inplace=False)
+    assert cb1 != cb2
+    assert cb1.to_ir() == cb2.to_ir()
+
+    cb1.bind_observables_to_inputs(inplace=True)
+    assert cb1 == cb2
+
+
+def test_bind_sum_warning(circuit_rx_parametrized):
+    observable = 0.5 * X(0) @ Z(1) + 2 * Y(0)
+    cb1 = CircuitBinding(circuit_rx_parametrized, observables=observable)
+    with pytest.warns(UserWarning):
+        cb1.bind_observables_to_inputs()
+
+
+def test_no_observables_in_binding(circuit_rx_parametrized):
+    input_sets = {"theta": [1.35, 1.58]}
+    cb1 = CircuitBinding(circuit_rx_parametrized, input_sets=input_sets)
+    cb2 = cb1.bind_observables_to_inputs(inplace=False)
+    assert cb1 == cb2
+
+
+def test_binding_without_measure(circuit_rx_parametrized):
+    input_sets = {"theta": [1.35, 1.58]}
+    cb1 = CircuitBinding(
+        circuit_rx_parametrized,
+        input_sets=input_sets,
+        observables=0.5 * X(0) @ Z(1) + 2 * Y(0),
+    )
+    cb2 = cb1.bind_observables_to_inputs(inplace=False, add_measure=False)
+    cb3 = cb1.bind_observables_to_inputs(inplace=False, add_measure=True)
+    assert cb2.circuit != cb3.circuit
+    circ = cb2.circuit
+    circ.measure(range(2))
+    assert circ == cb3.circuit
