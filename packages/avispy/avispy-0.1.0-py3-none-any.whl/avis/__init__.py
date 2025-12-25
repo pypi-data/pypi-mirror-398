519db897de11b47d167f4aec7573b22ba31d49d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 08:04:37 +0000

    Add package avispy/0.1.0/avispy-0.1.0-py3-none-any.whl

---
+++ packages/avispy/avispy-0.1.0-py3-none-any.whl/avis/__init__.py
@@ -0,0 +1,8 @@
+"""
+avis - A Python package for psychology statistics and psychometrics
+"""
+
+from avis.utils import xls, fix_parens, make_unique
+
+__version__ = "0.1.0"
+__all__ = ["xls", "fix_parens", "make_unique"]
