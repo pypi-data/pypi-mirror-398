fe224fa79640c0345ed4ed4ff0965892fc9840dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 13:15:15 +0000

    Add package flake8-tensors/1.3.1/flake8_tensors-1.3.1-py3-none-any.whl

---
+++ packages/flake8-tensors/flake8_tensors-1.3.1-py3-none-any.whl/flake8_tensors/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from importlib.metadata import version
+
+__version__ = version("flake8_tensors")
+__title__ = "flake8_tensors"
+__summary__ = "flake8_tensors - flake8 plugin for deep learning codes"
+__license__ = "MIT"
+__author__ = "David Völgyes"
+__email__ = "david.volgyes@ieee.org"
+__description__ = """
+This program is a flake8 plugin, and recommends some tricks
+and best practices for machine learning codes.
+"""
+
+from .plugin import Flake8TensorsPlugin
+
+__all__ = ["Flake8TensorsPlugin"]
