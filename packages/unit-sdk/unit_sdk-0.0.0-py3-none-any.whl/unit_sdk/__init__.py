c2aae8f4c3d73d3b9936ee37a293ab40e8d9a724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 08:04:24 +0000

    Add package unit-sdk/0.0.0/unit_sdk-0.0.0-py3-none-any.whl

---
+++ packages/unit-sdk/unit_sdk-0.0.0-py3-none-any.whl/unit_sdk/__init__.py
@@ -0,0 +1,12 @@
+# Copyright 2025 PageKey Solutions, LLC
+
+from ._version import version
+from .client import UnitClient
+
+__version__ = version
+
+
+__all__ = [
+    "__version__",
+    "UnitClient",
+]
