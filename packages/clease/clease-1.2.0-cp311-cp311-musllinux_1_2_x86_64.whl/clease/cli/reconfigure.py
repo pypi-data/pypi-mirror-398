fd9ab8dd750627b1b6185317083e85539a5cbe68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-25 06:05:10 +0000

    Add package clease/1.2.0/clease-1.2.0-cp311-cp311-musllinux_1_2_x86_64.whl

---
+++ packages/clease/clease-1.2.0-cp311-cp311-musllinux_1_2_x86_64.whl/clease/cli/reconfigure.py
@@ -0,0 +1,22 @@
+import click
+
+from clease import tools
+from clease.settings import settings_from_json
+
+from . import main
+
+
+@main.clease_cli.command()
+@click.argument(
+    "filename",
+    type=click.Path(exists=True),
+)
+def reconfigure(filename):
+    """
+    Reconfigure a settings file saved as a JSON file.
+    Will reconfigure the database that the settings points to.
+
+    FILENAME is the name of the settings JSON file
+    """
+    settings = settings_from_json(filename)
+    tools.reconfigure(settings, verbose=True)
