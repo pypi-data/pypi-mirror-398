f7cb57db00cdf28d09f550ebdf77e6acea7be3ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-28 16:05:47 +0000

    Add package simplepy/0.1.7/simplepy-0.1.7.tar.gz

---
+++ packages/simplepy/simplepy-0.1.7.tar.gz/simplepy-0.1.7/README.md
@@ -0,0 +1,2 @@
+# simplepy-module
+It's a python module repository which makes your python journey faster and simpler.
---
+++ packages/simplepy/simplepy-0.1.7.tar.gz/simplepy-0.1.7/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="simplepy",               # MUST match PyPI name
+    version="0.1.7",               # CHANGE every release
+    packages=find_packages(),
+    description="Simple Python utilities",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/GeniusVoid/simplepy",
+    author="Vibhor Kumar",
+    python_requires=">=3.7",
+)
