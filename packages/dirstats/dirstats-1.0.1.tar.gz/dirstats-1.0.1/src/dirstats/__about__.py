9e0eb76c0d7ee4e0216c5c3612881790441cdb39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-28 19:07:27 +0000

    Add package dirstats/1.0.1/dirstats-1.0.1.tar.gz

---
+++ packages/dirstats/dirstats-1.0.1.tar.gz/dirstats-1.0.1/src/dirstats/__about__.py
@@ -0,0 +1,27 @@
+__all__ = ["__title__", "__version__", "__summary__", "__description__", "__url__",
+         "__docs_url__", "__issues_url__", "__status__", "__keywords__",
+           "__author__", "__maintainer__", "__license__", "__copyright__"]
+
+
+__title__ = "dirstats"
+__version__ = "1.0.0"
+__summary__ = "Stats of a base archive directory."
+__description__ = ("""Analyze a base directory and calculate the total
+                    number of files, subfolders, and overall directory
+                    size, with optional filtering by one or more file
+                    extensions.""")
+__url__ = "https://github.com/Sharrrkkk/DirStats"
+__docs_url__ = "https://github.com/Sharrrkkk/DirStats#README.md"
+__issues_url__ = "https://github.com/Sharrrkkk/DirStats/issues"
+__status__ = "Production/Stable"
+__keywords__ = ["cli",
+    "filesystem",
+    "directory",
+    "file-analysis",
+    "automation",
+    "python-cli",
+    "pathlib"]
+__author__ = "Sharrrkkk"
+__maintainer__ = "Sharrrkkk"
+__license__ = "MIT"
+__copyright__ = "2025 Sharrrkkk"
