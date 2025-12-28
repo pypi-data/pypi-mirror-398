5ea02a6d9ad5c4809aeb7d06c9ddc4312b837ada
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-28 19:06:57 +0000

    Add package ai-creature-lang/5.3.0/ai_creature_lang-5.3.0-py3-none-any.whl

---
+++ packages/ai-creature-lang/ai_creature_lang-5.3.0-py3-none-any.whl/ai_creature_lang/pyarmor_runtime_000000/__init__.py
@@ -0,0 +1,3 @@
+# Pyarmor 9.1.7 (trial), 000000, 2025-12-26T10:20:33.690726
+from sys import version_info as py_version
+__pyarmor__ = __import__("py%d%d.pyarmor_runtime" % py_version[:2], globals(), locals(), ["__pyarmor__"], 1).__pyarmor__
