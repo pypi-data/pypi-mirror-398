80cf8c47a16b1765f7da64ad00c131e8e0d302c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-28 19:06:04 +0000

    Add package optimum-rbln/0.9.4/optimum_rbln-0.9.4-py3-none-any.whl

---
+++ packages/optimum-rbln/optimum_rbln-0.9.4-py3-none-any.whl/optimum/rbln/transformers/models/gpt2/gpt2_architecture.py
@@ -0,0 +1,93 @@
+# Copyright 2025 Rebellions Inc. All rights reserved.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import math
+from typing import TYPE_CHECKING, Tuple, Union
+
+import torch
+import torch.nn as nn
+
+from ..decoderonly.decoderonly_architecture import (
+    DecoderOnlyAttention,
+    DecoderOnlyLayer,
+    DecoderOnlyModel,
+    DecoderOnlyWrapper,
+)
+
+
+if TYPE_CHECKING:
+    from transformers import GPT2LMHeadModel, GPT2Model
+
+
+class GPT2Wrapper(DecoderOnlyWrapper):
+    def get_rbln_attn_class(self):
+        return GPT2Attention
+
+    def get_rbln_layer_class(self):
+        return GPT2Layer
+
+    def get_rbln_model_class(self):
+        return GPT2Model
+
+    def get_attn_layer(self, layer: nn.Module):
+        return layer.attn
+
+    def get_model_layer(self, model: Union["GPT2LMHeadModel", "GPT2Model"]):
+        return model.transformer if self.is_causal_lm else model
+
+    def get_decoder_layers(self, model: Union["GPT2LMHeadModel", "GPT2Model"]):
+        return model.transformer.h if self.is_causal_lm else model.h
+
+
+class GPT2Model(DecoderOnlyModel):
+    def get_last_layernorm(self) -> nn.LayerNorm:
+        return self._original_mod.ln_f
+
+    def get_embedding(self) -> nn.Embedding:
+        return self._original_mod.wte
+
+    def get_pos_embedding(self) -> nn.Embedding:
+        return self._original_mod.wpe
+
+
+class GPT2Layer(DecoderOnlyLayer):
+    def get_pre_attention_layernorm(self) -> nn.LayerNorm:
+        return self._original_mod.ln_1
+
+    def get_post_attention_layernorm(self) -> nn.LayerNorm:
+        return self._original_mod.ln_2
+
+
+class GPT2Attention(DecoderOnlyAttention):
+    def __post_init__(self):
+        self.c_attn = self._original_mod.c_attn
+        self.o_proj = self._original_mod.c_proj
+        self.split_size = self._original_mod.split_size
+
+    def projection(self, hidden_states, lora_int_id) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        if lora_int_id is not None:
+            raise NotImplementedError("LoRA is not supported for GPT2Attention")
+
+        query_states, key_states, value_states = self.c_attn(hidden_states).split(self.split_size, dim=2)
+        return query_states, key_states, value_states
+
+    def get_attn_scale(self):
+        scale = 1.0
+        if self._original_mod.scale_attn_weights:
+            scale /= math.sqrt(self.head_dim)
+
+        if self._original_mod.scale_attn_by_inverse_layer_idx:
+            scale /= 1 + self.layer_idx
+
+        return scale
