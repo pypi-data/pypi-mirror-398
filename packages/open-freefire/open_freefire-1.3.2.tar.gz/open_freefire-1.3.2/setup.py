9a874b6b9af9ffccdfc9ece97f9d1424044e74ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-28 18:06:49 +0000

    Add package open-freefire/1.3.2/open_freefire-1.3.2.tar.gz

---
+++ packages/open-freefire/open_freefire-1.3.2.tar.gz/open_freefire-1.3.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="open-freefire",
+    version="1.3.2",
+    packages=find_packages(),
+    entry_points = {
+        "console_scripts": ["show_info = OpenFF.cli:main","is_ban = OpenFF.cli2:main", "open-freefire = OpenFF.cli3:main"]
+    },
+    install_requires=[
+        "requests",
+        "argparse",
+        "colorama",
+        "datetime",
+        "lolpython",
+        "pyfiglet"
+    ],
+    author="Abdul Moeez",
+    description="OpenFF: Fetch FF account info and ban status",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://pypi.org/project/open-freefire/",
+    python_requires=">=3.7",
+)
