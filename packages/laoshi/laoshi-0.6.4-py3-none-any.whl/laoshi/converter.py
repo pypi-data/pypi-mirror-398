374fcbc1711718c77edde3379ea8e906b50b054e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-28 17:06:03 +0000

    Add package laoshi/0.6.4/laoshi-0.6.4-py3-none-any.whl

---
+++ packages/laoshi/laoshi-0.6.4-py3-none-any.whl/laoshi/converter.py
@@ -0,0 +1,33 @@
+"""Module which provides the Converter class to convert characters"""
+
+from functools import reduce
+from StarCC import PresetConversion
+from pypinyin import pinyin
+
+
+class Converter:
+    """Class for pinyin, traditional and simplified
+    chinese character conversions"""
+
+    @staticmethod
+    def to_traditional(hanzi: str) -> str:
+        """Transform simplified chinese characters to traditional"""
+        return Converter.convert(src="cn", dst="tw", hanzi=hanzi)
+
+    @staticmethod
+    def to_simplified(hanzi: str) -> str:
+        """Transform traditional chinese characters to simplified"""
+        return Converter.convert(src="tw", dst="cn", hanzi=hanzi)
+
+    @staticmethod
+    def to_pinyin(hanzi: str) -> str:
+        """Transform Chinese characters to pinyin.
+        We just take the first option from pypinyin."""
+        flat_pinyin = [elem for py_sl in pinyin(hanzi) for elem in py_sl]
+        return reduce(lambda a, b: a + b, flat_pinyin)
+
+    @staticmethod
+    def convert(src: str, dst: str, hanzi: str) -> str:
+        """Generic convertion method"""
+        convert = PresetConversion(src=src, dst=dst, with_phrase=True)
+        return convert(hanzi)
---
+++ packages/laoshi/laoshi-0.6.4-py3-none-any.whl/laoshi/translator.py
@@ -0,0 +1,20 @@
+"""Module for the Translator classes"""
+
+from googletrans import Translator as GTranslator
+import asyncio
+
+
+class Translator:  # pylint: disable=too-few-public-methods
+    """Translator class"""
+
+    def __init__(self):
+        """Initiliazer for Translator"""
+        self.translator = GTranslator()
+
+    async def translate_async(self, text: str, dest: str = "en") -> str:
+        """Translate a text into english (by default)"""
+        result = await self.translator.translate(text, dest=dest)
+        return result.text
+
+    def translate(self, text: str, dest: str = "en") -> str:
+        return asyncio.run(self.translate_async(text, dest))
