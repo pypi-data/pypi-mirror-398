f0c0f491bff4bf8a12ff60a1bb962d7b28eb26ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-28 17:05:59 +0000

    Add package rulelift/1.1.6/rulelift-1.1.6.tar.gz

---
+++ packages/rulelift/rulelift-1.1.6.tar.gz/rulelift-1.1.6/rulelift/__init__.py
@@ -0,0 +1,53 @@
+from .utils import load_example_data, preprocess_data
+from .analysis import (
+    VariableAnalyzer,
+    analyze_rules,
+    analyze_rule_correlation,
+    get_user_rule_matrix,
+    calculate_strategy_gain
+)
+from .mining import (
+    SingleFeatureRuleMiner,
+    MultiFeatureRuleMiner,
+    DecisionTreeRuleExtractor
+)
+from .visualization import (
+    RuleVisualizer,
+    plot_rule_comparison,
+    plot_rule_distribution,
+    plot_lift_precision_scatter,
+    plot_decision_tree,
+    plot_rule_network,
+    plot_heatmap,
+    generate_rule_report
+)
+
+__version__ = '1.1.5'
+__all__ = [
+    # Utils
+    'load_example_data',
+    'preprocess_data',
+    
+    # Analysis
+    'VariableAnalyzer',
+    'analyze_rules',
+    'analyze_rule_correlation',
+    'get_user_rule_matrix',
+    'calculate_strategy_gain',
+    
+    # Mining
+    'SingleFeatureRuleMiner',
+    'MultiFeatureRuleMiner',
+    'DecisionTreeRuleExtractor',
+    
+    # Visualization
+    'RuleVisualizer',
+    'plot_rule_comparison',
+    'plot_rule_distribution',
+    'plot_lift_precision_scatter',
+    'plot_decision_tree',
+    'plot_rule_network',
+    'plot_heatmap',
+    'generate_rule_report'
+]
+
