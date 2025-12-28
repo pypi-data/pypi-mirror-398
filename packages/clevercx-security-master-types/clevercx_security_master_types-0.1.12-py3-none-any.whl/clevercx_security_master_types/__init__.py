9d08ba27499bfcabf1a16914435d14e74bb926a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-28 15:06:05 +0000

    Add package clevercx-security-master-types/0.1.12/clevercx_security_master_types-0.1.12-py3-none-any.whl

---
+++ packages/clevercx-security-master-types/clevercx_security_master_types-0.1.12-py3-none-any.whl/clevercx_security_master_types/__init__.py
@@ -0,0 +1,12 @@
+"""
+clevercx-security-master-types
+
+Python type definitions for the CleverCX Security Master database.
+Generated from PostgreSQL database via sqlacodegen.
+
+DO NOT EDIT MANUALLY - This file is auto-generated.
+"""
+
+__version__ = "0.1.12"
+
+from .models import *
