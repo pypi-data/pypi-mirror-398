de3872a8630f04df48dab8e5501ac1b18d89992f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-28 17:06:00 +0000

    Add package kolzchut-ragbot/1.7.13/kolzchut_ragbot-1.7.13.tar.gz

---
+++ packages/kolzchut-ragbot/kolzchut_ragbot-1.7.13.tar.gz/kolzchut_ragbot-1.7.13/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+# to deploy the package to PyPI, run the following command:
+# pip install setuptools wheel twine
+# python setup.py sdist bdist_wheel
+# twine upload dist/*
+
+setup(
+    name='kolzchut-ragbot',
+    version='1.7.13',
+    author='Shmuel Robinov',
+    author_email='shmuel_robinov@webiks.com',
+    description='A search engine using machine learning models and Elasticsearch for advanced document retrieval.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/shmuelrob/rag-bot',
+    packages=find_packages(),
+    install_requires=[
+        'elasticsearch==8.17.1',
+        'sentence-transformers==3.4.1',
+        'torch==2.6.0',
+        'transformers==4.48.3'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+)
