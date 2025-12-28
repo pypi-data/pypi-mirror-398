f034bf8e559fd1a6d6bce3775f1c876da3e451f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-28 15:06:06 +0000

    Add package errorwatch/0.1.0/errorwatch-0.1.0.tar.gz

---
+++ packages/errorwatch/errorwatch-0.1.0.tar.gz/errorwatch-0.1.0/README.md
@@ -0,0 +1,13 @@
+# ErrorWatch
+
+Intercept Python exceptions and send them to a server.
+
+## Installation
+
+```bash
+pip install errorwatch
+```
+
+## License
+
+MIT
---
+++ packages/errorwatch/errorwatch-0.1.0.tar.gz/errorwatch-0.1.0/src/errorwatch/__init__.py
@@ -0,0 +1,8 @@
+"""ErrorWatch - Intercept Python exceptions and send them to a server."""
+
+__version__ = "0.1.0"
+
+
+def hello() -> str:
+    """Return a greeting."""
+    return "Hello from ErrorWatch!"
