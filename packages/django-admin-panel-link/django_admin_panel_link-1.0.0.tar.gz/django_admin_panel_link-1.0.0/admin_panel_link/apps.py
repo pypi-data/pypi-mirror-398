eb2e02bca17468fa116bc79e47c82b542f35cec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-28 14:05:45 +0000

    Add package django-admin-panel-link/1.0.0/django_admin_panel_link-1.0.0.tar.gz

---
+++ packages/django-admin-panel-link/django_admin_panel_link-1.0.0.tar.gz/django_admin_panel_link-1.0.0/admin_panel_link/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AdminLinkConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'admin_panel_link'
---
+++ packages/django-admin-panel-link/django_admin_panel_link-1.0.0.tar.gz/django_admin_panel_link-1.0.0/setup.py
@@ -0,0 +1,36 @@
+from pip_setuptools import setup, find_packages, clean
+
+clean()
+setup(
+    name='django-admin-panel-link',
+    version='1.0.0',
+    author='Маг Ильяс DOMA (MagIlyasDOMA)',
+    author_email='magilyas.doma.09@list.ru',
+    url='https://github.com/MagIlyasDOMA/django-admin-link',
+    packages=find_packages(exclude=['app_test']),
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Natural Language :: Russian',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Internet :: WWW/HTTP :: Site Management',
+        'Framework :: Django',
+        'Framework :: Django :: 5.2',
+        'Framework :: Django :: 6.0',
+    ]
+)
