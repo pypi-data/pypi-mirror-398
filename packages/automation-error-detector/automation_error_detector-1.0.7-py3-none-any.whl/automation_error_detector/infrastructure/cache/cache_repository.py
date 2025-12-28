9c3c6010daf0b0292d7e5322fa831ccbdc0b9a42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-28 16:05:15 +0000

    Add package automation-error-detector/1.0.7/automation_error_detector-1.0.7-py3-none-any.whl

---
+++ packages/automation-error-detector/automation_error_detector-1.0.7-py3-none-any.whl/automation_error_detector/infrastructure/cache/cache_repository.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+
+
+class CacheRepository(ABC):
+
+    @abstractmethod
+    def get(self, signature: str):
+        pass
+
+    @abstractmethod
+    def save(self, signature: str, data: dict):
+        pass
