984ff2fd08e3be973e010704fa0004964c7669a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-28 14:06:11 +0000

    Add package tds2/0.2.2/tds2-0.2.2.tar.gz

---
+++ packages/tds2/tds2-0.2.2.tar.gz/tds2-0.2.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="tds2",
+    version="0.2.2",
+    author="ZatOFF",
+    author_email="zaton318@gmail.com",
+    description="lib for file management with telegram api",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ZatON318/TDS2",
+    packages=find_packages(),
+    install_requires=[
+        'telethon',
+        'tqdm',
+        'python-dotenv',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
