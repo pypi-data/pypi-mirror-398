14f02f7fa26250eb0ec7955cf1680c9fe8b81f9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-28 18:06:01 +0000

    Add package thread-notebook/0.1.27/thread_notebook-0.1.27-py3-none-any.whl

---
+++ packages/thread-notebook/thread_notebook-0.1.27-py3-none-any.whl/thread_notebook/__init__.py
@@ -0,0 +1,17 @@
+from .thread_notebook_app import ThreadNotebookApp
+
+
+def _jupyter_server_extension_paths():
+    return [{"module": "thread_notebook"}]
+
+
+def _jupyter_server_extension_points():
+    """
+    Returns a list of dictionaries with metadata describing
+    the extension.
+    """
+    return [{"module": "thread_notebook", "app": ThreadNotebookApp}]
+
+
+def launch_instance():
+    ThreadNotebookApp.launch_instance()
