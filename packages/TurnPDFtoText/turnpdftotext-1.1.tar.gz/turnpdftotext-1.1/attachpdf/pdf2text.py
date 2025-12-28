a80ca196b855f62c8814acf2eb764e7bc15522fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-28 14:05:32 +0000

    Add package TurnPDFtoText/1.1/turnpdftotext-1.1.tar.gz

---
+++ packages/TurnPDFtoText/turnpdftotext-1.1.tar.gz/turnpdftotext-1.1/attachpdf/pdf2text.py
@@ -0,0 +1,26 @@
+""" This module provides functions to convert a PDF to text. """
+
+
+class Converter:
+    """ This is the class for cnverting PDF to text file. """
+
+    def __init__(self, x):
+        """ A simple variable for inputing data. """
+        self.x = x
+
+    def convert(self, path):
+        """ 
+        Convert the given PDF to text.
+
+        Parameters:
+        path (str): path of the PDF
+
+        Returns: 
+        str: The content in the PDF file
+        """
+        print("pdf2text")
+
+    @classmethod
+    def margin(cls):
+        """ Return the value of given margin on terminal. """
+        return cls
