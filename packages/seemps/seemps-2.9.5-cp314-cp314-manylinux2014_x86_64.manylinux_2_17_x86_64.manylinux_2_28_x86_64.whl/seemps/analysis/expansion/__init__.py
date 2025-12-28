afb6450624fb7497af30796744943148f1a0812a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-28 18:05:59 +0000

    Add package seemps/2.9.5/seemps-2.9.5-cp314-cp314-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/seemps/seemps-2.9.5-cp314-cp314-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl/seemps/analysis/expansion/__init__.py
@@ -0,0 +1,10 @@
+from .expansion import PowerExpansion, PolynomialExpansion
+from .chebyshev import ChebyshevExpansion
+from .legendre import LegendreExpansion
+
+__all__ = [
+    "PolynomialExpansion",
+    "PowerExpansion",
+    "ChebyshevExpansion",
+    "LegendreExpansion",
+]
