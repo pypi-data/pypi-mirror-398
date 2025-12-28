e9e72ace2ecb3627ec812f32ed77b3b2e0a9cdb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-28 19:07:27 +0000

    Add package libellula/1.2.0/libellula-1.2.0.tar.gz

---
+++ packages/libellula/libellula-1.2.0.tar.gz/libellula-1.2.0/libellula/__init__.py
@@ -0,0 +1,31 @@
+from .libellula import (
+    argmax,
+    argmin,
+    group_by,
+    flatmap,
+    flatten,
+    get_only,
+    get_any,
+    compose,
+    batch,
+    compact,
+    typecheck,
+    transpose,
+)
+
+__all__ = [
+    "argmax",
+    "argmin",
+    "group_by",
+    "flatmap",
+    "flatten",
+    "get_only",
+    "get_any",
+    "compose",
+    "batch",
+    "compact",
+    "typecheck",
+    "transpose",
+]
+
+del libellula
