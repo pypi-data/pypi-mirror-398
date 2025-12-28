b338724993a6fcfda243b996d5e58b5fbbde3e65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-28 17:05:31 +0000

    Add package fabricatio-typst/0.1.23/fabricatio_typst-0.1.23-cp314-cp314-macosx_11_0_arm64.whl

---
+++ packages/fabricatio-typst/fabricatio_typst-0.1.23-cp314-cp314-macosx_11_0_arm64.whl/fabricatio_typst/__init__.py
@@ -0,0 +1,4 @@
+"""This package provides the core functionality for the fabricatio-typst module.
+
+It includes capabilities for processing and generating typst documents, integrating with other components of the fabricatio system.
+"""
