1303f08eafc8ff2e5039bdb49019242a8cbb4648
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-28 15:06:06 +0000

    Add package pytest-mqtt/0.6.2.dev0/pytest_mqtt-0.6.2.dev0-py3-none-any.whl

---
+++ packages/pytest-mqtt/pytest_mqtt-0.6.2.dev0-py3-none-any.whl/pytest_mqtt/model.py
@@ -0,0 +1,21 @@
+import dataclasses
+import typing as t
+
+
+@dataclasses.dataclass
+class MqttMessage:
+    """
+    Container for `capmqtt`'s `message` response items.
+    """
+
+    topic: str
+    payload: t.Union[str, bytes]
+    userdata: t.Optional[t.Union[t.Dict, None]]
+
+
+@dataclasses.dataclass
+class MqttSettings:
+    host: str
+    port: int
+    username: str
+    password: str
