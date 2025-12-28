d3b7443cf648f55c43174ef5cda5e17fdb7e2819
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-28 16:05:00 +0000

    Add package luckytools/0.8/luckytools-0.8.tar.gz

---
+++ packages/luckytools/luckytools-0.8.tar.gz/luckytools-0.8/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='luckytools',
+      version='0.8',
+      description='Print utility',
+      packages=['luckytools'],
+      author_email='sayginaego.say@gmail.com',
+      zip_safe=False)
