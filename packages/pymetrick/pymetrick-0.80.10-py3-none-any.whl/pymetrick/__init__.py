38a828bcafecd5eb94eb254bca5605c524bd25db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-28 14:05:41 +0000

    Add package pymetrick/0.80.10/pymetrick-0.80.10-py3-none-any.whl

---
+++ packages/pymetrick/pymetrick-0.80.10-py3-none-any.whl/pymetrick/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ['bucket', 'common', 'device', 'esc_pos_costants', 'exchequer', 'fonts', 'fpdf', 'helpers', 'image', 'ipaddress', 'issue', 'mail', 'php', 'routing', 'rst', 'session', 'sqldb', 'template', 'ttfonts', 'version']
+
+
