5676f6ae49ee83303f8eada654a52518f4ce1127
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-28 14:06:01 +0000

    Add package django-admin-panel-link/1.0.2/django_admin_panel_link-1.0.2.tar.gz

---
+++ packages/django-admin-panel-link/django_admin_panel_link-1.0.2.tar.gz/django_admin_panel_link-1.0.2/admin_panel_link/templatetags/admin_panel_link.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from django.template import Library
+from django.utils.translation import gettext
+from .. import ADMIN_LINK_ONLY_SUPERUSER, ADMIN_LINK_USE_STYLES
+
+register = Library()
+
+
+@register.inclusion_tag('admin_panel_link/includes/admin_panel_link.html', takes_context=True)
+def admin_panel_link(context):
+    user: User = context['user']
+    condition = user.is_authenticated and ((user.is_staff and not ADMIN_LINK_ONLY_SUPERUSER) or user.is_superuser)
+    return dict(user=user, condition=condition, use_styles=ADMIN_LINK_USE_STYLES, text=gettext("Admin panel"))
