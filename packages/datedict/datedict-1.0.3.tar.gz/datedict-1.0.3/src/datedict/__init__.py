6aa04b514f5eac58bff37c8ff22d88a1ba55f7ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-28 14:06:01 +0000

    Add package datedict/1.0.3/datedict-1.0.3.tar.gz

---
+++ packages/datedict/datedict-1.0.3.tar.gz/datedict-1.0.3/src/datedict/__init__.py
@@ -0,0 +1,6 @@
+from .DateDict import DateDict
+from .YearDict import YearDict
+from .common import ZERO, ONE, NAN, Decimable
+
+__all__ = ["DateDict", "YearDict", "ZERO", "ONE", "NAN", "Decimable"]
+__version__ = "1.0.3"
