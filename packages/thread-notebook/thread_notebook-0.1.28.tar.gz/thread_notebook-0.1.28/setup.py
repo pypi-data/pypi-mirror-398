27d24576a288965e2e4d281b32a42d39fa0c98bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-28 19:06:58 +0000

    Add package thread-notebook/0.1.28/thread_notebook-0.1.28.tar.gz

---
+++ packages/thread-notebook/thread_notebook-0.1.28.tar.gz/thread_notebook-0.1.28/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+# Determine the directory containing setup.py and get its parent directory
+this_directory = os.path.abspath(os.path.dirname(__file__))
+parent_directory = os.path.abspath(os.path.join(this_directory, os.pardir))
+
+# Read the content of README.md from the parent directory
+with open(os.path.join(parent_directory, "README.md"), "r") as fh:
+    long_description = fh.read()
+
+# Replace all mentions of 'public/' with 'thread-notebook/static/'
+long_description = long_description.replace(
+    'public/', 'thread_notebook/static/')
+
+setup(
+    name="thread-notebook",
+    version="0.1.28",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={"thread_notebook": ["static/**/*"]},
+    install_requires=[
+        "jupyter-server>=2.0",
+        "jupyter",
+    ],
+    entry_points={
+        "console_scripts": [
+            "thread-notebook = thread_notebook:launch_instance",
+            "thread = thread_notebook:launch_instance",
+        ],
+        "jupyter_server_extension_points": [
+            "thread-notebook = thread_notebook:ThreadNotebookApp",
+        ],
+    },
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
---
+++ packages/thread-notebook/thread_notebook-0.1.28.tar.gz/thread_notebook-0.1.28/thread_notebook/static/_next/static/chunks/74.d0181866c39f9037.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[74],{22868:function(){},14777:function(){},99830:function(){},70209:function(){}}]);
