bc1993e8f0e8ec7efab31a22da40dbcdb5d7ac36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-28 14:05:37 +0000

    Add package neuralk/0.0.12/neuralk-0.0.12-py3-none-any.whl

---
+++ packages/neuralk/neuralk-0.0.12-py3-none-any.whl/neuralk/__init__.py
@@ -0,0 +1,37 @@
+"""
+NeuralkAI SDK main module.
+
+This module provides the main interface for interacting with the Neuralk AI platform.
+It handles authentication and provides access to various services through specialized handlers.
+"""
+
+import logging
+from pathlib import Path
+
+from ._classifier import Classifier
+from ._on_premise_classifier import OnPremiseClassifier
+from .exceptions import NeuralkException
+from .model.analysis import Analysis
+from .model.dataset import Dataset
+from .model.organization import Organization
+from .model.project import Project
+from .model.project_file import ProjectFile
+from .neuralk import Neuralk
+from .utils.docs import add_submodules_to_docstring
+
+VERSION_PATH = Path(__file__).resolve().parent / "VERSION.txt"
+__version__ = VERSION_PATH.read_text(encoding="utf-8").strip()
+
+
+__all__ = [
+    "Neuralk",
+    "Analysis",
+    "Dataset",
+    "Organization",
+    "Project",
+    "ProjectFile",
+    "NeuralkException",
+    "Classifier",
+    "OnPremiseClassifier",
+    "logger",
+]
