00c1fc2369d716f0f217026df0737edc7c00c2d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-28 17:05:12 +0000

    Add package gptio/0.1.3/gptio-0.1.3-py3-none-any.whl

---
+++ packages/gptio/gptio-0.1.3-py3-none-any.whl/gptio.py
@@ -0,0 +1,72 @@
+import requests
+
+def text(api, model, prompt):
+    url = "https://api.openai.com/v1/chat/completions"
+
+    r = requests.post(
+        url,
+        headers={
+            "Authorization": f"Bearer {api}",
+            "Content-Type": "application/json"
+        },
+        json={
+            "model": model,
+            "messages": [{"role": "user", "content": prompt}]
+        },
+)
+    data = r.json()
+    print(data["choices"][0]["message"]["content"])
+
+def image(api, model, text, size):
+    url = "https://api.openai.com/v1/images/generations"
+    r = requests.post(
+        url,
+        headers={
+            "Authorization": f"Bearer {api}",
+            "Content-Type": "application/json"
+        },
+        json={
+            "model": model,
+            "prompt": text,
+            "n": 1,
+            "size": size
+        },
+)
+    print(r.json()["data"][0]["url"])
+
+def apicheck(api):
+    checkurl = "https://api.openai.com/v1/chat/completions"
+    check = requests.post(
+        checkurl,
+        headers={
+             "Authorization": f"Bearer {api}",
+             "Content-Type": "application/json"
+             },
+        json={
+            "model": "gpt-4o-mini",
+            "messages": [{"role": "user", "content": "test"}]
+        },
+)
+
+    if check.status_code == 200:
+        print("gptio | Success / Text Service")
+    else:
+        print("gptio | Unsuccess / Text Service")
+
+    imgcheck = requests.post(
+        "https://api.openai.com/v1/images/generations",
+        headers={
+            "Authorization": f"Bearer {api}",
+            "Content-Type": "application/json"
+        },
+        json={
+            "model": "gpt-image-1",
+            "prompt": "test",
+            "size": "1024x1024"
+        }
+    )
+
+    if imgcheck.status_code == 200:
+        print("gptio | Success / Image Service")
+    else:
+        print("gptio | Unsuccess / Image Service")
