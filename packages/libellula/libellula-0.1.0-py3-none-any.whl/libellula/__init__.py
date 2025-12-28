7da70181ba7d1f98685a4210fc5d741c722f51c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-28 19:07:24 +0000

    Add package libellula/0.1.0/libellula-0.1.0-py3-none-any.whl

---
+++ packages/libellula/libellula-0.1.0-py3-none-any.whl/libellula/__init__.py
@@ -0,0 +1,29 @@
+from .libellula import (
+    argmax,
+    argmin,
+    group_by,
+    flatmap,
+    flatten,
+    get_only,
+    get_any,
+    compose,
+    batch,
+    compact,
+    typecheck,
+)
+
+__all__ = [
+    "argmax",
+    "argmin",
+    "group_by",
+    "flatmap",
+    "flatten",
+    "get_only",
+    "get_any",
+    "compose",
+    "batch",
+    "compact",
+    "typecheck",
+]
+
+del libellula
