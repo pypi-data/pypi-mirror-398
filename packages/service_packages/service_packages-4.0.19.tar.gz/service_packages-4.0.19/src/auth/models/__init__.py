e0a2d1f0587928df36b5c576ef11cd03b21a4610
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-28 14:06:19 +0000

    Add package service_packages/4.0.19/service_packages-4.0.19.tar.gz

---
+++ packages/service_packages/service_packages-4.0.19.tar.gz/service_packages-4.0.19/src/auth/models/__init__.py
@@ -0,0 +1,15 @@
+from .auth_code_model import AuthCodeModel
+from .permission_model import PermissionModel
+from .role_model import RoleModel
+from .role_permission_model import RolePermissionModel
+from .user_model import UserModel
+from .user_role_model import UserRoleModel
+
+__all__ = [
+    "AuthCodeModel",
+    "RoleModel",
+    "PermissionModel",
+    "RolePermissionModel",
+    "UserModel",
+    "UserRoleModel",
+]
