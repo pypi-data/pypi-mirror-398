f95836927eac14d8dbc3e940ac217955bd4967ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-28 14:06:00 +0000

    Add package django-admin-panel-link/1.0.1/django_admin_panel_link-1.0.1-py3-none-any.whl

---
+++ packages/django-admin-panel-link/django_admin_panel_link-1.0.1-py3-none-any.whl/admin_panel_link/__init__.py
@@ -0,0 +1,5 @@
+from django.conf import settings
+
+ADMIN_LINK_ONLY_SUPERUSER = getattr(settings, 'ADMIN_LINK_ONLY_SUPERUSER', False)
+ADMIN_LINK_USE_STYLES = getattr(settings, 'ADMIN_LINK_USE_STYLES', True)
+__version__ = '1.0.1'
