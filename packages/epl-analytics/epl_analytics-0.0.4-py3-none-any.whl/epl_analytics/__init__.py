a0f02669970f346f93421190aa5a3dc6de23b7cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-28 19:06:38 +0000

    Add package epl-analytics/0.0.4/epl_analytics-0.0.4-py3-none-any.whl

---
+++ packages/epl-analytics/epl_analytics-0.0.4-py3-none-any.whl/epl_analytics/__init__.py
@@ -0,0 +1,12 @@
+# epl_analytics/__init__.py
+
+"""
+EPL Analytics
+=============
+
+Package Python pour l'analyse des notes des étudiants à l'EPL.
+"""
+
+__version__ = "0.0.1"
+
+from .core import EPLAnalytics
