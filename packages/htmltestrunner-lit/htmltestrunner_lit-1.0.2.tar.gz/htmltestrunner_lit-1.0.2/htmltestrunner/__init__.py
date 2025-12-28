5e13ad2d28dcc07ef2b69cbb8ace991dcb42c7cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-28 19:06:53 +0000

    Add package htmltestrunner-lit/1.0.2/htmltestrunner_lit-1.0.2.tar.gz

---
+++ packages/htmltestrunner-lit/htmltestrunner_lit-1.0.2.tar.gz/htmltestrunner_lit-1.0.2/htmltestrunner/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+HTMLTestRunner PY3 - 现代化的 Python 测试报告生成器
+"""
+
+from .runner import HTMLTestRunner
+
+__version__ = "1.0.2"
+__author__ = "Lit"
+__all__ = ["HTMLTestRunner"]
