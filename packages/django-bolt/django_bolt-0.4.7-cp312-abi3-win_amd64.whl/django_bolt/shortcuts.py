22706f212d2a4e3c5538f8722a1a846e81653dd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-28 17:05:21 +0000

    Add package django-bolt/0.4.7/django_bolt-0.4.7-cp312-abi3-win_amd64.whl

---
+++ packages/django-bolt/django_bolt-0.4.7-cp312-abi3-win_amd64.whl/django_bolt/shortcuts.py
@@ -0,0 +1,14 @@
+from django.template import loader
+
+from .responses import Response
+
+
+def render(
+    request, template_name : str, context=None, content_type=None, status=200, using=None
+):
+    """
+    Return an HttpResponse whose content is filled with the result of calling
+    django.template.loader.render_to_string() with the passed arguments.
+    """
+    content = loader.render_to_string(template_name, context, request, using=using)
+    return Response(content=content, media_type=content_type, status_code=status)
