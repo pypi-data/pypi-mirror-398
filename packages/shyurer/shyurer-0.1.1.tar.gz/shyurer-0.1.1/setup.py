71a699dbaa634def779e7dc02e65548da7426c6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-28 18:08:50 +0000

    Add package shyurer/0.1.1/shyurer-0.1.1.tar.gz

---
+++ packages/shyurer/shyurer-0.1.1.tar.gz/shyurer-0.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="shyurer",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=["colorama"],
+    python_requires=">=3.7",
+)
