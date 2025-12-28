9e0bc2ab9bee49391a8e320efce977cf71b3299f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-28 17:05:36 +0000

    Add package ngen-argoapi/0.1.9/ngen_argoapi-0.1.9.tar.gz

---
+++ packages/ngen-argoapi/ngen_argoapi-0.1.9.tar.gz/ngen_argoapi-0.1.9/ngen_argoapi/__init__.py
@@ -0,0 +1,10 @@
+import os
+
+# Default version, can be overridden by ARGOAPI_VERSION environment variable
+_DEFAULT_VERSION = "0.1.8"
+
+def _get_version():
+    """Get version from environment variable or use default."""
+    return os.environ.get("ARGOAPI_VERSION", _DEFAULT_VERSION)
+
+__version__ = _get_version()
