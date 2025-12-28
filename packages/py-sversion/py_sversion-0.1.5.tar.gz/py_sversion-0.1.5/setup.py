f1a28c88ebe4ebf39d47f049815d7957c320ca9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-28 18:07:39 +0000

    Add package py-sversion/0.1.5/py_sversion-0.1.5.tar.gz

---
+++ packages/py-sversion/py_sversion-0.1.5.tar.gz/py_sversion-0.1.5/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="py-sversion",
+    version="0.1.5",
+    description="Shyurer Engine Banner",
+    packages=find_packages(),
+    install_requires=["colorama"],
+    python_requires=">=3.7",
+)
