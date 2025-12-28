60ffbe01266d731c4ef3c91b9fdbe6aa9426bd59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-28 18:06:00 +0000

    Add package nchess/1.3.1/nchess-1.3.1-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/nchess/nchess-1.3.1-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/nchess/core/src/pyboard_getset.h
@@ -0,0 +1,9 @@
+#ifndef NCHESS_CORE_PYBOARD_GETSET_H
+#define NCHESS_CORE_PYBOARD_GETSET_H
+
+#define PY_SSIZE_CLEAN_H
+#include <Python.h>
+
+extern PyGetSetDef pyboard_getset[];
+
+#endif // NCHESS_CORE_PYBOARD_GETSET_H
