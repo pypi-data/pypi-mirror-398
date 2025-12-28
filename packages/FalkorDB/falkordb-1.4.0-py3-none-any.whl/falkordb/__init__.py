a0b33432f7696e2292ed8ada2588a864c36c1ccf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-28 14:06:21 +0000

    Add package FalkorDB/1.4.0/falkordb-1.4.0-py3-none-any.whl

---
+++ packages/FalkorDB/falkordb-1.4.0-py3-none-any.whl/falkordb/__init__.py
@@ -0,0 +1,7 @@
+from .falkordb       import FalkorDB
+from .node           import Node
+from .edge           import Edge
+from .path           import Path
+from .graph          import Graph
+from .execution_plan import ExecutionPlan, Operation
+from .query_result   import QueryResult
