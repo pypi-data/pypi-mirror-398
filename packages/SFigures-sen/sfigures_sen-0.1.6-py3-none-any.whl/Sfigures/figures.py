125fe44936ee3e923c2f6d0df5da41eee5f0c87d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-28 16:04:55 +0000

    Add package SFigures-sen/0.1.6/sfigures_sen-0.1.6-py3-none-any.whl

---
+++ packages/SFigures-sen/sfigures_sen-0.1.6-py3-none-any.whl/Sfigures/figures.py
@@ -0,0 +1,13 @@
+import math
+
+# Площадь квадрата
+def square_area(side):
+    return side * side
+
+# Площадь круга
+def circle_area(radius):
+    return math.pi * (radius ** 2)
+
+# Площадь треугольника
+def triangle_area(base, height):
+    return 0.5 * base * height
