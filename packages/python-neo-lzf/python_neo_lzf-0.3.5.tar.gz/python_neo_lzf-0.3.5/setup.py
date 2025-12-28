c220378c3aa0d0b3afb252d03516b16b9790f6fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-28 17:05:54 +0000

    Add package python-neo-lzf/0.3.5/python_neo_lzf-0.3.5.tar.gz

---
+++ packages/python-neo-lzf/python_neo_lzf-0.3.5.tar.gz/python_neo_lzf-0.3.5/setup.py
@@ -0,0 +1,47 @@
+import sys
+from pathlib import Path
+
+from setuptools import Extension, setup
+
+VERSION = (0, 3, 5)
+
+compile_args = ["/W3"] if sys.platform == "win32" else ["-Wall"]
+long_description = Path("README.txt").read_text(encoding="utf-8")
+
+setup(
+    name="python-neo-lzf",
+    description="A fork of python-lzf with pre-built wheel files.",
+    long_description=long_description,
+    long_description_content_type="text/plain",
+    version=".".join(map(str, VERSION)),
+    author="Fledge Shiu",
+    author_email="xzk0701@gmail.com",
+    url="https://github.com/FledgeXu/python-neo-lzf",
+    ext_modules=[
+        Extension(
+            "lzf",
+            ["lzf_module.c", "lzf_c.c", "lzf_d.c"],
+            include_dirs=["."],
+            extra_compile_args=compile_args,
+        )
+    ],
+    license="BSD-3-Clause",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
+        "Programming Language :: C",
+        "Topic :: System :: Archiving :: Compression",
+    ],
+    python_requires=">=3.7",
+)
