b8f5aaa309958da9fc248c816ec2227285ddd915
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-28 17:05:35 +0000

    Add package onnx-paddleocr/0.2.4/onnx_paddleocr-0.2.4-py3-none-any.whl

---
+++ packages/onnx-paddleocr/onnx_paddleocr-0.2.4-py3-none-any.whl/onnx_ocr/__init__.py
@@ -0,0 +1,13 @@
+from onnx_ocr.onnx_paddleocr import (
+    ONNXPaddleOcr,
+    result_to_json_data,
+    save_to_img,
+    save_to_json,
+)
+
+__all__ = [
+    ONNXPaddleOcr,
+    result_to_json_data,
+    save_to_img,
+    save_to_json,
+]
