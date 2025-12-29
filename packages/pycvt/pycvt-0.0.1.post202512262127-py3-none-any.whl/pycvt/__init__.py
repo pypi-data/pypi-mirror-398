d341b22cdcfd4a53b3094a06befbce9ad97e3438
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 04:24:12 +0000

    Add package pycvt/0.0.1.post202512262127/pycvt-0.0.1.post202512262127-py3-none-any.whl

---
+++ packages/pycvt/pycvt-0.0.1.post202512262127-py3-none-any.whl/pycvt/__init__.py
@@ -0,0 +1,54 @@
+from importlib.resources import files
+from pathlib import Path
+
+basepath = files("pycvt")
+
+test_file = Path(basepath) / "static/pexels-dsengul77-32423632.jpg"
+
+from .annotations.yolo import (
+    load_yolo_annotations,
+    save_yolo_annotations,
+    load_yolo_names,
+)
+
+
+from .vision.plot_boxes import (
+    draw_bounding_boxes,
+)
+
+
+from .vision.bbox import (
+    xyxy2xywh,
+    xywh2xyxy,
+    xyxy2xywhn,
+    xywhn2xyxy,
+    box_iou,
+    generate_sliding_windows,
+    sliding_crop,
+    scale_boxes,
+)
+
+
+from .clolors.colors import (
+    getcolor, # deprecated, use get_color instead
+    get_color
+)
+
+
+__all__ = [
+    "load_yolo_annotations",
+    "save_yolo_annotations",
+    "load_yolo_names",
+    "draw_bounding_boxes",
+    "xyxy2xywh",
+    "xywh2xyxy",
+    "xyxy2xywhn",
+    "xywhn2xyxy",
+    "box_iou",
+    "generate_sliding_windows",
+    "sliding_crop",
+    "scale_boxes",
+    "getcolor",
+    "get_color",
+    "test_file",
+]
