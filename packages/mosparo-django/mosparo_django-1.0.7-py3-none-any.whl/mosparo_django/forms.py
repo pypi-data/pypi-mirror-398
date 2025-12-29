8559a7fa49eae4af8c18eaa127efd94607b87143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 07:09:51 +0000

    Add package mosparo-django/1.0.7/mosparo_django-1.0.7-py3-none-any.whl

---
+++ packages/mosparo-django/mosparo_django-1.0.7-py3-none-any.whl/mosparo_django/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+
+from .fields import MosparoField
+
+
+class MosparoForm(forms.Form):
+    def clean(self):
+        mosparo_field = None
+        for key, field in self.fields.items():
+            if isinstance(field, MosparoField):
+                mosparo_field = field
+                break
+
+        if mosparo_field is None:
+            raise ValidationError(_("No mosparo field found in form."))
+
+        mosparo_field.verify_data(self)
+
+        return super().clean()
