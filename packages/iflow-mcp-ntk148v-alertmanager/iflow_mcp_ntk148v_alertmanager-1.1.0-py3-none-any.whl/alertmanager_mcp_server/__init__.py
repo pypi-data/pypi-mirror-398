e0161850a07ce7d2bbc9bb91b47e85daafea7571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 03:35:18 +0000

    Add package iflow-mcp-ntk148v-alertmanager/1.1.0/iflow_mcp_ntk148v_alertmanager-1.1.0-py3-none-any.whl

---
+++ packages/iflow-mcp-ntk148v-alertmanager/iflow_mcp_ntk148v_alertmanager-1.1.0-py3-none-any.whl/alertmanager_mcp_server/__init__.py
@@ -0,0 +1,7 @@
+"""Prometheus Alertmanager MCP Server.
+
+A Model Context Protocol (MCP) server that enables AI assistants to query
+and integrate with Promeheus Alertmanager.
+"""
+
+__version__ = "0.0.1"
