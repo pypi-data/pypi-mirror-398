f04ce53689628fd567857a38a3c935dfc6bba223
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 05:12:26 +0000

    Add package zavudev/0.15.0/zavudev-0.15.0-py3-none-any.whl

---
+++ packages/zavudev/zavudev-0.15.0-py3-none-any.whl/zavudev/resources/__init__.py
@@ -0,0 +1,103 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .senders import (
+    SendersResource,
+    AsyncSendersResource,
+    SendersResourceWithRawResponse,
+    AsyncSendersResourceWithRawResponse,
+    SendersResourceWithStreamingResponse,
+    AsyncSendersResourceWithStreamingResponse,
+)
+from .contacts import (
+    ContactsResource,
+    AsyncContactsResource,
+    ContactsResourceWithRawResponse,
+    AsyncContactsResourceWithRawResponse,
+    ContactsResourceWithStreamingResponse,
+    AsyncContactsResourceWithStreamingResponse,
+)
+from .messages import (
+    MessagesResource,
+    AsyncMessagesResource,
+    MessagesResourceWithRawResponse,
+    AsyncMessagesResourceWithRawResponse,
+    MessagesResourceWithStreamingResponse,
+    AsyncMessagesResourceWithStreamingResponse,
+)
+from .templates import (
+    TemplatesResource,
+    AsyncTemplatesResource,
+    TemplatesResourceWithRawResponse,
+    AsyncTemplatesResourceWithRawResponse,
+    TemplatesResourceWithStreamingResponse,
+    AsyncTemplatesResourceWithStreamingResponse,
+)
+from .broadcasts import (
+    BroadcastsResource,
+    AsyncBroadcastsResource,
+    BroadcastsResourceWithRawResponse,
+    AsyncBroadcastsResourceWithRawResponse,
+    BroadcastsResourceWithStreamingResponse,
+    AsyncBroadcastsResourceWithStreamingResponse,
+)
+from .introspect import (
+    IntrospectResource,
+    AsyncIntrospectResource,
+    IntrospectResourceWithRawResponse,
+    AsyncIntrospectResourceWithRawResponse,
+    IntrospectResourceWithStreamingResponse,
+    AsyncIntrospectResourceWithStreamingResponse,
+)
+from .phone_numbers import (
+    PhoneNumbersResource,
+    AsyncPhoneNumbersResource,
+    PhoneNumbersResourceWithRawResponse,
+    AsyncPhoneNumbersResourceWithRawResponse,
+    PhoneNumbersResourceWithStreamingResponse,
+    AsyncPhoneNumbersResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "MessagesResource",
+    "AsyncMessagesResource",
+    "MessagesResourceWithRawResponse",
+    "AsyncMessagesResourceWithRawResponse",
+    "MessagesResourceWithStreamingResponse",
+    "AsyncMessagesResourceWithStreamingResponse",
+    "TemplatesResource",
+    "AsyncTemplatesResource",
+    "TemplatesResourceWithRawResponse",
+    "AsyncTemplatesResourceWithRawResponse",
+    "TemplatesResourceWithStreamingResponse",
+    "AsyncTemplatesResourceWithStreamingResponse",
+    "SendersResource",
+    "AsyncSendersResource",
+    "SendersResourceWithRawResponse",
+    "AsyncSendersResourceWithRawResponse",
+    "SendersResourceWithStreamingResponse",
+    "AsyncSendersResourceWithStreamingResponse",
+    "ContactsResource",
+    "AsyncContactsResource",
+    "ContactsResourceWithRawResponse",
+    "AsyncContactsResourceWithRawResponse",
+    "ContactsResourceWithStreamingResponse",
+    "AsyncContactsResourceWithStreamingResponse",
+    "BroadcastsResource",
+    "AsyncBroadcastsResource",
+    "BroadcastsResourceWithRawResponse",
+    "AsyncBroadcastsResourceWithRawResponse",
+    "BroadcastsResourceWithStreamingResponse",
+    "AsyncBroadcastsResourceWithStreamingResponse",
+    "IntrospectResource",
+    "AsyncIntrospectResource",
+    "IntrospectResourceWithRawResponse",
+    "AsyncIntrospectResourceWithRawResponse",
+    "IntrospectResourceWithStreamingResponse",
+    "AsyncIntrospectResourceWithStreamingResponse",
+    "PhoneNumbersResource",
+    "AsyncPhoneNumbersResource",
+    "PhoneNumbersResourceWithRawResponse",
+    "AsyncPhoneNumbersResourceWithRawResponse",
+    "PhoneNumbersResourceWithStreamingResponse",
+    "AsyncPhoneNumbersResourceWithStreamingResponse",
+]
