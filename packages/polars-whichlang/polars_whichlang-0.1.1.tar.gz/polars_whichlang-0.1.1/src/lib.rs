ceb62271a4e28a0e89cdd6a405f28f3da421573c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 06:07:21 +0000

    Add package polars-whichlang/0.1.1/polars_whichlang-0.1.1.tar.gz

---
+++ packages/polars-whichlang/polars_whichlang-0.1.1.tar.gz/polars_whichlang-0.1.1/src/lib.rs
@@ -0,0 +1,20 @@
+#![allow(clippy::unused_unit)]
+
+use polars::prelude::*;
+use pyo3_polars::PolarsAllocator;
+use pyo3_polars::derive::polars_expr;
+use whichlang::detect_language;
+
+#[global_allocator]
+static ALLOC: PolarsAllocator = PolarsAllocator::new();
+
+#[polars_expr(output_type=String)]
+fn detect_lang(inputs: &[Series]) -> PolarsResult<Series> {
+    let ca: &StringChunked = inputs[0].str()?;
+    let out: StringChunked = ca.apply_into_string_amortized(|value: &str, output: &mut String| {
+        let lang = detect_language(value);
+        let code = lang.three_letter_code();
+        output.push_str(code);
+    });
+    Ok(out.into_series())
+}
