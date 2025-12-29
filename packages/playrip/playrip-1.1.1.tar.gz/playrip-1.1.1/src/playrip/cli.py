3783659ee6cede93cadfaba7fcafc4c3180c5376
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 05:12:11 +0000

    Add package playrip/1.1.1/playrip-1.1.1.tar.gz

---
+++ packages/playrip/playrip-1.1.1.tar.gz/playrip-1.1.1/src/playrip/cli.py
@@ -0,0 +1,14 @@
+from playrip import Dowload
+import sys
+
+def main():
+    url = sys.argv[1]
+    print("abaixando")
+    if("youtu" in url):
+        print("toutube")
+        if(len(sys.argv) > 2):
+            tipo = sys.argv[2]
+            Dowload.Youtube(url=url, tipo=tipo)
+    if("spotify" in url):
+        print("spotify")
+        Dowload.Spotify(url=url)
