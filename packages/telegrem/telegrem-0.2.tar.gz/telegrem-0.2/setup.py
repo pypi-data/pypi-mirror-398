4305878d8eecedd95214b624b558d7418fc11073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 05:11:57 +0000

    Add package telegrem/0.2/telegrem-0.2.tar.gz

---
+++ packages/telegrem/telegrem-0.2.tar.gz/telegrem-0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="telegrem",
+    version="0.2",
+    packages=find_packages(),
+    author="Elijah Joseph",
+    description="Simple Scopper Library",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.6',
+)
