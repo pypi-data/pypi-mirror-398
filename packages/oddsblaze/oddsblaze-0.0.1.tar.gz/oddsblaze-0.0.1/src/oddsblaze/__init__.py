9e27da9716d2bf4569dfa01dc6776dd6d5d78d0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 04:24:59 +0000

    Add package oddsblaze/0.0.1/oddsblaze-0.0.1.tar.gz

---
+++ packages/oddsblaze/oddsblaze-0.0.1.tar.gz/oddsblaze-0.0.1/src/oddsblaze/__init__.py
@@ -0,0 +1,30 @@
+"""OddsBlaze Python SDK."""
+
+from importlib.metadata import version
+
+from .client import OddsblazeClient
+from .exceptions import (
+    AuthenticationError,
+    EventNotFoundError,
+    InvalidMarketError,
+    OddsblazeError,
+    PlayerNotFoundError,
+)
+from .settings import OddsblazeSettings, PriceFormat, get_settings
+
+__version__ = version("oddsblaze")
+
+__all__ = [
+    # Client
+    "OddsblazeClient",
+    # Settings
+    "OddsblazeSettings",
+    "PriceFormat",
+    "get_settings",
+    # Exceptions
+    "OddsblazeError",
+    "AuthenticationError",
+    "InvalidMarketError",
+    "EventNotFoundError",
+    "PlayerNotFoundError",
+]
