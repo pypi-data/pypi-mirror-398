90a3b6174eab185f50886ae02f916429ab75e8bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 03:35:50 +0000

    Add package rdkit-headers/2025.9.3.post1/rdkit_headers-2025.9.3.post1.tar.gz

---
+++ packages/rdkit-headers/rdkit_headers-2025.9.3.post1.tar.gz/rdkit_headers-2025.9.3.post1/rdkit_headers/include/rdkit/ForceField/UFF/PositionConstraint.h
@@ -0,0 +1,20 @@
+//
+//  Copyright (C) 2013-2024 Paolo Tosco and other RDKit contributors
+//
+//   @@ All Rights Reserved @@
+//  This file is part of the RDKit.
+//  The contents are covered by the terms of the BSD license
+//  which is included in the file license.txt, found at the root
+//  of the RDKit source tree.
+//
+#include <RDGeneral/export.h>
+#ifndef RD_UFFPOSITIONCONSTRAINT_H
+#define RD_UFFPOSITIONCONSTRAINT_H
+#include <ForceField/PositionConstraint.h>
+
+namespace ForceFields {
+namespace UFF {
+using PositionConstraintContrib = ForceFields::PositionConstraintContrib;
+}  // namespace UFF
+}  // namespace ForceFields
+#endif
