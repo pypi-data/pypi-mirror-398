eee6de438a0ab7555e1dda2c24185a91b64c2d8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 06:07:20 +0000

    Add package ipyjadwal/0.1.51/ipyjadwal-0.1.51.tar.gz

---
+++ packages/ipyjadwal/ipyjadwal-0.1.51.tar.gz/ipyjadwal-0.1.51/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ipyjadwal",
+    version="0.1.51",
+    python_requires=">=3.7",
+    description="A clean, interactive Google Sheet explorer for Colab (Jadwal)",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Mustafa Marzouk",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        "gspread>=5.0.0",
+        "pandas",
+        "ipywidgets",
+        "google-auth",
+        "ipython",
+    ],
+    keywords=["jupyter", "widget", "google sheets", "colab", "jadwal"],
+)
