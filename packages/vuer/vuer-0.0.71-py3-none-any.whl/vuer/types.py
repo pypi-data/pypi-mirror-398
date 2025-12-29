616369a7824c851cd6bba89dc37de622c4ef09df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 07:09:37 +0000

    Add package vuer/0.0.71/vuer-0.0.71-py3-none-any.whl

---
+++ packages/vuer/vuer-0.0.71-py3-none-any.whl/vuer/types.py
@@ -0,0 +1,56 @@
+from math import pi
+from typing import NamedTuple, Callable, Union, Coroutine
+from uuid import UUID
+
+from vuer.events import ClientEvent
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from vuer.server import VuerSession
+
+IDType = Union[UUID, str]
+"""IDType is either a UUID or a string. Not in use."""
+
+CoroutineFn = Callable[[], Coroutine]
+"""A function that returns a coroutine. Not in use."""
+
+# SendProxy = Callable[[ServerEvent], None]
+EventHandler = Callable[[ClientEvent, "VuerSession"], None]
+"""Defines a function that handles a client event. Second argument is the VuerSession instance bound
+to a specific client connected through a websocket session."""
+
+SocketHandler = Callable[["VuerSession"], Coroutine]
+"""Defines a function that spawns a new entity. Argument is the VuerSession instance."""
+
+
+class Vector3(NamedTuple):
+    x: int
+    y: int
+    z: int
+
+
+class Euler(NamedTuple):
+    x: int
+    y: int
+    z: int
+    order: str = "XYZ"
+
+
+class EulerDeg(Euler):
+    unit: str = "deg"
+
+    def to_rad(self) -> Euler:
+        return Euler(*[v / 180 * pi for v in self[:3]], order=self.order)
+
+
+class Body(NamedTuple):
+    position: Vector3
+    rotation: Euler
+    scale: float
+
+
+if __name__ == "__main__":
+    e = Euler(x=1, y=2, z=3)
+    assert e[:3] == (1, 2, 3)
+    assert e == (1, 2, 3, "XYZ")
