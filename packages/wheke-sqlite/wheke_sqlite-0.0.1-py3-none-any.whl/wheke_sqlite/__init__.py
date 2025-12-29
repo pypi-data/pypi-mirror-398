8e3e3bacd0a1c4099d8f57a4ec9a36084b3f1bac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 05:12:31 +0000

    Add package wheke-sqlite/0.0.1/wheke_sqlite-0.0.1-py3-none-any.whl

---
+++ packages/wheke-sqlite/wheke_sqlite-0.0.1-py3-none-any.whl/wheke_sqlite/__init__.py
@@ -0,0 +1,8 @@
+from ._service import DatabaseService, get_database_service
+from ._settings import DatabaseSettings
+
+__all__ = [
+    "DatabaseService",
+    "DatabaseSettings",
+    "get_database_service",
+]
---
+++ packages/wheke-sqlite/wheke_sqlite-0.0.1-py3-none-any.whl/wheke_sqlite/_pod.py
@@ -0,0 +1,12 @@
+from wheke import Pod, ServiceConfig
+
+from ._cli import cli
+from ._service import DatabaseService, database_service_factory
+
+core_pod = Pod(
+    "database",
+    services=[
+        ServiceConfig(DatabaseService, database_service_factory, as_value=True),
+    ],
+    cli=cli,
+)
