5404140b3a766ab9534f020ca0af9c3c392e18dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 03:35:24 +0000

    Add package pedros/0.0.9/pedros-0.0.9.tar.gz

---
+++ packages/pedros/pedros-0.0.9.tar.gz/pedros-0.0.9/examples/progress_bar.py
@@ -0,0 +1,19 @@
+import time
+
+from pedros.logger import get_logger
+from pedros.progress_bar import progbar
+
+
+def run_demo(backend = "auto"):
+    logger = get_logger()
+    logger.info(f"Using backend: {backend}")
+    for _ in progbar(range(10), backend=backend):
+        time.sleep(0.1)
+
+
+if __name__ == "__main__":
+    run_demo("none")
+    run_demo("rich")
+    run_demo("tqdm")
+    run_demo("auto")
+    run_demo("dummy")
