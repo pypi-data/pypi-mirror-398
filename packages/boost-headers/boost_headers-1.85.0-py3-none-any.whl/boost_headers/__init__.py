bf672661aa35a82d1eb0e0e22cc5307ba6f1ff46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 03:35:53 +0000

    Add package boost-headers/1.85.0/boost_headers-1.85.0-py3-none-any.whl

---
+++ packages/boost-headers/boost_headers-1.85.0-py3-none-any.whl/boost_headers/__init__.py
@@ -0,0 +1,12 @@
+"""boost-headers headers."""
+
+from __future__ import annotations
+
+from pathlib import Path
+
+__version__ = "1.85.0"
+
+
+def get_include() -> Path:
+    """Return the path to the boost-headers headers."""
+    return Path(__file__).parent / "include"
