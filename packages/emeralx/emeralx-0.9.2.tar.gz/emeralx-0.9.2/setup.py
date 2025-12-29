4cb1930cb1112cef9f1893bad7aac2774e2f026b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 08:06:34 +0000

    Add package emeralx/0.9.2/emeralx-0.9.2.tar.gz

---
+++ packages/emeralx/emeralx-0.9.2.tar.gz/emeralx-0.9.2/setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup, find_packages
+
+with open("README.rst", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    # 基本信息
+    name="emeralx",
+    version="0.9.2",
+    author="modaochangyang",
+    author_email="1602422136@qq.com",
+    description="A library for making simple 2D games, an advanced version for Emeral",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    
+    packages=find_packages(),
+    
+    # Python版本要求
+    python_requires=">=3.7",
+    
+    # 依赖
+    install_requires=[
+        'pygame>=2.5.0',          # 用于图形、声音、事件处理
+        'pywin32>=306',           # Windows API调用（GetMonitorInfo等）
+        'numpy>=1.24.0',          # 图像处理（numpy数组操作）
+        'Pillow>=10.0.0',         # GIF动画处理（Image.open等）
+    ],
+    
+    # 分类器
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    
+    # 许可证
+    license="MIT",
+    
+    # 关键词
+    keywords="emeralx",
+    
+    # 包含数据文件
+    include_package_data=True,
+    package_data={
+        "emeralx": ["resources/*.png","resources/*.ttf"],
+    }
+)
