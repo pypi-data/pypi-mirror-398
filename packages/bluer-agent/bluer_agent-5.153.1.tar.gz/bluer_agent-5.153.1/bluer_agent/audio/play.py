502511dc7d061594b42873a6d856de82bdce4737
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 03:35:14 +0000

    Add package bluer-agent/5.153.1/bluer_agent-5.153.1.tar.gz

---
+++ packages/bluer-agent/bluer_agent-5.153.1.tar.gz/bluer_agent-5.153.1/bluer_agent/audio/play.py
@@ -0,0 +1,41 @@
+from blueness import module
+from bluer_options import string
+from bluer_options.host import is_rpi
+from bluer_objects import file, objects
+from bluer_objects.host import shell
+
+from bluer_agent import NAME
+from bluer_agent.logger import logger
+
+
+NAME = module.name(__file__, NAME)
+
+
+def play(
+    object_name: str,
+    filename: str = "audio.wav",
+) -> bool:
+    full_filename = objects.path_of(
+        object_name=object_name,
+        filename=filename,
+    )
+    if not file.exists(full_filename):
+        logger.error(f"file not found: {full_filename}")
+        return False
+
+    logger.info(
+        "{}.play: {}/{} ({})".format(
+            NAME,
+            object_name,
+            filename,
+            string.pretty_bytes(file.size(full_filename)),
+        )
+    )
+
+    return shell(
+        command=[
+            "aplay" if is_rpi() else "afplay",
+            full_filename,
+        ],
+        log=True,
+    )
