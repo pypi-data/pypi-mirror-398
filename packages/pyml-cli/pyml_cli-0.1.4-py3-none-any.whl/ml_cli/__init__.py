aa4a84816aa3ea2bb296a9be992342aed1bf1457
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 06:07:44 +0000

    Add package pyml-cli/0.1.4/pyml_cli-0.1.4-py3-none-any.whl

---
+++ packages/pyml-cli/pyml_cli-0.1.4-py3-none-any.whl/ml_cli/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level API for pyml-cli.
+
+This is the file from which you can do:
+
+    from ml_cli import some_function
+
+Use it to control the top-level API of your Python data science project.
+"""
---
+++ packages/pyml-cli/pyml_cli-0.1.4-py3-none-any.whl/ml_cli/models.py
@@ -0,0 +1,1 @@
+"""Custom model code for pyml-cli."""
