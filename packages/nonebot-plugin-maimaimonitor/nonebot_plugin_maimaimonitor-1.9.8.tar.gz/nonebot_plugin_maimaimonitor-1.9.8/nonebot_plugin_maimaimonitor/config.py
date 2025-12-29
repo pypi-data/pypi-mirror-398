1f16aa5313e97bc3baeb0dcbac307008dd3282d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 04:24:07 +0000

    Add package nonebot-plugin-maimaimonitor/1.9.8/nonebot_plugin_maimaimonitor-1.9.8.tar.gz

---
+++ packages/nonebot-plugin-maimaimonitor/nonebot_plugin_maimaimonitor-1.9.8.tar.gz/nonebot_plugin_maimaimonitor-1.9.8/nonebot_plugin_maimaimonitor/config.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings
+from pydantic import ConfigDict, Field
+from typing import Dict, Optional
+
+class Config(BaseSettings):
+    model_config = ConfigDict(extra='ignore')
+    
+
+    maimai_bot_client_id: Optional[str] = Field(default=None)
+    maimai_bot_private_key: Optional[str] = None
+    maimai_bot_display_name: Optional[str] = None
+
+    maimai_worker_url: str = "https://maiapi.chongxi.us"
+    command_aliases: Dict[str, str] = {}
