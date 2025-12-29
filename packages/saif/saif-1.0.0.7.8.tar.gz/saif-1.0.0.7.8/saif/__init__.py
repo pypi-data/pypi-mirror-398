c5100c82716f6ed9cd1a2c0b16e25daf7a2dde57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 07:09:51 +0000

    Add package saif/1.0.0.7.8/saif-1.0.0.7.8.tar.gz

---
+++ packages/saif/saif-1.0.0.7.8.tar.gz/saif-1.0.0.7.8/saif/__init__.py
@@ -0,0 +1,4 @@
+from .core import inLove, Love, loveWithWhom, get_version
+
+__version__ = "1.0.0.7.8"
+__all__ = ['inLove', 'Love', 'loveWithWhom', 'get_version']
---
+++ packages/saif/saif-1.0.0.7.8.tar.gz/saif-1.0.0.7.8/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="saif",
+    version="1.0.0.7.8",
+    author="Saif",
+    author_email="saifullahanwar00040@gmail.com",  # CHANGE THIS
+    description="Saif loves Myheer",
+    long_description="Saif is in Love with Myheer ❤️",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.6',
+)
