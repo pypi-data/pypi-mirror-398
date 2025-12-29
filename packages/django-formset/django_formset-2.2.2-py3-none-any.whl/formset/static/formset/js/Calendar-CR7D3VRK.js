0cbf1173aba7ef618cf06e2259adcb80da1edde2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 05:12:25 +0000

    Add package django-formset/2.2.2/django_formset-2.2.2-py3-none-any.whl

---
+++ packages/django-formset/django_formset-2.2.2-py3-none-any.whl/formset/static/formset/js/Calendar-CR7D3VRK.js
@@ -0,0 +1,1 @@
+import{a,b,c,d,e}from"./chunk-VNDV5BC6.js";import"./chunk-74KQCU7Q.js";import"./chunk-S7O4ILSJ.js";import"./chunk-JC2BH436.js";export{a as CalendarSheet,b as DateCalendarElement,d as DateRangeCalendarElement,c as DateTimeCalendarElement,e as DateTimeRangeCalendarElement};
