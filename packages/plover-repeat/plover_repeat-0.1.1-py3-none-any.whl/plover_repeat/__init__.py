df9ee6bb1b43aea43b038711138e4fc75ef32b07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 04:24:30 +0000

    Add package plover-repeat/0.1.1/plover_repeat-0.1.1-py3-none-any.whl

---
+++ packages/plover-repeat/plover_repeat-0.1.1-py3-none-any.whl/plover_repeat/__init__.py
@@ -0,0 +1,11 @@
+"""
+Plover Repeat Plugin
+
+A Plover extension that allows you to repeat previous strokes using binary-encoded commands.
+"""
+
+__version__ = '0.1.0'
+
+from plover_repeat.plover_repeat import PloverRepeat
+
+__all__ = ['PloverRepeat']
