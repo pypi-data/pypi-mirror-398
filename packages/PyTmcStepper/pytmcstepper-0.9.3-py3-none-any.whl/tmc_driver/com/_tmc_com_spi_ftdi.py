79ff3b2c0f391e8248abee1fb31a42305d44fc08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 08:06:10 +0000

    Add package PyTmcStepper/0.9.3/pytmcstepper-0.9.3-py3-none-any.whl

---
+++ packages/PyTmcStepper/pytmcstepper-0.9.3-py3-none-any.whl/tmc_driver/com/_tmc_com_spi_ftdi.py
@@ -0,0 +1,49 @@
+# pylint: disable=unused-import
+# pylint: disable=too-few-public-methods
+"""
+TmcComSpiFtdi stepper driver spi module
+"""
+
+from pyftdi.spi import SpiPort
+from .._tmc_exceptions import TmcComException, TmcDriverException
+from ._tmc_com_spi_base import TmcComSpiBase, TmcLogger
+
+
+class TmcComSpiFtdi(TmcComSpiBase):
+    """TmcComSpiFtdi
+
+    this class is used to communicate with the TMC via SPI via FT232H USB adapter
+    it can be used to change the settings of the TMC.
+    like the current or the microsteppingmode
+    """
+
+    def __init__(self, spi_port: SpiPort, driver_address: int = 0):
+        """constructor
+
+        Args:
+            spi_port (SpiPort): pyftdi SpiPort object
+            driver_address (int, optional): driver address. Defaults to 0.
+        """
+        super().__init__(driver_address)
+
+        self.spi = spi_port
+
+    def init(self):
+        """init - SPI port is already configured via pyftdi"""
+
+    def __del__(self):
+        self.deinit()
+
+    def deinit(self):
+        """destructor"""
+
+    def _spi_transfer(self, data: list) -> list:
+        """Perform SPI transfer using pyftdi
+
+        Args:
+            data: Data to send
+
+        Returns:
+            Received data
+        """
+        return list(self.spi.exchange(bytes(data), duplex=True))
