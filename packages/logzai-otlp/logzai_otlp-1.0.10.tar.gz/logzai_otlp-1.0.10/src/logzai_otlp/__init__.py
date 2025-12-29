e2c934087320cac5d3ebbccdfbf793ddb8c383a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 05:11:57 +0000

    Add package logzai-otlp/1.0.10/logzai_otlp-1.0.10.tar.gz

---
+++ packages/logzai-otlp/logzai_otlp-1.0.10.tar.gz/logzai_otlp-1.0.10/src/logzai_otlp/__init__.py
@@ -0,0 +1,21 @@
+from .main import LogzAI, logzai
+from .plugins import (
+    LogzAIPlugin,
+    CleanupFunction,
+    PluginEntry,
+    pydantic_ai_plugin,
+    fastapi_plugin,
+    langchain_plugin,
+)
+
+# Export the class for direct instantiation and singleton instance
+__all__ = [
+    "LogzAI",
+    "logzai",
+    "LogzAIPlugin",
+    "CleanupFunction",
+    "PluginEntry",
+    "pydantic_ai_plugin",
+    "fastapi_plugin",
+    "langchain_plugin",
+]
