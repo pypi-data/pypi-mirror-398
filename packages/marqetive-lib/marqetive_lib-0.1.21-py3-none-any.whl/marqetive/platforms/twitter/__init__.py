d84b91d8cf292482f53146b37b9283af75bce733
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 04:24:06 +0000

    Add package marqetive-lib/0.1.21/marqetive_lib-0.1.21-py3-none-any.whl

---
+++ packages/marqetive-lib/marqetive_lib-0.1.21-py3-none-any.whl/marqetive/platforms/twitter/__init__.py
@@ -0,0 +1,6 @@
+"""Twitter/X platform integration."""
+
+from marqetive.platforms.twitter.client import TwitterClient
+from marqetive.platforms.twitter.models import TwitterPostRequest
+
+__all__ = ["TwitterClient", "TwitterPostRequest"]
