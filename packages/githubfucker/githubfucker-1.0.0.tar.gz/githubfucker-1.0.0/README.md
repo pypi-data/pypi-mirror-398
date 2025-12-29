e46febf200344aaac9ae1c07958cfaf7c8d2401d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 07:09:16 +0000

    Add package githubfucker/1.0.0/githubfucker-1.0.0.tar.gz

---
+++ packages/githubfucker/githubfucker-1.0.0.tar.gz/githubfucker-1.0.0/README.md
@@ -0,0 +1,35 @@
+# 介绍
+githubfucker是一个由python学霸开发的github/pip/node/docker加速工具
+
+# 安装
+```bash
+pip install githubfucker
+
+# 使用
+## 全局加速
+** Windows (以管理员运行) **
+```bash
+fuckit --all
+
+** Linux/Mac/Termux **
+```bash
+sudo fuckit --all
+
+## 选择性加速
+**加速github**
+
+```bash
+sudo fuckit --github
+
+**加速npm和pip**
+
+```bash
+fuckit --pip --npm
+
+```bash
+sudo fuckit --docker --apt
+
+**帮助**
+```bash
+fuckit --help
+# 欢迎关注，python学霸公众号！
---
+++ packages/githubfucker/githubfucker-1.0.0.tar.gz/githubfucker-1.0.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import os
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="githubfucker",           
+    version="1.0.0",
+    author="pythonxueba",
+    description="Dev All-in-One Accelerator (Github, Pip, Docker, Apt, UV)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pythonxueba", 
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "requests",
+        "colorama",
+    ],
+    entry_points={
+        "console_scripts": [
+            "fuckit=githubfucker.cli:main",
+        ],
+    },
+)
