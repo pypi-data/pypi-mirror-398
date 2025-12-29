42982b72c33bd8f549e38ac6a2f67bb7800daf6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 03:35:25 +0000

    Add package iflow-mcp-searxng/0.1.0/iflow_mcp_searxng-0.1.0-py3-none-any.whl

---
+++ packages/iflow-mcp-searxng/iflow_mcp_searxng-0.1.0-py3-none-any.whl/mcp_searxng/__init__.py
@@ -0,0 +1,5 @@
+import asyncio
+from mcp_searxng.main import run
+
+def main() -> None:
+    asyncio.run(run())
---
+++ packages/iflow-mcp-searxng/iflow_mcp_searxng-0.1.0-py3-none-any.whl/mcp_searxng/server.py
@@ -0,0 +1,5 @@
+from mcp.server import Server
+
+__all__ = ["server"]
+
+server = Server("searxng")
