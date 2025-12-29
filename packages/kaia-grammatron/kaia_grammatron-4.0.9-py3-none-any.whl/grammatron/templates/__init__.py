ed26fde6c926f432e57643ea6f4a27bfaa37ba74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 08:06:45 +0000

    Add package kaia-grammatron/4.0.9/kaia_grammatron-4.0.9-py3-none-any.whl

---
+++ packages/kaia-grammatron/kaia_grammatron-4.0.9-py3-none-any.whl/grammatron/templates/__init__.py
@@ -0,0 +1,6 @@
+from .template import Template, TemplateContext
+from .utterance_sequence import UtterancesSequence
+from .utterance import Utterance
+from .collections import TemplatesCollection
+from .template_base import TemplateBase
+from .template_parser_base import TemplateParserBase
