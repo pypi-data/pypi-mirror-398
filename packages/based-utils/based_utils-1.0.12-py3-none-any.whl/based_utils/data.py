48b22f8fb54429ed0377050b98d90af190fbf6ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 06:07:28 +0000

    Add package based-utils/1.0.12/based_utils-1.0.12-py3-none-any.whl

---
+++ packages/based-utils/based_utils-1.0.12-py3-none-any.whl/based_utils/data.py
@@ -0,0 +1,101 @@
+from collections import deque
+from typing import TYPE_CHECKING, overload
+
+if TYPE_CHECKING:
+    from collections.abc import Callable, Iterable, Iterator, Mapping
+
+
+def ignore[T](v: T) -> T:
+    return v
+
+
+@overload
+def try_convert[T, R](cls: Callable[[T], R], val: T, *, default: R) -> R: ...
+
+
+@overload
+def try_convert[T, R](
+    cls: Callable[[T], R], val: T, *, default: None = None
+) -> R | None: ...
+
+
+def try_convert[T, R](cls: Callable[[T], R], val: T, *, default: R = None) -> R | None:
+    try:
+        return cls(val)
+    except ValueError:
+        return default
+
+
+def consume(iterator: Iterator) -> None:
+    """
+    Consume an iterator entirely.
+
+    We will achieve this by feeding the entire iterator into a zero-length deque.
+    Redefined here to avoid needing more_itertools for just this function.
+    """
+    deque(iterator, maxlen=0)
+
+
+def compose_number(numbers: Iterable[int]) -> int:
+    return int("".join(str(n) for n in numbers))
+
+
+def bits_to_int(items: Iterable[object]) -> int:
+    """
+    Convert boolean/int array -> number.
+
+    >>> bits_to_int([True, False, 0, 1, "", 23, "je moeder"])
+    75
+    """
+    return int("".join(f"{bool(i):b}" for i in items), 2)
+
+
+def int_to_bits(i: int, *, zero_pad_to_length: int = 0) -> list[bool]:
+    """
+    Convert number -> boolean array.
+
+    >>> int_to_bits(75)
+    [True, False, False, True, False, True, True]
+    >>> int_to_bits(75, zero_pad_to_length=10)
+    [False, False, False, True, False, False, True, False, True, True]
+    """
+    s = f"{i:b}"
+    if zero_pad_to_length:
+        s = s.zfill(zero_pad_to_length)
+    return [c == "1" for c in s]
+
+
+def invert_dict[K, V](d: Mapping[K, V]) -> dict[V, K]:
+    return {v: k for k, v in d.items()}
+
+
+def grouped_by_key[K, V](pairs: Iterable[tuple[K, V]]) -> dict[K, list[V]]:
+    """
+    Group items by key(item).
+
+    >>> grouped_by_key(
+    ...     [
+    ...         ("Alice", 3),
+    ...         ("Bob", 6),
+    ...         ("Charles", 4),
+    ...         ("Alice", 8),
+    ...         ("Charles", 5),
+    ...         ("Bob", 2),
+    ...         ("Charles", 7),
+    ...         ("Alice", 9),
+    ...         ("Charles", 1),
+    ...     ]
+    ... )
+    {'Alice': [3, 8, 9], 'Bob': [6, 2], 'Charles': [4, 5, 7, 1]}
+    """
+    result: dict[K, list[V]] = {}
+    for k, v in pairs:
+        result.setdefault(k, []).append(v)
+    return result
+
+
+def filled_empty[T](rows: Iterable[Iterable[T]], value: T) -> Iterator[list[T]]:
+    rows_seq = [list(row) for row in rows]
+    max_width = max(len(row) for row in rows_seq)
+    for row in rows_seq:
+        yield [*row, *([value] * (max_width - len(row)))]
---
+++ packages/based-utils/based_utils-1.0.12-py3-none-any.whl/based_utils/keyboard.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+from time import perf_counter_ns
+from typing import TYPE_CHECKING
+
+from pynput import keyboard
+
+if TYPE_CHECKING:
+    from pynput.keyboard import Key, KeyCode
+
+
+def listen_to_keys(*keys: Key | KeyCode) -> dict[Key | KeyCode, list[int]]:
+    keys_pressed: dict[Key | KeyCode, list[int]] = defaultdict(list)
+    keys_pressed |= {key: [] for key in keys}
+    start = perf_counter_ns()
+
+    def on_release(k: Key | KeyCode | None) -> None:
+        if k and (not keys or k in keys):
+            keys_pressed[k].append(perf_counter_ns() - start)
+
+    keyboard.Listener(on_release=on_release, suppress=True).start()
+    return keys_pressed
