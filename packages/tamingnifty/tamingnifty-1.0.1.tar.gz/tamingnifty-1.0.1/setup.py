0b8cacad901b05bf3722a6b1e89a0f39516db4a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 03:35:56 +0000

    Add package tamingnifty/1.0.1/tamingnifty-1.0.1.tar.gz

---
+++ packages/tamingnifty/tamingnifty-1.0.1.tar.gz/tamingnifty-1.0.1/setup.py
@@ -0,0 +1,20 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="tamingnifty",  # your package name
+    version="1.0.1",
+    description="A library for converting OHLC to Noiseless Charts (Renko and PNF)",
+    author="Sugam Gupta",
+    packages=find_packages(),
+    install_requires=[
+        "pymongo",
+        "requests",
+        "pandas",
+        "slack_sdk",
+        "python-dotenv",
+        # add other dependencies here
+    ],
+    include_package_data=True,
+    python_requires=">=3.7",
+)
