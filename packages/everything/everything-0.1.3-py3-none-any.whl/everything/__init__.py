c18d630f04d77d10410f6e833b924c342725720f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 06:07:24 +0000

    Add package everything/0.1.3/everything-0.1.3-py3-none-any.whl

---
+++ packages/everything/everything-0.1.3-py3-none-any.whl/everything/__init__.py
@@ -0,0 +1,31 @@
+"""Top-level package for everything.py."""
+
+import os
+import logging
+from typing import Callable
+from dotenv import load_dotenv
+
+__author__ = """Wolf Mermelstein"""
+__email__ = "wolf@404wolf.com"
+
+
+load_dotenv()
+from everything.generator import (
+    runtime_generate_function,
+)  # noqa: E402 (need env var set to import)
+
+# Setup logger if env var set
+if os.getenv("EVERYTHING_DEBUG", "0") == "1":
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.DEBUG)
+    handler.setFormatter(
+        logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    )
+    logger.addHandler(handler)
+    logger.debug("Debug logging enabled for everything.py")
+
+
+def __getattr__(name: str) -> Callable:
+    return runtime_generate_function(name)
