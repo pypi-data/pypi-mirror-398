24d7d734b6b30f24eeedecbaf4a8cb77c5c40ce6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 04:24:21 +0000

    Add package ida-hcli/0.14.3/ida_hcli-0.14.3.tar.gz

---
+++ packages/ida-hcli/ida_hcli-0.14.3.tar.gz/ida_hcli-0.14.3/src/hcli/commands/ida/__init__.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+import rich_click as click
+
+
+@click.group()
+def ida() -> None:
+    """Manage IDA installations."""
+    pass
+
+
+from .install import install  # noqa: E402
+from .set_default import set_default_ida  # noqa: E402
+
+ida.add_command(install)
+ida.add_command(set_default_ida)
