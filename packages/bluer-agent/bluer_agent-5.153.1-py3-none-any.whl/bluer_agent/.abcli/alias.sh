f990a2a6a25ce4463570dc6333625eaac9f1c975
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 03:35:14 +0000

    Add package bluer-agent/5.153.1/bluer_agent-5.153.1-py3-none-any.whl

---
+++ packages/bluer-agent/bluer_agent-5.153.1-py3-none-any.whl/bluer_agent/.abcli/alias.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env bash
+
+alias @audio=bluer_agent_audio
+
+alias @chat=bluer_agent_chat
+
+alias @crawl=bluer_agent_crawl
+
+alias @rag=bluer_agent_rag
+
+alias @transcribe=bluer_agent_transcribe
+
+# ignore
+alias @agent=bluer_agent
+alias @ai_agent=bluer_agent
---
+++ packages/bluer-agent/bluer_agent-5.153.1-py3-none-any.whl/bluer_agent/.abcli/tests/help.sh
@@ -0,0 +1,48 @@
+#! /usr/bin/env bash
+
+function test_bluer_agent_help() {
+    local options=$1
+
+    local module
+    for module in \
+        "@agent" \
+        "@agent chat" \
+        \
+        "@agent chat validate" \
+        "@agent transcribe" \
+        \
+        "@agent pypi" \
+        "@agent pypi browse" \
+        "@agent pypi build" \
+        "@agent pypi install" \
+        \
+        "@agent pytest" \
+        \
+        "@agent test" \
+        "@agent test list" \
+        \
+        "@ai_agent" \
+        \
+        "@audio" \
+        "@audio install" \
+        "@audio play" \
+        "@audio record" \
+        \
+        "@crawl" \
+        "@crawl collect" \
+        "@crawl review" \
+        \
+        "@rag" \
+        "@rag build_corpus" \
+        "@rag query" \
+        \
+        "bluer_agent"; do
+        bluer_ai_eval ,$options \
+            bluer_ai_help $module
+        [[ $? -ne 0 ]] && return 1
+
+        bluer_ai_hr
+    done
+
+    return 0
+}
