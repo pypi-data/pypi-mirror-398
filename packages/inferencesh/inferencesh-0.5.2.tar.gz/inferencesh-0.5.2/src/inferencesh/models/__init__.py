37d1cbdfe984a9836a9d271fcd6deadf91be73db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 03:35:55 +0000

    Add package inferencesh/0.5.2/inferencesh-0.5.2.tar.gz

---
+++ packages/inferencesh/inferencesh-0.5.2.tar.gz/inferencesh-0.5.2/src/inferencesh/models/__init__.py
@@ -0,0 +1,61 @@
+"""Models package for inference.sh SDK."""
+
+from .base import BaseApp, BaseAppInput, BaseAppOutput
+from .file import File
+from .llm import (
+    ContextMessageRole,
+    Message,
+    ContextMessage,
+    LLMInput,
+    LLMOutput,
+    build_messages,
+    stream_generate,
+    timing_context,
+)
+from .output_meta import (
+    MetaItem,
+    MetaItemType,
+    TextMeta,
+    ImageMeta,
+    VideoMeta,
+    VideoResolution,
+    AudioMeta,
+    RawMeta,
+    OutputMeta,
+)
+from .errors import (
+    APIError,
+    RequirementsNotMetError,
+    RequirementError,
+    SetupAction,
+)
+
+__all__ = [
+    "BaseApp",
+    "BaseAppInput",
+    "BaseAppOutput",
+    "File",
+    "ContextMessageRole",
+    "Message",
+    "ContextMessage",
+    "LLMInput",
+    "LLMOutput",
+    "build_messages",
+    "stream_generate",
+    "timing_context",
+    # OutputMeta types
+    "MetaItem",
+    "MetaItemType",
+    "TextMeta",
+    "ImageMeta",
+    "VideoMeta",
+    "VideoResolution",
+    "AudioMeta",
+    "RawMeta",
+    "OutputMeta",
+    # Error types
+    "APIError",
+    "RequirementsNotMetError",
+    "RequirementError",
+    "SetupAction",
+] 
