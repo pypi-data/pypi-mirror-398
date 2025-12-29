eec7f83832b99fd2fec47034261abbaafbcb2935
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 03:36:00 +0000

    Add package NiceGui-UGForm/1.0.1/nicegui_ugform-1.0.1.tar.gz

---
+++ packages/NiceGui-UGForm/nicegui_ugform-1.0.1.tar.gz/nicegui_ugform-1.0.1/nicegui_ugform/i18n/keys.py
@@ -0,0 +1,56 @@
+"""Translation keys."""
+
+from typing import NamedTuple
+
+
+class TranslationMap(NamedTuple):
+    # Button texts
+    submit: str
+    reset: str
+    addField: str
+    deleteField: str
+    complete: str
+    exportJson: str
+    exportBase64: str
+
+    # Field type texts
+    text: str
+    integer: str
+    float: str
+    boolean: str
+
+    # Property label texts
+    label: str
+    description: str
+    required: str
+    defaultValue: str
+    fieldName: str
+    fieldType: str
+
+    # Section headers
+    formEditor: str
+    formInformation: str
+    formDescription: str
+    formFields: str
+    formTitle: str
+    formLocale: str
+    uuid: str
+
+    # Field-specific properties
+    minLength: str
+    maxLength: str
+    regexPattern: str
+    minValue: str
+    maxValue: str
+
+    # Template texts
+    newFieldTemplate: str
+
+    # Notification messages
+    jsonSchemaCopied: str
+    base64SchemaCopied: str
+    formSubmittedSuccessfully: str
+    pleaseFixValidationErrors: str
+    formReset: str
+    fieldRequiredTemplate: str
+    fieldInvalidValueTemplate: str
