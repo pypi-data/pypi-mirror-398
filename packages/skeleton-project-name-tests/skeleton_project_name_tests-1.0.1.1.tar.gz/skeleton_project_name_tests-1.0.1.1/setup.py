ca79682ba8b9d4294036ce29fd250030a2c01103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 08:06:16 +0000

    Add package skeleton-project-name-tests/1.0.1.1/skeleton_project_name_tests-1.0.1.1.tar.gz

---
+++ packages/skeleton-project-name-tests/skeleton_project_name_tests-1.0.1.1.tar.gz/skeleton_project_name_tests-1.0.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import setuptools  # 导入setuptools打包工具
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="skeleton_project_name_tests",  # 用自己的名替换其中的YOUR_USERNAME_
+    version="1.0.1.1",  # 包版本号，便于维护版本,保证每次发布都是版本都是唯一的
+    author="zhangxianggang73",  # 作者，可以写自己的姓名
+    author_email="zhangxianggang73@sina.com",  # 作者联系方式，可写自己的邮箱地址
+    description="A sample My Project",  # 包的简述
+    long_description=long_description,  # 包的详细介绍，一般在README.md文件内
+    long_description_content_type="text/markdown",
+    url="",  # 自己项目地址，比如github的项目地址
+    packages=setuptools.find_packages(),
+    scripts= [],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',  # 对python的最低版本要求
+    install_requires=['nose']
+)
