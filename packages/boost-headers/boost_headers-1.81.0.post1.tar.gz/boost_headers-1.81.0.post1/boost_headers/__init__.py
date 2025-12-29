e46a6432d2bd2c40629763d0f473fb8664762a2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 03:35:50 +0000

    Add package boost-headers/1.81.0.post1/boost_headers-1.81.0.post1.tar.gz

---
+++ packages/boost-headers/boost_headers-1.81.0.post1.tar.gz/boost_headers-1.81.0.post1/boost_headers/__init__.py
@@ -0,0 +1,12 @@
+"""boost-headers headers."""
+
+from __future__ import annotations
+
+from pathlib import Path
+
+__version__ = "1.81.0.post1"
+
+
+def get_include() -> Path:
+    """Return the path to the boost-headers headers."""
+    return Path(__file__).parent / "include"
