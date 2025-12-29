ba6c97b281a316611ada18c92b4fc7263beecda8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 04:24:14 +0000

    Add package llmling-models/1.4.6/llmling_models-1.4.6-py3-none-any.whl

---
+++ packages/llmling-models/llmling_models-1.4.6-py3-none-any.whl/llmling_models/configs/__init__.py
@@ -0,0 +1,37 @@
+"""Configs."""
+
+from .model_configs import (
+    AnyModelConfig,
+    AugmentedModelConfig,
+    ClaudeCodeModelConfig,
+    DelegationModelConfig,
+    FallbackModelConfig,
+    FunctionModelConfig,
+    ImportModelConfig,
+    InputModelConfig,
+    PrePostPromptConfig,
+    RemoteInputConfig,
+    RemoteProxyConfig,
+    StringModelConfig,
+    TestModelConfig,
+    UserSelectModelConfig,
+    ModelSettings,
+)
+
+__all__ = [
+    "AnyModelConfig",
+    "AugmentedModelConfig",
+    "ClaudeCodeModelConfig",
+    "DelegationModelConfig",
+    "FallbackModelConfig",
+    "FunctionModelConfig",
+    "ImportModelConfig",
+    "InputModelConfig",
+    "ModelSettings",
+    "PrePostPromptConfig",
+    "RemoteInputConfig",
+    "RemoteProxyConfig",
+    "StringModelConfig",
+    "TestModelConfig",
+    "UserSelectModelConfig",
+]
