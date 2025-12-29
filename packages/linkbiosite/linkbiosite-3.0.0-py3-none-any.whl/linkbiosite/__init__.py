ef107834a9dd10946925e53455c53b70086a41b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 06:07:58 +0000

    Add package linkbiosite/3.0.0/linkbiosite-3.0.0-py3-none-any.whl

---
+++ packages/linkbiosite/linkbiosite-3.0.0-py3-none-any.whl/linkbiosite/__init__.py
@@ -0,0 +1,29 @@
+"""
+LinkBioSite - Static Site Generator for Link Bio Pages
+
+A modern, brutalist-designed static site generator for creating beautiful link bio pages.
+Built with Python and Jinja2 templates.
+
+Example:
+    >>> from linkbiosite import build
+    >>> build()  # Build site in current directory
+    'public'
+"""
+
+from .core import LinkBioSiteGenerator, build, init, serve
+from .data import load_data, validate_data, save_data
+
+__version__ = "1.0.0"
+__author__ = "Rafnix Guzmán"
+__email__ = "rafnixg@gmail.com"
+
+__all__ = [
+    "LinkBioSiteGenerator",
+    "build",
+    "init",
+    "serve",
+    "load_data",
+    "validate_data",
+    "save_data",
+    "cli_main",
+]
