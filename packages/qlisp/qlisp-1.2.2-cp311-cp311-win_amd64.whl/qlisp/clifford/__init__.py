85ab4b9583c05a68ddd844687aa3289f9be64f61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 04:23:59 +0000

    Add package qlisp/1.2.2/qlisp-1.2.2-cp311-cp311-win_amd64.whl

---
+++ packages/qlisp/qlisp-1.2.2-cp311-cp311-win_amd64.whl/qlisp/clifford/__init__.py
@@ -0,0 +1,4 @@
+from .group import (CliffordGroup, Cycles, find_permutation,
+                    find_permutation_for_Unitary)
+from .utils import (one_qubit_clifford_mul_table, one_qubit_clifford_seq,
+                    one_qubit_clifford_seq2)
