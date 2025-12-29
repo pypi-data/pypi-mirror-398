3f1e93d00171663bb3c5fd6e3232e708f96d0a7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 03:36:01 +0000

    Add package gumiyabot/1.1.1/gumiyabot-1.1.1.tar.gz

---
+++ packages/gumiyabot/gumiyabot-1.1.1.tar.gz/gumiyabot-1.1.1/src/gumiyabot/utils.py
@@ -0,0 +1,34 @@
+import aiohttp
+from typing import Any
+
+
+class TillerinoApi:
+    API_BASE = "https://api.tillerino.org"
+    BEATMAP_INFO = "/".join((API_BASE, "beatmapinfo"))
+    BOT_INFO = "/".join((API_BASE, "botinfo"))
+    USER_BY_ID = "/".join((API_BASE, "userbyid"))
+
+    def __init__(self, key):
+        self.key = key
+        self.session = None
+
+    async def _get(self, url: str, params={}):
+        if not self.session:
+            self.session = aiohttp.ClientSession()
+
+        params.update({"k": self.key})
+        async with self.session.get(url, params=params) as r:
+            if r.status == 200:
+                return await r.json()
+            else:
+                return None
+
+    async def beatmapinfo(
+        self, beatmap_id: int, mods: int = 0, wait: int | None = None
+    ) -> dict[str, Any]:
+        """Get beatmapinfo from Tillerino API"""
+        params = {"beatmapid": beatmap_id, "mods": mods}
+        if wait:
+            params["wait"] = wait
+        resp = await self._get(self.BEATMAP_INFO, params=params)
+        return resp
