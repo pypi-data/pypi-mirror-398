d00726e7c0c746b3b190fc45da8e4c18e11e3adc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 05:12:31 +0000

    Add package wheke-sqlite/0.0.1/wheke_sqlite-0.0.1.tar.gz

---
+++ packages/wheke-sqlite/wheke_sqlite-0.0.1.tar.gz/wheke_sqlite-0.0.1/src/wheke_sqlite/_service.py
@@ -0,0 +1,31 @@
+from sqlalchemy import Engine
+from sqlmodel import Session, SQLModel, create_engine
+from svcs import Container
+from wheke import WhekeSettings, get_service, get_settings
+
+from ._settings import DatabaseSettings
+
+
+class DatabaseService:
+    settings: DatabaseSettings
+
+    engine: Engine
+
+    def __init__(self, *, database_settings: DatabaseSettings) -> None:
+        self.engine = create_engine(database_settings.connection_string)
+
+    @property
+    def session(self) -> Session:
+        return Session(self.engine)
+
+    def create_db(self) -> None:
+        SQLModel.metadata.create_all(self.engine)
+
+
+def database_service_factory(container: Container) -> DatabaseService:
+    settings = get_settings(container, WhekeSettings).get_feature(DatabaseSettings)
+    return DatabaseService(database_settings=settings)
+
+
+def get_database_service(container: Container) -> DatabaseService:
+    return get_service(container, DatabaseService)
