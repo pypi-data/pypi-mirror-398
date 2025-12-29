bff8a396006e6b3e602e66295310e0e7ddb0c9a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 07:09:54 +0000

    Add package orbit-torch/0.0.3/orbit_torch-0.0.3.tar.gz

---
+++ packages/orbit-torch/orbit_torch-0.0.3.tar.gz/orbit_torch-0.0.3/orbit/plugin/__init__.py
@@ -0,0 +1,9 @@
+try: from orbit.plugin.classification import ClassificationReport
+except: pass
+
+from orbit.plugin.warmup import Warmup
+from orbit.plugin.early_stopping import EarlyStopping
+from orbit.plugin.gradient_accumulation import GradientAccumulation
+from orbit.plugin.mentor import Mentor
+from orbit.plugin.ema import EMA # Not tested
+from orbit.plugin.memory_estimator import MemoryEstimator
