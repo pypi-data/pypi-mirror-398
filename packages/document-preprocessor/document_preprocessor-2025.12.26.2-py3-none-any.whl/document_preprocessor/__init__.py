8c9671b7cb2ba0a0b1d643dc92fd99f25f1e218b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-29 05:12:23 +0000

    Add package document-preprocessor/2025.12.26.2/document_preprocessor-2025.12.26.2-py3-none-any.whl

---
+++ packages/document-preprocessor/document_preprocessor-2025.12.26.2-py3-none-any.whl/document_preprocessor/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "2025.12.26.2"
+
+from .core import PreprocessorConfig, DocumentPreprocessor, DocumentProcessingError
+
+__all__ = ["PreprocessorConfig", "DocumentPreprocessor", "DocumentProcessingError"]
