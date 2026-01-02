76435093d3e978a8d8ef9ed57a1e8561007fa024
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 15:06:44 +0000

    Add package mbp/1.7.4/mbp-1.7.4-py3-none-any.whl

---
+++ packages/mbp/mbp-1.7.4-py3-none-any.whl/mbp/llm.py
@@ -0,0 +1,47 @@
+import json
+import functools
+from .core import *
+
+__all__ = ["update_messages", "build_messages", "build_icl_inputs", "load_icl_inputs"]
+
+
+def encode_message(context):
+    return json.dumps(context, ensure_ascii=False, separators=(",", ":"))
+
+
+FORMAT_HINT = "Expecting format of => instruction: str, format: dict, examples: [dict]"
+
+
+def build_icl_inputs(instruction, format={}, examples=[]):
+    assert isinstance(instruction, str) and isinstance(format, dict) and len(format) > 0 and isinstance(examples, list), FORMAT_HINT
+    format = {k: {"type": v} if isinstance(v, str) else v for k, v in format.items()}
+    res = [instruction]
+    if examples:
+        res.append("Example:" if len(examples) == 1 else "Examples:")
+        inp, out = {}, {}
+        for e in examples:
+            for k, v in e.items():
+                if k in format:
+                    out[k] = v
+                else:
+                    inp[k] = v
+            res.append(f"user: {encode_message(inp)}\nassistant: {encode_message(out)}")
+    return "\n\n".join(res), format
+
+
+@functools.lru_cache(maxsize=None)
+def load_icl_inputs(path):
+    return build_icl_inputs(**load_yaml(path))
+
+
+def update_messages(messages, role, content):
+    if isinstance(content, dict):
+        content = encode_message(content)
+    messages.append({"role": role, "content": content})
+
+
+def build_messages(system_message, context):
+    res = []
+    update_messages(res, "system", system_message)
+    update_messages(res, "user", context)
+    return res
