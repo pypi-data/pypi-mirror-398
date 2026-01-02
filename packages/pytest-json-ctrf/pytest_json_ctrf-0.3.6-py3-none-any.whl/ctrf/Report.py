aa9a1d7594a164d95cde0266151f425830ce5215
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 17:07:02 +0000

    Add package pytest-json-ctrf/0.3.6/pytest_json_ctrf-0.3.6-py3-none-any.whl

---
+++ packages/pytest-json-ctrf/pytest_json_ctrf-0.3.6-py3-none-any.whl/ctrf/Report.py
@@ -0,0 +1,85 @@
+import os
+import pytest
+import time
+import json
+from typing import Optional
+from pytest import TestReport
+from collections import OrderedDict
+from .TestObject import TestObject, TestStatus
+
+
+class Report:
+    def __init__(self):
+        self.test_items = OrderedDict()
+        self.prepared_tests = dict()
+        self.start_time = None
+        self.stop_time = None
+
+    def start(self) -> None:
+        self.start_time = time.time()
+
+    def stop(self) -> None:
+        self.stop_time = time.time()
+
+    @staticmethod
+    def _get_tool() -> dict:
+        return {
+            "name": "pytest",
+            "version": str(pytest.__version__)
+        }
+
+    def list_tests_by_status(self, status: TestStatus) -> list:
+        return [test for test in self.prepared_tests.values() if test.status == status]
+
+    def _get_summary(self) -> dict:
+        return {
+            'tests': len(self.prepared_tests),
+            'passed': len(self.list_tests_by_status(TestStatus.PASSED)),
+            'failed': len(self.list_tests_by_status(TestStatus.FAILED)),
+            'skipped': len(self.list_tests_by_status(TestStatus.SKIPPED)),
+            'pending': len(self.list_tests_by_status(TestStatus.PENDING)),
+            'other': 0,
+            'start': self.start_time,
+            'stop': self.stop_time
+        }
+
+    def collect(self, report: TestReport) -> None:
+        if report.nodeid not in self.test_items.keys():
+            worker_id = getattr(report, 'worker_id', None)
+            test = TestObject(report, worker_id)
+        else:
+            test = self.test_items.get(report.nodeid)  # type: ignore
+        test.update(report)
+        self.test_items[report.nodeid] = test
+
+    def process_retries(self) -> None:
+        for test_id, test_data in self.test_items.items():
+            name = test_id.split('[')[0]
+            test: Optional[TestObject] = self.prepared_tests.get(name)
+            if test:
+                test.status = test_data.status
+                test.raw_status = test_data.raw_status
+                if test_data.status != TestStatus.PASSED:
+                    test.status = test_data.status
+                    test.raw_status = test_data.raw_status
+                    test.message = test_data.message
+                    test.trace = test_data.trace
+                test.retries += 1
+            else:
+                self.prepared_tests[name] = test_data
+
+    def get_report(self) -> dict:
+        self.process_retries()
+        return {'results': {
+            "tool": self._get_tool(),
+            "summary": self._get_summary(),
+            "tests": [test.serialize() for test in self.prepared_tests.values()]
+        }
+        }
+
+    def save(self, report_file: str) -> None:
+        dirname = os.path.dirname(report_file)
+        if dirname:
+            os.makedirs(dirname, exist_ok=True)
+        with open(report_file, 'w') as file:
+            json.dump(self.get_report(), file, default=str, indent=4)
---
+++ packages/pytest-json-ctrf/pytest_json_ctrf-0.3.6-py3-none-any.whl/ctrf/main.py
@@ -0,0 +1,52 @@
+from pytest import Config, Parser, FixtureRequest, fixture
+from ctrf.CommonTestReportPlugin import CTRF
+from ctrf.BaseMetadataReport import BaseMetadataReport
+
+
+def pytest_addoption(parser: Parser):
+    group = parser.getgroup('ctrf', 'generate test report in CTR format')
+    group.addoption('--ctrf',
+                    action='store',
+                    help='generate test report. Report file name is optional')
+
+
+def pytest_configure(config: Config):
+    if not config.option.ctrf:
+        return
+    if hasattr(config, 'workerinput'):
+        plugin = BaseMetadataReport()
+    else:
+        plugin = CTRF()
+    setattr(config, '_ctrf', plugin)
+    config.pluginmanager.register(plugin, name='ctrf_plugin')
+    pass
+
+
+def pytest_unconfigure(config: Config):
+    ctrf = getattr(config, '_ctrf', None)
+    if ctrf is not None:
+        delattr(config, '_ctrf')
+    if config.pluginmanager.hasplugin('ctrf_plugin'):
+        config.pluginmanager.unregister(name='ctrf_plugin')
+
+
+@fixture(autouse=True)
+def ctrf_json_metadata(request: FixtureRequest):
+    if not request.config.option.ctrf:
+        return
+    tags = list()
+    for mark in request.node.iter_markers():
+        tag = mark.name
+        if mark.args:
+            for arg in mark.args:
+                tag += f'::{arg}'
+        if mark.kwargs:
+            for key, value in mark.kwargs.items():
+                tag += f'::{key}_{value}'
+        tags.append(tag)
+    request.node._ctrf_metadata.setdefault('tags', tags)
+    if hasattr(request.node, 'callspec'):
+        browser = request.node.callspec.params.get('browser_name')
+        if browser:
+            request.node._ctrf_metadata.setdefault('browser', browser)
+    return request.node._ctrf_metadata
