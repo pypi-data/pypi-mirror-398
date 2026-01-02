f2369abaf1993ec96c1024d03cdbd0f851289cd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 14:06:49 +0000

    Add package dominus-sdk-python-dev/2.1.6/dominus_sdk_python_dev-2.1.6.tar.gz

---
+++ packages/dominus-sdk-python-dev/dominus_sdk_python_dev-2.1.6.tar.gz/dominus_sdk_python_dev-2.1.6/dominus/config/__init__.py
@@ -0,0 +1,24 @@
+"""SDK Configuration"""
+from .endpoints import (
+    SOVEREIGN_URL,
+    ARCHITECT_URL,
+    ORCHESTRATOR_URL,
+    WARDEN_URL,
+    get_service_url,
+    get_architect_url,
+    get_sovereign_url,
+    PROJECT_NUMBER,
+    REGION,
+)
+
+__all__ = [
+    "SOVEREIGN_URL",
+    "ARCHITECT_URL",
+    "ORCHESTRATOR_URL",
+    "WARDEN_URL",
+    "get_service_url",
+    "get_architect_url",
+    "get_sovereign_url",
+    "PROJECT_NUMBER",
+    "REGION",
+]
