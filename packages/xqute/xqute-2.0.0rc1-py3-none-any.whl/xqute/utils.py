c1357e24c6a3c1834678d93b077036afab331e4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 16:06:48 +0000

    Add package xqute/2.0.0rc1/xqute-2.0.0rc1-py3-none-any.whl

---
+++ packages/xqute/xqute-2.0.0rc1-py3-none-any.whl/xqute/utils.py
@@ -0,0 +1,34 @@
+"""Utilities for xqute"""
+
+from __future__ import annotations
+
+import os
+import logging
+from typing import Union, Tuple, List
+
+from rich.logging import RichHandler
+
+from .defaults import LOGGER_NAME, LOGGER_LEVEL
+
+CommandType = Union[str, Tuple[str], List[str]]
+
+
+class DuplicateFilter(logging.Filter):
+    def __init__(self):
+        super().__init__()
+        self.prev_msg = None
+
+    def filter(self, record):
+        message = record.getMessage()
+        if message == self.prev_msg:
+            return False
+        self.prev_msg = message
+        return True
+
+
+logger = logging.getLogger(LOGGER_NAME)
+logger.addHandler(RichHandler(show_path=False, omit_repeated_times=False))
+logger.addFilter(DuplicateFilter())
+
+loglevel = os.getenv("XQUTE_LOG_LEVEL", LOGGER_LEVEL).upper()
+logger.setLevel(loglevel)
