da3eb3066012ca4c0ac226385cce6890be09498c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 15:06:41 +0000

    Add package wrenfold/0.4.1/wrenfold-0.4.1-cp312-cp312-macosx_11_0_arm64.whl

---
+++ packages/wrenfold/wrenfold-0.4.1-cp312-cp312-macosx_11_0_arm64.whl/wrenfold/__init__.py
@@ -0,0 +1,7 @@
+"""Symbolic code-generation tools."""
+
+from pywrenfold import __git_version__, __version__  # noqa: F401
+
+__author__ = "Gareth <gcross.code@icloud.com>"
+__copyright__ = "Copyright (C) 2024 Gareth Cross"
+__license__ = "MIT"
