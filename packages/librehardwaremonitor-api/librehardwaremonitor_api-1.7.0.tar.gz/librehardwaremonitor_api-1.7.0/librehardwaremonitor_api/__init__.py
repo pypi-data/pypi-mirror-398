d2c8f866d5398a072b3319694f3e87af293a5e75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 16:07:37 +0000

    Add package librehardwaremonitor-api/1.7.0/librehardwaremonitor_api-1.7.0.tar.gz

---
+++ packages/librehardwaremonitor-api/librehardwaremonitor_api-1.7.0.tar.gz/librehardwaremonitor_api-1.7.0/librehardwaremonitor_api/__init__.py
@@ -0,0 +1,5 @@
+"""LibreHardwareMonitor API library"""
+
+from librehardwaremonitor_api.client import LibreHardwareMonitorClient
+from librehardwaremonitor_api.errors import LibreHardwareMonitorConnectionError
+from librehardwaremonitor_api.errors import LibreHardwareMonitorNoDevicesError
