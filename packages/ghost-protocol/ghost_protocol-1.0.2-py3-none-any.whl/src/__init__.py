7fdf2932432641cf2e2fefed19308ec6aff1d4fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 14:06:49 +0000

    Add package ghost-protocol/1.0.2/ghost_protocol-1.0.2-py3-none-any.whl

---
+++ packages/ghost-protocol/ghost_protocol-1.0.2-py3-none-any.whl/src/__init__.py
@@ -0,0 +1,8 @@
+"""
+Ghost Protocol
+The automated guardian of your codebase.
+"""
+
+__version__ = "1.0.2"
+__author__ = "Lazy Vibe Coder"
+__all__ = ["Config", "IgnoreFileManager", "ProjectScanner"]
