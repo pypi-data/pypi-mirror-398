f999647d4a7b4a9cc896aa8cd0584640faf02842
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 17:07:08 +0000

    Add package minecraftbotsapi/1.4/minecraftbotsapi-1.4.tar.gz

---
+++ packages/minecraftbotsapi/minecraftbotsapi-1.4.tar.gz/minecraftbotsapi-1.4/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup, find_packages
+
+def get_libraries():
+    return open("D:/Florest/Programming/Python/minecraftbotsapi/libraries.txt", 'r').readlines()
+
+setup(name='minecraftbotsapi', version='1.4', description='Функции для создания своего бота в Minecraft!', long_description='Документацию Вы можете найти на Github! Для использования установите mineflayer, среду NodeJS.', author='florestdev', author_email='florestone4185@internet.ru', packages=find_packages(), python_requires='>=3.10', project_urls={"Social Resources": 'https://taplink.cc/florestone4185', 'GitHub of Project':"https://github.com/florestdev/minecraftbotsapi"}, install_requires=get_libraries())
