562025fa55e5b71e3c1ef809f068182f18c0eabf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 17:06:47 +0000

    Add package baypy/1.5.0/baypy-1.5.0-py3-none-any.whl

---
+++ packages/baypy/baypy-1.5.0-py3-none-any.whl/baypy/__init__.py
@@ -0,0 +1,12 @@
+__all__ = [
+    "analysis",
+    "diagnostics",
+    "model",
+    "regression"
+]
+
+
+from baypy import model
+from baypy import regression
+from baypy import diagnostics
+from baypy import analysis
