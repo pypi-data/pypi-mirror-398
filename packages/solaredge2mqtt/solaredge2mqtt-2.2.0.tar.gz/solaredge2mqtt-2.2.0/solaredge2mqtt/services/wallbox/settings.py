ccc26a8b4a1cd8f813aef5af95ff43dcfa0ad024
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 15:06:45 +0000

    Add package solaredge2mqtt/2.2.0/solaredge2mqtt-2.2.0.tar.gz

---
+++ packages/solaredge2mqtt/solaredge2mqtt-2.2.0.tar.gz/solaredge2mqtt-2.2.0/solaredge2mqtt/services/wallbox/settings.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel, Field, SecretStr
+
+
+class WallboxSettings(BaseModel):
+    host: str = Field(None)
+    password: SecretStr = Field(None)
+    serial: str = Field(None)
+    retain: bool = Field(False)
+
+    @property
+    def is_configured(self) -> bool:
+        return all(
+            [self.host is not None, self.password is not None, self.serial is not None]
+        )
