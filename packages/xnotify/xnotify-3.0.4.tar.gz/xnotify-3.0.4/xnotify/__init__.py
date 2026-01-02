6afdcdb4cf6b0697671557957e2d751e7bbebb09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 16:07:40 +0000

    Add package xnotify/3.0.4/xnotify-3.0.4.tar.gz

---
+++ packages/xnotify/xnotify-3.0.4.tar.gz/xnotify-3.0.4/xnotify/__init__.py
@@ -0,0 +1,40 @@
+import logging
+
+from .__version__ import (
+    __version__,
+    __author__,
+    __email__,
+    __license__,
+    __url__,
+    __description__,
+)
+
+from .core import XNotify
+from .config import Config
+from .plugins import (
+    NotificationPlugin,
+    list_plugins,
+    get_plugin,
+)
+
+notify = XNotify
+
+# Setup default logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+__all__ = [
+    '__version__',
+    '__author__',
+    '__email__',
+    '__license__',
+    '__url__',
+    '__description__',
+    'XNotify',
+    'Config',
+    'NotificationPlugin',
+    'list_plugins',
+    'get_plugin',
+]
