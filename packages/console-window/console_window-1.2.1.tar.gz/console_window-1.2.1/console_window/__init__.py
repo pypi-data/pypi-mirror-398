b2119466ef4b3594f8533214296d1ca1a9ab6194
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 15:06:59 +0000

    Add package console-window/1.2.1/console_window-1.2.1.tar.gz

---
+++ packages/console-window/console_window-1.2.1.tar.gz/console_window-1.2.1/console_window/__init__.py
@@ -0,0 +1,3 @@
+from .ConsoleWindow import ConsoleWindow, OptionSpinner, ConsoleWindowOpts
+from .ConsoleWindow import Context, IncrementalSearchBar, Screen, ScreenStack
+from .ConsoleWindow import BasicHelpScreen
