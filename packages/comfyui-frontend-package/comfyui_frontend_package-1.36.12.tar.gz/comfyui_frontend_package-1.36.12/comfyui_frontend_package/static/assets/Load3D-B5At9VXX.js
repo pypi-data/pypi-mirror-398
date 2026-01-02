d538f04d81f9a2e74d678497a51a2a89b2416f8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 15:06:53 +0000

    Add package comfyui-frontend-package/1.36.12/comfyui_frontend_package-1.36.12.tar.gz

---
+++ packages/comfyui-frontend-package/comfyui_frontend_package-1.36.12.tar.gz/comfyui_frontend_package-1.36.12/comfyui_frontend_package/static/assets/Load3D-B5At9VXX.js
@@ -0,0 +1,2 @@
+import{_ as o}from"./Load3D.vue_vue_type_script_setup_true_lang-B4L9yzzJ.js";import"./vendor-other-CSCK_VYO.js";import"./vendor-primevue-De0k_7Rd.js";import"./index-DxFSMtyW.js";import"./vendor-vue-C_r2VRgZ.js";import"./vendor-xterm-CWYFmgbN.js";import"./vendor-three-BsnVSA6y.js";import"./vendor-tiptap-B2vFozDB.js";export{o as default};
+//# sourceMappingURL=Load3D-B5At9VXX.js.map
---
+++ packages/comfyui-frontend-package/comfyui_frontend_package-1.36.12.tar.gz/comfyui_frontend_package-1.36.12/comfyui_frontend_package/static/assets/WidgetLegacy-DV93aOvn.js
@@ -0,0 +1,2 @@
+import{aQ as o}from"./index-DxFSMtyW.js";import"./vendor-primevue-De0k_7Rd.js";import"./vendor-other-CSCK_VYO.js";import"./vendor-vue-C_r2VRgZ.js";import"./vendor-xterm-CWYFmgbN.js";import"./vendor-three-BsnVSA6y.js";import"./vendor-tiptap-B2vFozDB.js";export{o as default};
+//# sourceMappingURL=WidgetLegacy-DV93aOvn.js.map
