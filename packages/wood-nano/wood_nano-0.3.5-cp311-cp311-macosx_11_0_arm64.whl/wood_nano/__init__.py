db73e1c737d11eb5623d1433ed4a0d3d1b404d87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 17:06:24 +0000

    Add package wood-nano/0.3.5/wood_nano-0.3.5-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/wood-nano/wood_nano-0.3.5-cp311-cp311-macosx_11_0_arm64.whl/wood_nano/__init__.py
@@ -0,0 +1,29 @@
+__version__ = "0.3.5"
+
+from .wood_nano_ext import add
+from .wood_nano_ext import version
+from .wood_nano_ext import inspect
+from .wood_nano_ext import int1, int2, int3, int4
+from .wood_nano_ext import double1, double2, double3, double4
+from .wood_nano_ext import bool1
+from .wood_nano_ext import point, point1, point2, point3, point4
+from .wood_nano_ext import vector, vector1, vector2, vector3, vector4
+from .wood_nano_ext import middle_point
+from .wood_nano_ext import rtree
+from .wood_nano_ext import get_connection_zones
+from .wood_nano_ext import cut_type, cut_type1, cut_type2
+from .wood_nano_ext import test
+from .wood_nano_ext import read_xml_polylines
+from .wood_nano_ext import read_xml_polylines_and_properties
+from .wood_nano_ext import closed_mesh_from_polylines
+from .wood_nano_ext import joints
+from .wood_nano_ext import joint_parameters_and_types
+from .wood_nano_ext import mesh_boolean_difference_from_polylines
+from .wood_nano_ext import GLOBALS
+from .wood_nano_ext import beam_volumes
+from .wood_nano_ext import mesh_skeleton
+from .wood_nano_ext import beam_skeleton
+
+
+
+
