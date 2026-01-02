39812fcaabddddbed40b9d82ed1fa76f182eaa2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 15:07:22 +0000

    Add package VideoTimestamps/1.1.0/videotimestamps-1.1.0-cp310-cp310-win_arm64.whl

---
+++ packages/VideoTimestamps/videotimestamps-1.1.0-cp310-cp310-win_arm64.whl/video_timestamps/video_provider/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ["ABCVideoProvider", "BestSourceVideoProvider", "FFMS2VideoProvider"]
+
+from .abc_video_provider import ABCVideoProvider
+from .best_source_video_provider import BestSourceVideoProvider
+from .ffms2_video_provider import FFMS2VideoProvider
