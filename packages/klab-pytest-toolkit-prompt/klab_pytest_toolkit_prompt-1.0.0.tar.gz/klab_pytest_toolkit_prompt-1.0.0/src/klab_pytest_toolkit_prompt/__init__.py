f82ec864e817be38cc2ed538fa0bfef6e3002649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 16:06:46 +0000

    Add package klab-pytest-toolkit-prompt/1.0.0/klab_pytest_toolkit_prompt-1.0.0.tar.gz

---
+++ packages/klab-pytest-toolkit-prompt/klab_pytest_toolkit_prompt-1.0.0.tar.gz/klab_pytest_toolkit_prompt-1.0.0/src/klab_pytest_toolkit_prompt/__init__.py
@@ -0,0 +1,10 @@
+"""Klab Pytest Toolkit - Prompt"""
+
+from klab_pytest_toolkit_prompt.core import PromptInterface, PromptFactory
+
+__version__ = "1.0.0"
+
+__all__ = [
+    "PromptInterface",
+    "PromptFactory",
+]
