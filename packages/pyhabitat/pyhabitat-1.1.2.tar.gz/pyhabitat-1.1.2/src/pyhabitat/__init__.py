aebcd943b5ba020664d68b9f985e82c661eec7f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 14:07:08 +0000

    Add package pyhabitat/1.1.2/pyhabitat-1.1.2.tar.gz

---
+++ packages/pyhabitat/pyhabitat-1.1.2.tar.gz/pyhabitat-1.1.2/src/pyhabitat/__init__.py
@@ -0,0 +1,81 @@
+# src/pyhabitat/__init__.py
+from .version_info import get_package_version as version
+from .system_info import SystemInfo
+from .reporting import report
+from .environment import (
+    matplotlib_is_available_for_gui_plotting,
+    matplotlib_is_available_for_headless_image_export,
+    tkinter_is_available,
+    in_repl,
+    on_freebsd,
+    on_linux,
+    on_android,
+    on_windows,
+    on_wsl,
+    on_apple,
+    on_termux,
+    on_pydroid,
+    on_ish_alpine,
+    as_pyinstaller,
+    as_frozen,
+    is_elf,
+    is_pyz,
+    is_windows_portable_executable,
+    is_msix,
+    is_macos_executable,
+    is_pipx,
+    is_python_script,
+    interactive_terminal_is_available,
+    web_browser_is_available,
+    edit_textfile,
+    interp_path,
+    main,
+    user_darrin_deyoung,
+    can_spawn_shell,
+    is_ascii,
+    is_binary,
+    is_running_in_uvicorn
+)
+
+# Optional: Set __all__ for explicit documentation and cleaner imports
+__all__ = [
+    'matplotlib_is_available_for_gui_plotting',
+    'matplotlib_is_available_for_headless_image_export',
+    'tkinter_is_available',
+    'in_repl',
+    'on_termux',
+    'on_pydroid',
+    'on_wsl',
+    'on_freebsd',
+    'on_linux',
+    'on_android',
+    'on_windows',
+    'on_apple',
+    'on_ish_alpine',
+    'as_pyinstaller',
+    'as_frozen',
+    'is_elf',
+    'is_pyz',
+    'is_windows_portable_executable',
+    'is_msix',
+    'is_macos_executable',
+    'is_pipx',
+    'is_python_script',
+    'interactive_terminal_is_available',
+    'is_running_in_uvicorn',
+    'web_browser_is_available',
+    'edit_textfile',
+    'interp_path',
+    'user_darrin_deyoung',
+    'can_spawn_shell',
+    'is_ascii',
+    'is_binary',
+    'main',
+    'report',
+    'is_running_in_uvicorn',
+    'version',
+    'SystemInfo'
+    
+]
+
+__version__ = version()
