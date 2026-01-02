78d6622bca0d6363553e31b96f0804c583c6afe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 16:07:14 +0000

    Add package kcjoshi-distributions/0.1/kcjoshi_distributions-0.1.tar.gz

---
+++ packages/kcjoshi-distributions/kcjoshi_distributions-0.1.tar.gz/kcjoshi_distributions-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="kcjoshi_distributions",
+    version="0.1",
+    description="Gaussian distributions",
+    packages=["kcjoshi_distributions"],
+    zip_safe=False,
+    author="Keyur Joshi",
+    author_email="keyur79syd@gmail.com"
+)
