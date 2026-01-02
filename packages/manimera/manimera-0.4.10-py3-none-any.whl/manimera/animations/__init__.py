4f14e80d2c021e546a21f7663b260bc72bea27cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 17:06:12 +0000

    Add package manimera/0.4.10/manimera-0.4.10-py3-none-any.whl

---
+++ packages/manimera/manimera-0.4.10-py3-none-any.whl/manimera/animations/__init__.py
@@ -0,0 +1,18 @@
+"""
+Manimera Animations Package.
+
+This package provides custom Animation classes for creating complex
+visual effects and transitions.
+"""
+
+from .advance_clock_time import AdvanceClockTime
+from .pendulum_oscillation import PendulumOscillation
+from .lamp_glow import LampGlow
+from .fade_reveal import FadeReveal
+
+__all__ = [
+    "AdvanceClockTime",
+    "PendulumOscillation",
+    "LampGlow",
+    "FadeReveal",
+]
