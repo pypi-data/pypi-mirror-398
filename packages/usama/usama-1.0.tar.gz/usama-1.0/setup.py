e27e6dd41a9449e03d4da79eccfc2ea763ec604b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 14:07:18 +0000

    Add package usama/1.0/usama-1.0.tar.gz

---
+++ packages/usama/usama-1.0.tar.gz/usama-1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="usama",
+    version="1.0",
+    author="Saif",
+    author_email="saifullahanwar.numl@gmail.com",
+    description="Coming Soon",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.6',
+)
