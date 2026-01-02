c2503b314159b22b9c4647e63c71d7e237961e4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 14:07:18 +0000

    Add package usama/5.1.1/usama-5.1.1.tar.gz

---
+++ packages/usama/usama-5.1.1.tar.gz/usama-5.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="usama",
+    version="5.1.1",
+    author="Saif",
+    author_email="saifullahanwar.numl@gmail.com",
+    description="Thanks Sir",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.6',
+)
---
+++ packages/usama/usama-5.1.1.tar.gz/usama-5.1.1/usama/__init__.py
@@ -0,0 +1,4 @@
+from .core import (get_version, ali)
+
+__version__ = "5.1.1"
+__all__ = ['get_version', 'ali']
