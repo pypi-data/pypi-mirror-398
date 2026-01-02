6959227ecad6d3d10b2ac993341b680c5e0e287f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 17:06:12 +0000

    Add package msdlib/1.1.15/msdlib-1.1.15.tar.gz

---
+++ packages/msdlib/msdlib-1.1.15.tar.gz/msdlib-1.1.15/setup.py
@@ -0,0 +1,43 @@
+"""
+Author : Abdullah Al Masud\n
+email : abdullahalmasud.buet@gmail.com\n
+LICENSE : MIT License
+"""
+
+import setuptools
+
+
+def parse_requirements():
+    pkg = open('requirements.txt', 'r').read().split('\n')
+    pkg = [s.strip() for s in pkg if s.strip() != '']
+    return pkg
+
+
+def parse_version():
+    file = open('msdlib/__init__.py', 'r').read().split('\n')
+    _version = [f for f in file if '__version__=' in f.replace(
+        ' ', '')][0].split('=')[-1].strip()[1: -1]
+    return _version
+
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="msdlib",
+    version=parse_version(),
+    author="Abdullah Al Masud",
+    author_email="abdullahalmasud.buet@gmail.com",
+    description="msdlib is meant for making life easier of a common data scientist/data analyst/ML enginner.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/abdullah-al-masud/msdlib",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    install_requires=parse_requirements()
+)
---
+++ packages/msdlib/msdlib-1.1.15.tar.gz/msdlib-1.1.15/tests/test_mlutils.py
@@ -0,0 +1,98 @@
+"""
+Author : Abdullah Al Masud\n
+email : abdullahalmasud.buet@gmail.com\n
+LICENSE : MIT License
+"""
+
+import shutil
+from msdlib import mlutils
+import torch
+from msdlib import msd
+
+
+tempdir = 'tests/storage'
+
+
+def test_define_layers():
+    """
+    To check the define_layers function's activity
+    """
+    units = [5, 5, 5, 5, 5, 5]
+    input_units = 10
+    output_units = 2
+    activation = torch.nn.ReLU()
+    model_type = 'multi-classifier'
+
+    total_layers = 14
+
+    layers = mlutils.define_layers(
+        input_units, output_units, units, actual_units=True, model_type=model_type, apply_bn=True)
+    assert len(layers) == total_layers
+
+
+def test_torchModel():
+
+    least_f1_score = .95
+
+    from sklearn.datasets import load_iris
+
+    dataloader = load_iris()
+    data = dataloader['data']
+    label = dataloader['target']
+
+    splitter = msd.SplitDataset(data, label)
+    outdata = splitter.random_split(val_ratio=.2)
+
+    layers = mlutils.define_layers(data.shape[1], len(dataloader['target_names']), [100, 100],
+                                   model_type='regressor', actual_units=True, activation=torch.nn.ReLU())
+    tmodel = mlutils.torchModel(
+        layers, epoch=80, model_type='multi-classifier', plot_loss=False, plot_evaluation=False, savepath=tempdir)
+    tmodel.fit(outdata['train']['data'], outdata['train']['label'], val_data=outdata['validation']['data'],
+               val_label=outdata['validation']['label'], evaluate=False)
+    pred = tmodel.predict(outdata['validation']
+                          ['data']).argmax(1).cpu().numpy()
+    result, distmat = msd.class_result(
+        outdata['validation']['label'], pred, True)
+    assert result['average'].loc['f1_score'] >= least_f1_score
+    shutil.rmtree(tempdir)
+
+
+def test_torchModel_with_loader():
+
+    least_f1_score = .95
+
+    from sklearn.datasets import load_iris
+
+    dataloader = load_iris()
+    data = dataloader['data']
+    label = dataloader['target']
+
+    splitter = msd.SplitDataset(data, label)
+    outdata = splitter.random_split(val_ratio=.2)
+
+    layers = mlutils.define_layers(data.shape[1], len(dataloader['target_names']), [100, 100],
+                                   model_type='regressor', actual_units=True, activation=torch.nn.ReLU())
+    
+    # Training Pytorch model
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    if device == 'cpu':
+        use_gpu = False
+    else:
+        use_gpu = True
+    tmodel = mlutils.torchModel(
+        layers, epoch=80, model_type='multi-classifier', plot_loss=False, plot_evaluation=False, use_gpu=use_gpu, savepath=tempdir)
+    
+    train_set = mlutils.DataSet(torch.tensor(outdata['train']['data']), 
+                                torch.tensor(outdata['train']['label']).squeeze(), dtype=torch.float32, model_type='multi-classifier')
+    val_set = mlutils.DataSet(torch.tensor(outdata['validation']['data']), 
+                                torch.tensor(outdata['validation']['label']).squeeze(), dtype=torch.float32, model_type='multi-classifier')
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=32)
+    val_loader = torch.utils.data.DataLoader(val_set, batch_size=32)
+
+    tmodel.fit(train_loader=train_loader, val_loader=val_loader, evaluate=False)
+    pred = tmodel.predict(outdata['validation']
+                          ['data']).argmax(1).cpu().numpy()
+    result, distmat = msd.class_result(
+        outdata['validation']['label'], pred, True)
+    assert result['average'].loc['f1_score'] >= least_f1_score
+    shutil.rmtree(tempdir)
