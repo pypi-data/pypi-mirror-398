8e48319c7090197f86824a6666c86d5e4b666039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 15:07:19 +0000

    Add package VideoTimestamps/1.1.0/videotimestamps-1.1.0-cp310-cp310-musllinux_1_2_x86_64.whl

---
+++ packages/VideoTimestamps/videotimestamps-1.1.0-cp310-cp310-musllinux_1_2_x86_64.whl/video_timestamps/__init__.py
@@ -0,0 +1,11 @@
+# Packages
+from .video_provider import *
+
+# Files
+from .abc_timestamps import *
+from .fps_timestamps import *
+from .rounding_method import *
+from .text_file_timestamps import *
+from .time_type import *
+from .time_unit_converter import *
+from .video_timestamps import *
