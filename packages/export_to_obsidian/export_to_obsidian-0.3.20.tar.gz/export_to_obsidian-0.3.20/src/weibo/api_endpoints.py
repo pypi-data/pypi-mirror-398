8babeeeded786e3723d6922592533540513086e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 16:06:56 +0000

    Add package export_to_obsidian/0.3.20/export_to_obsidian-0.3.20.tar.gz

---
+++ packages/export_to_obsidian/export_to_obsidian-0.3.20.tar.gz/export_to_obsidian-0.3.20/src/weibo/api_endpoints.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Author : bGZo
+@Date : 2025-12-06
+@Links : https://github.com/bGZo
+"""
+
+WEIBO_LIKE_URL = "https://weibo.com/ajax/statuses/likelist"
+# waiting
+FAV_URL = "https://www.example.com/api"
