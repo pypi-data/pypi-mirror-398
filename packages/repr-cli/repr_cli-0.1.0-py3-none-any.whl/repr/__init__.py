bfb652b06a8884ca4e47ee25a68b85420bfdcaf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 17:07:07 +0000

    Add package repr-cli/0.1.0/repr_cli-0.1.0-py3-none-any.whl

---
+++ packages/repr-cli/repr_cli-0.1.0-py3-none-any.whl/repr/__init__.py
@@ -0,0 +1,10 @@
+"""
+Repr CLI - Privacy-first developer profile generator.
+
+Analyzes your local git repositories and generates a compelling
+developer profile without ever sending your source code to the cloud.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Repr"
+__email__ = "hello@repr.dev"
