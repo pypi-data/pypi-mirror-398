88da42f27c68d0fc79425f7896207f0624e768d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 16:07:41 +0000

    Add package type-bridge/1.2.3/type_bridge-1.2.3.tar.gz

---
+++ packages/type-bridge/type_bridge-1.2.3.tar.gz/type_bridge-1.2.3/type_bridge/expressions/__init__.py
@@ -0,0 +1,34 @@
+"""
+Type-safe query expression system for TypeBridge.
+
+This module provides expression classes for building type-safe TypeQL queries.
+"""
+
+from type_bridge.expressions.aggregate import AggregateExpr
+from type_bridge.expressions.base import Expression
+from type_bridge.expressions.boolean import BooleanExpr
+from type_bridge.expressions.comparison import AttributeExistsExpr, ComparisonExpr
+from type_bridge.expressions.functions import (
+    FunctionCall,
+    FunctionCallExpr,
+    FunctionQuery,
+    ReturnType,
+)
+from type_bridge.expressions.iid import IidExpr
+from type_bridge.expressions.role_player import RolePlayerExpr
+from type_bridge.expressions.string import StringExpr
+
+__all__ = [
+    "Expression",
+    "ComparisonExpr",
+    "AttributeExistsExpr",
+    "StringExpr",
+    "BooleanExpr",
+    "AggregateExpr",
+    "FunctionCallExpr",
+    "FunctionCall",
+    "FunctionQuery",
+    "ReturnType",
+    "IidExpr",
+    "RolePlayerExpr",
+]
