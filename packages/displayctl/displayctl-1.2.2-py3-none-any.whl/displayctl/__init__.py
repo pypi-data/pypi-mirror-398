5983f088da4ee7e67267f570eaf523f9a0631ae8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 14:06:33 +0000

    Add package displayctl/1.2.2/displayctl-1.2.2-py3-none-any.whl

---
+++ packages/displayctl/displayctl-1.2.2-py3-none-any.whl/displayctl/__init__.py
@@ -0,0 +1,5 @@
+"""DisplayCtl - Display Configuration Manager for GNOME."""
+
+__version__ = "1.2.2"
+__author__ = "DisplayCtl Contributors"
+__description__ = "Display Configuration Manager for GNOME"
